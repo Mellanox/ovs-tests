98c75d441c95740fcda890c311647507353bc86d
Roi Dayan <roid@mellanox.com>
2017-02-26 08:17:49 +0200

    New test to try and replace tc rule
    Signed-off-by: Roi Dayan <roid@mellanox.com>

---
+++ test-tc-replace.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Test that verified tc replace flower rule cause syndrome
+#
+# Bug SW #988519: Trying to replace a flower rule cause a syndrome and rule to be deleted
+#
+
+NIC=${1:-ens5f0}
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+
+title "Test for groups overlapping"
+start_check_syndrome
+reset_tc_nic $NIC
+
+for i in `seq 10`; do
+    tc filter replace dev $NIC protocol 0x806 parent ffff: prio 8 handle 0x1 flower  dst_mac e4:11:22:11:4a:51 src_mac e4:11:22:11:4a:50 action drop
+done
+
+check_syndrome && success || err "Failed"
+reset_tc_nic $NIC
+
+echo "done"
