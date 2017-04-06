bfcf16530f7e6a48f0f275b8b60cc61008d099f5
Roi Dayan <roid@mellanox.com>
2017-04-06 10:42:39 +0300

    test-del-flows-during-reload.sh: cleanup
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-del-flows-during-reload.sh
+++ test-del-flows-during-reload.sh
@@ -3,7 +3,7 @@
 # Test reload of mlx5 core module while deleting tc flows from userspace
 # 1. add many tc rules
 # 2. start reload of mlx5 core module in the background
-# 3. sleep 5 seconds and start deleting tc rules
+# 3. start deleting tc rules
 #
 # Expected result: not to crash
 #
@@ -15,20 +15,14 @@ my_dir="$(dirname "$0")"
 . $my_dir/common.sh
 
 rep=${NIC}_0
+enable_switchdev_if_no_rep $rep
 if [ ! -e /sys/class/net/$rep ]; then
     fail "Missing rep $rep"
     exit 1
 fi
-vf=virtfn0
-vfpci=$(basename `readlink /sys/class/net/$NIC/device/$vf`)
-if [ ! -e /sys/bus/pci/drivers/mlx5_core/$vfpci ]; then
-    echo "bind vf $vfpci"
-    echo $vfpci > /sys/bus/pci/drivers/mlx5_core/bind
-fi
 reset_tc_nic $NIC
 reset_tc_nic $rep
 
-set -e
 
 title "add $COUNT rules"
 for i in `seq $COUNT`; do
@@ -43,12 +37,19 @@ done
 
 function del_rules() {
     local pref=49152
+    local first=true
     for i in `seq $COUNT`; do
         num1=`printf "%02x" $((i / 100))`
         num2=`printf "%02x" $((i % 100))`
-        tc filter del dev $rep protocol ip parent ffff: prio $((pref--)) || fail "Failed to del rule"
+        tc filter del dev $rep protocol ip parent ffff: prio $((pref--))
+        if [ "$?" != 0 ]; then
+            if [ $first = true ]; then
+                fail "Failed to del rule"
+            fi
+            break
+        fi
+        first=false
     done
-    echo "del rules done"
 }
 
 function reload_modules() {
@@ -63,9 +64,10 @@ function reload_modules() {
 
 title "test reload modules"
 reload_modules &
-sleep 5
+sleep 1
 title "del $COUNT rules"
 del_rules
+sleep 5
+reset_tc_nic $rep
 
-success "Test success"
-echo "done"
+test_done
