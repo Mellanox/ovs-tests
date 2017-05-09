ef08bb1e284696c8138695319c3fa72c5071b084
Roi Dayan <roid@mellanox.com>
2017-05-09 11:04:01 +0300

    Add test to verify ping between rep->vf and vf->rep
    Signed-off-by: Roi Dayan <roid@mellanox.com>

---
+++ test-vf-rep-ping.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Bug SW #1040416: slow path xmit on VF reps broken
+#
+# with tcpdump we could see traffic VF->rep works but rep->VF doesn't.
+#
+
+NIC=${1:-ens5f0}
+VF=${2:-ens5f2}
+REP=${3:-ens5f0_0}
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+IP1="7.7.7.1"
+IP2="7.7.7.2"
+
+enable_switchdev_if_no_rep $REP
+bind_vfs
+
+function cleanup() {
+    echo "cleanup"
+    ip netns del ns0 &> /dev/null
+}
+
+cleanup
+ifconfig $REP $IP1/24 up
+ip netns add ns0
+ip link set $VF netns ns0
+ip netns exec ns0 ifconfig $VF $IP2/24 up
+
+title "Test ping $IP1 -> $IP2"
+ping -q -c 2 -w 2 $IP2 && success || err
+
+title "Test ping $IP2 -> $IP1"
+ip netns exec ns0 ping -q -c 2 -w 2 $IP1 && success || err
+
+cleanup
+test_done
