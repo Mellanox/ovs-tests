db00ffbf88daa04d8b81fe4027729ed9ebbe984b
Roi Dayan <roid@mellanox.com>
2017-07-06 17:38:42 +0300

    test-tc-insert-rules.sh: remove title of check syndrome
    since the log goes to dmesg the check_syndrome operation will catch it.
    
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-tc-insert-rules.sh
+++ test-tc-insert-rules.sh
@@ -269,7 +269,6 @@ function test_insert_ip_with_unmatched_bits_mask() {
 			src_mac e4:11:22:11:4a:50 \
 			src_ip 1.1.1.5/24 \
                 action drop
-    title "-check syndrome"
     check_syndrome && success || err "Failed"
 }
 
