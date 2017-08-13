33a3b628134bbca9f1a82406d4df49a1d1504ce7
Roi Dayan <roid@mellanox.com>
2017-08-13 15:15:01 +0300

    test-tc-shuffle-2.sh: add prios to avoid syndrome because of duplicate rule
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-tc-shuffle-2.sh
+++ test-tc-shuffle-2.sh
@@ -17,15 +17,15 @@ my_dir="$(dirname "$0")"
 TMPFILE=/tmp/rules-$$
 
 cat >$TMPFILE <<EOF
-tc filter add dev $NIC parent ffff: protocol arp  flower dst_mac e4:1d:2d:5d:25:35 src_mac e4:1d:2d:5d:25:34 action mirred egress redirect dev $NIC
-tc filter add dev $NIC parent ffff: protocol ip  flower dst_mac e4:1d:2d:5d:25:35 src_mac e4:1d:2d:5d:25:34 action mirred egress redirect dev $NIC
+tc filter add dev $NIC parent ffff: protocol arp prio 1 flower dst_mac e4:1d:2d:5d:25:35 src_mac e4:1d:2d:5d:25:34 action mirred egress redirect dev $NIC
+tc filter add dev $NIC parent ffff: protocol ip prio 2 flower dst_mac e4:1d:2d:5d:25:35 src_mac e4:1d:2d:5d:25:34 action mirred egress redirect dev $NIC
 
-tc filter add dev $NIC parent ffff: protocol ip flower dst_mac e4:1d:2d:5d:25:35 src_mac e4:1d:2d:5d:25:34 ip_proto udp src_port 11636 action mirred egress redirect dev $NIC
-tc filter add dev $NIC parent ffff: protocol ip flower dst_mac e4:1d:2d:5d:25:35 src_mac e4:1d:2d:5d:25:34 ip_proto udp src_port 2229 action mirred egress redirect dev $NIC
-tc filter add dev $NIC parent ffff: protocol ip flower dst_mac e4:1d:2d:5d:25:35 src_mac e4:1d:2d:5d:25:34 ip_proto udp src_port 6822 action mirred egress redirect dev $NIC
-tc filter add dev $NIC parent ffff: protocol ip flower dst_mac e4:1d:2d:5d:25:35 src_mac e4:1d:2d:5d:25:34 ip_proto udp src_port 2009 action mirred egress redirect dev $NIC
-tc filter add dev $NIC parent ffff: protocol ip flower dst_mac e4:1d:2d:5d:25:35 ip_proto udp src_port 2009 action mirred egress redirect dev $NIC
-tc filter add dev $NIC parent ffff: protocol ip flower dst_mac e4:1d:2d:5d:25:39 action mirred egress redirect dev $NIC
+tc filter add dev $NIC parent ffff: protocol ip prio 3 flower dst_mac e4:1d:2d:5d:25:35 src_mac e4:1d:2d:5d:25:34 ip_proto udp src_port 11636 action mirred egress redirect dev $NIC
+tc filter add dev $NIC parent ffff: protocol ip prio 4 flower dst_mac e4:1d:2d:5d:25:35 src_mac e4:1d:2d:5d:25:34 ip_proto udp src_port 2229 action mirred egress redirect dev $NIC
+tc filter add dev $NIC parent ffff: protocol ip prio 5 flower dst_mac e4:1d:2d:5d:25:35 src_mac e4:1d:2d:5d:25:34 ip_proto udp src_port 6822 action mirred egress redirect dev $NIC
+tc filter add dev $NIC parent ffff: protocol ip prio 6 flower dst_mac e4:1d:2d:5d:25:35 src_mac e4:1d:2d:5d:25:34 ip_proto udp src_port 2009 action mirred egress redirect dev $NIC
+tc filter add dev $NIC parent ffff: protocol ip prio 7 flower dst_mac e4:1d:2d:5d:25:35 ip_proto udp src_port 2009 action mirred egress redirect dev $NIC
+tc filter add dev $NIC parent ffff: protocol ip prio 8 flower dst_mac e4:1d:2d:5d:25:39 action mirred egress redirect dev $NIC
 
 tc filter del dev $NIC parent ffff:
 EOF
