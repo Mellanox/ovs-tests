ef1387c19ac285d49775b1431620a41f57e5e0ce
Roi Dayan <roid@mellanox.com>
2017-08-15 08:57:55 +0300

    test-tc-max-rules.sh: Refactor to have timeout
    Currently defaults to 30s which is a lot for current cases.
    If a case takes more than 30s then we have an issue.
    
    Remove redundant test-tc-insert-rate.sh which only tested timeout
    and not rate.
    
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-tc-max-rules.sh
+++ test-tc-max-rules.sh
@@ -11,42 +11,57 @@ NIC2=${2:-ens5f1}
 my_dir="$(dirname "$0")"
 . $my_dir/common.sh
 
-set -e
-
 CASE_SKIP=${CASE_SKIP:-skip_hw skip_sw}
 CASE_COUNT=${CASE_COUNT:-30*1024 64*1024-16}
 CASE_INDEX=${CASE_INDEX:-0 1}
+TIMEOUT=${TIMEOUT:-30s}
 
-for _nic in $NIC $NIC2; do
-	# in case user has only one NIC
-	if [[ "$_nic" == "" ]]; then
-		continue
-	fi
-	for skip in $CASE_SKIP; do
-		for num in $CASE_COUNT; do
-			((num=num))
-			# if set_index == 1, all filters share the same action
-			for index in $CASE_INDEX; do
-				title "Testing $num rules $skip $_nic set_index:$index"
-				sh $my_dir/tc_batch.sh $num $skip $_nic $index \
-				    && success || fail
-				echo "cleanup"
-				reset_tc_nic $_nic
-			done
-		done
-	done
-done
 
-((num=64*1024-16))
-skip=skip_sw
-index=0
-title "Add both ports $num rules $skip set_index:$index"
-sh $my_dir/tc_batch.sh $num $skip $NIC $index \
-    && success || fail
-sh $my_dir/tc_batch.sh $num $skip $NIC2 $index \
-    && success || fail
-echo "cleanup"
+function tc_batch() {
+    timeout $TIMEOUT sh $my_dir/tc_batch.sh $@ \
+    && success || rc=$? && err
+    if [ "$rc" == "124" ]; then
+        err "Timed out after $TIMEOUT"
+    fi
+    return $rc
+}
+
+function do_test1() {
+    for _nic in $NIC $NIC2; do
+        # in case user has only one NIC
+        if [[ "$_nic" == "" ]]; then
+            continue
+        fi
+        for skip in $CASE_SKIP; do
+            for num in $CASE_COUNT; do
+                ((num=num))
+                # if set_index == 1, all filters share the same action
+                for index in $CASE_INDEX; do
+                    title "Testing $num rules $skip $_nic set_index:$index"
+                    tc_batch $num $skip $_nic $index || return
+                    echo "cleanup"
+                    reset_tc_nic $_nic
+                done
+            done
+        done
+    done
+}
+
+function do_test2() {
+    ((num=64*1024-16))
+    skip=skip_sw
+    index=0
+    title "Add both ports $num rules $skip set_index:$index"
+    tc_batch $num $skip $NIC $index || return
+    tc_batch $num $skip $NIC2 $index || return
+    echo "cleanup"
+    reset_tc_nic $NIC
+    reset_tc_nic $NIC2
+}
+
+
+do_test1
+do_test2
 reset_tc_nic $NIC
 reset_tc_nic $NIC2
-
 test_done
