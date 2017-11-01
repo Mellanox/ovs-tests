1d89369d9f34bc0694f66a5bb48700723fefcf7d
Roi Dayan <roid@mellanox.com>
2017-11-01 15:10:28 +0000

    test-tc-vxlan-decap-not-properly-offloaded.sh: Fix getting rep device
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-tc-vxlan-decap-not-properly-offloaded.sh
+++ test-tc-vxlan-decap-not-properly-offloaded.sh
@@ -11,11 +11,7 @@ my_dir="$(dirname "$0")"
 . $my_dir/common.sh
 
 get_mst_dev
-MST=$DEV
 
-# this has to be your PF
-# ETH_0 is assumed to be the VF rep device
-ETH=$NIC
 VXLAN=vxlan_sys_4789
 TC=tc
 IP=ip
@@ -23,19 +19,23 @@ SKIP_DEC=skip_sw
 TUN_SRC_V4=20.1.184.1
 TUN_DST_V4=20.1.183.1
 VM_DST_MAC=e4:11:22:33:44:70
+REP=`get_rep 0`
+if [ -z "$REP" ]; then
+    fail "Missing rep $rep"
+fi
 
 $IP link add $VXLAN type vxlan dstport 4789 external udp6zerocsumrx
 ifconfig $VXLAN up
 tc qdisc add dev $VXLAN ingress
 
-reset_tc_nic $ETH
-reset_tc_nic ${ETH}_0
+reset_tc_nic $NIC
+reset_tc_nic $REP
 
-$IP addr add dev $ETH $TUN_SRC_V4/16
+$IP addr add dev $NIC $TUN_SRC_V4/16
 
 rm -fr /tmp/fsdump_before_add /tmp/fsdump_after_add /tmp/fsdump_after_del
 
-mlxdump -d $MST fsdump --type FT --no_zero=true > /tmp/fsdump_before_add || err "mlxdump failed"
+mlxdump -d $DEV fsdump --type FT --no_zero=true > /tmp/fsdump_before_add || err "mlxdump failed"
 
 # decap rule set on the vxlan device
 title "Add vxlan decap rule"
@@ -44,9 +44,9 @@ $TC filter add dev $VXLAN protocol ip parent ffff: prio 10\
                 enc_key_id 100 enc_dst_port 4789 src_mac $VM_DST_MAC \
                 $SKIP_DEC \
                 action tunnel_key unset \
-                action mirred egress redirect dev ${ETH}_0 || err "TC command failed"
+                action mirred egress redirect dev $REP || err "TC command failed"
 
-mlxdump -d $MST fsdump --type FT --no_zero=true > /tmp/fsdump_after_add || err "mlxdump failed"
+mlxdump -d $DEV fsdump --type FT --no_zero=true > /tmp/fsdump_after_add || err "mlxdump failed"
 
 DIF=`diff -u /tmp/fsdump_before_add /tmp/fsdump_after_add`
 
@@ -55,10 +55,10 @@ if [ -z "$DIF" ]; then
 fi
 
 title "Delete ingress qdisc"
-$TC qdisc del dev ${ETH}_0 ingress
+$TC qdisc del dev $REP ingress
 $TC qdisc del dev $VXLAN ingress
 
-mlxdump -d $MST fsdump --type FT --no_zero=true > /tmp/fsdump_after_del || err "mlxdump failed"
+mlxdump -d $DEV fsdump --type FT --no_zero=true > /tmp/fsdump_after_del || err "mlxdump failed"
 
 title "Verify rule deleted from HW"
 DIF=`diff -u /tmp/fsdump_after_add /tmp/fsdump_after_del`
