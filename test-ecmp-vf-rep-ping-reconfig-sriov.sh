a9f354d48af0f143ea322692d9fa6d3d2be02235
Roi Dayan <roid@mellanox.com>
2017-12-18 17:46:38 +0200

    test-ecmp-vf-rep-ping-reconfig-sriov.sh: Fix test
    Update test to verify another crash without breaking catching old bug.
    
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-ecmp-vf-rep-ping-reconfig-sriov.sh
+++ test-ecmp-vf-rep-ping-reconfig-sriov.sh
@@ -7,7 +7,8 @@
 # Bug SW #1242030: [ASAP MLNX OFED] VF to Rep traffic doesn't work after
 # reconfiguring VFs without reloading mlx5_core
 #
-# Bug SW #1244300: system crash after reconfig SRIOV several time
+# Bug SW #1244300: Crash reconfiguring SRIOV+switchdev more than once with
+# different VFs per port
 #
 
 NIC=${1:-ens5f0}
@@ -16,6 +17,8 @@ REP=${3:-ens5f0_0}
 my_dir="$(dirname "$0")"
 . $my_dir/common.sh
 
+IP1="7.7.7.1"
+IP2="7.7.7.2"
 
 function disable_sriov() {
     title "- Disable SRIOV"
@@ -25,8 +28,27 @@ function disable_sriov() {
 
 function enable_sriov() {
     title "- Enable SRIOV"
-    echo 2 > /sys/class/net/$NIC/device/sriov_numvfs
-    echo 2 > /sys/class/net/$NIC2/device/sriov_numvfs
+    echo $VF_COUNT_PF0 > /sys/class/net/$NIC/device/sriov_numvfs
+    echo $VF_COUNT_PF1 > /sys/class/net/$NIC2/device/sriov_numvfs
+}
+
+function cleanup() {
+    ip netns del ns0 2> /dev/null
+    ifconfig $REP 0
+}
+
+function ping_test() {
+    cleanup
+    ifconfig $REP $IP1/24 up
+    ip netns add ns0
+    ip link set $VF netns ns0
+    ip netns exec ns0 ifconfig $VF $IP2/24 up
+
+    title "Test ping REP($IP1) -> VF($IP2)"
+    ping -q -c 10 -i 0.2 -w 2 $IP2 && success || err
+
+    title "Test ping VF($IP2) -> REP($IP1)"
+    ip netns exec ns0 ping -q -c 10 -i 0.2 -w 2 $IP1 && success || err
 }
 
 function do_test() {
@@ -35,14 +57,20 @@ function do_test() {
     disable_multipath
     enable_multipath || err "Failed to enable multipath"
     enable_sriov
+    set_macs
     enable_switchdev $NIC
     enable_switchdev $NIC2
-    $my_dir/test-vf-rep-ping-reconfig-sriov.sh || err
+    bind_vfs
+    sleep 2
+    ping_test || err
 }
 
 
-for i in `seq 10`; do
+VF_COUNT_PF0=2
+VF_COUNT_PF1=1
+for i in `seq 2`; do
     do_test || break
+    cleanup
 done
 
 disable_sriov
