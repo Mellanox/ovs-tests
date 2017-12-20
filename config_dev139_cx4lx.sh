c3608e5888acae23ed9412c55076f9f4d3da6237
Roi Dayan <roid@mellanox.com>
2017-12-20 10:30:48 +0200

    Add new vf-vf test
    This requires second VF rep. Update dev139 config as example.
    
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- config_dev139_cx4lx.sh
+++ config_dev139_cx4lx.sh
@@ -4,3 +4,4 @@ VF=ens2f2
 VF1=$VF
 VF2=ens2f3
 REP=ens2f0_0
+REP2=ens2f0_1
---
+++ test-vf-vf-ping.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# 
+#
+
+NIC=${1:-ens5f0}
+VF=${2:-ens5f2}
+REP=${3:-ens5f0_0}
+REP2=${3:-ens5f0_1}
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+IP1="7.7.7.1"
+IP2="7.7.7.2"
+
+enable_switchdev_if_no_rep $REP
+bind_vfs
+
+function cleanup() {
+    ip netns del ns0 2> /dev/null
+    ip netns del ns1 2> /dev/null
+}
+
+function config_vf() {
+    local ns=$1
+    local vf=$2
+    local rep=$3
+    local ip=$4
+
+    echo "$ns : $vf ($ip) -> $rep"
+    ifconfig $rep 0
+    ip netns add $ns
+    ip link set $vf netns $ns
+    ip netns exec $ns ifconfig $vf $ip/24 up
+}
+
+
+cleanup
+start_clean_openvswitch
+config_vf ns0 $VF $REP $IP1
+config_vf ns1 $VF2 $REP2 $IP2
+BR=ov1
+ovs-vsctl add-br $BR
+ovs-vsctl add-port $BR $REP
+ovs-vsctl add-port $BR $REP2
+
+title "Test ping VF($IP1) -> VF($IP2)"
+ip netns exec ns0 ping -q -c 10 -i 0.2 -w 2 $IP2 && success || err
+
+echo sleep 300
+sleep 300
+
+del_all_bridges
+cleanup
+test_done
