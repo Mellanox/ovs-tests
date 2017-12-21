c5d8110b610e520db6491cb74511a0c6dfb5bd07
Roi Dayan <roid@mellanox.com>
2017-12-21 13:01:22 +0200

    test-tc-insert-rules-port2.sh: Add missing ingress qdisc to port2
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-tc-insert-rules-port2.sh
+++ test-tc-insert-rules-port2.sh
@@ -37,6 +37,7 @@ start_check_syndrome
 disable_sriov_port2
 enable_sriov_port2
 enable_switchdev $NIC2
+reset_tc_nic $NIC2
 title "- Add drop rule"
 tc_filter add dev $NIC2 protocol ip parent ffff: flower dst_mac e4:11:22:11:4a:51 src_mac e4:11:22:11:4a:50 action drop
 disable_sriov_port2
