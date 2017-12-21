3fdd72258a9b1bb9eec2fe94037c5bb1455d970e
Roi Dayan <roid@mellanox.com>
2017-12-21 10:41:53 +0200

    Add test case to add simple rules in legacy mode
    Signed-off-by: Roi Dayan <roid@mellanox.com>

---
+++ test-tc-insert-rules-legacy.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#
+# 1. Test add rule without ip_proto
+# 2. Test add rule with unmatched bits
+#
+
+NIC=${1:-ens5f0}
+FILTER=${FILTER}
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+
+nic=$NIC
+skip=""
+
+function tc_filter() {
+    eval2 tc filter $@ && success || err
+}
+
+function test_basic_L2() {
+    tc_filter add dev $nic protocol ip parent ffff: \
+            flower \
+                    $skip \
+                    dst_mac e4:11:22:11:4a:51 \
+                    src_mac e4:11:22:11:4a:50 \
+            action drop
+}
+
+function test_basic_L3() {
+    tc_filter add dev $nic protocol ip parent ffff: \
+            flower \
+                    $skip \
+                    dst_mac e4:11:22:11:4a:51 \
+                    src_mac e4:11:22:11:4a:50 \
+                    src_ip 1.1.1.1 \
+                    dst_ip 2.2.2.2 \
+            action drop
+}
+
+function test_basic_L3_ipv6() {
+    tc_filter add dev $nic protocol ipv6 parent ffff: \
+            flower \
+                    $skip \
+                    dst_mac e4:11:22:11:4a:51 \
+                    src_mac e4:11:22:11:4a:50 \
+                    src_ip 2001:0db8:85a3::8a2e:0370:7334\
+                    dst_ip 2001:0db8:85a3::8a2e:0370:7335 \
+            action drop
+}
+
+function test_basic_L4() {
+    tc_filter add dev $nic protocol ip parent ffff: \
+            flower \
+                    $skip \
+                    dst_mac e4:11:22:11:4a:51 \
+                    src_mac e4:11:22:11:4a:50 \
+                    ip_proto tcp \
+                    src_ip 1.1.1.1 \
+                    dst_ip 2.2.2.2 \
+            action drop
+}
+
+
+reset_tc_nic $NIC
+unbind_vfs
+switch_mode_legacy
+
+# Execute all test_* functions
+for i in `declare -F | awk {'print $3'} | grep ^test_ | grep -v test_done` ; do
+    title $i
+    eval $i
+    reset_tc_nic $nic
+done
+
+check_kasan
+test_done
