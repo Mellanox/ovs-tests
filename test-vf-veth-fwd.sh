d0be00283448610b5c80f4d5a1b1fbc9bd0ac373
Roi Dayan <roid@mellanox.com>
2018-01-24 10:51:07 +0200

    test-vf-veth-fwd.sh: move cleanup into an exit trap function
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-vf-veth-fwd.sh
+++ test-vf-veth-fwd.sh
@@ -5,13 +5,16 @@
 
 my_dir="$(dirname "$0")"
 
-ip link del veth0 2>/dev/null
-ip link del veth1 2>/dev/null
+function clean_veth() {
+    ip link del veth0 2>/dev/null
+    ip link del veth1 2>/dev/null
+}
+
+trap clean_veth EXIT
+clean_veth
 ip link add veth0 type veth peer name veth1
 
 FORCE_VF2=veth0
 FORCE_REP2=veth1
 
 . $my_dir/test-vf-vf-fwd.sh
-
-ip link del veth0 2>/dev/null
