9e46910cf9a400f6524ffb7be978424b3d444ad3
ziyadatiyyeh <ziyadat@mellanox.com>
2018-01-31 10:11:09 +0200

    Move reload_modules to common.sh in order not to duplicate code
    Signed-off-by: Ziyad Atiyyeh <ziyadat@mellanox.com>

--- test-eswitch-add-flows-during-reload.sh
+++ test-eswitch-add-flows-during-reload.sh
@@ -44,22 +44,6 @@ function add_rules() {
     done
 }
 
-function reload_modules() {
-    if [ "$devlink_compat" = 1 ]; then
-        service openibd force-restart
-    else
-        modprobe -r mlx5_ib mlx5_core devlink || fail "Failed to unload modules"
-        modprobe -a devlink mlx5_core mlx5_ib || fail "Failed to load modules"
-    fi
-
-    a=`journalctl -n200 | grep KASAN || true`
-    if [ "$a" != "" ]; then
-        fail "Detected KASAN in journalctl"
-    fi
-    set_macs
-    echo "reload modules done"
-}
-
 
 title "test reload modules"
 start_check_syndrome
