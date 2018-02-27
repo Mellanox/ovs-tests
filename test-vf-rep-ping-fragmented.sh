32c4db09931deb2863f87e37e7c1d8debfc776c3
Roi Dayan <roid@mellanox.com>
2018-02-27 15:59:49 +0200

    Add new test to verify vf-rep fragmented traffic
    Signed-off-by: Roi Dayan <roid@mellanox.com>

---
+++ test-vf-rep-ping-fragmented.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Bug SW #1040416: slow path xmit on VF reps broken
+# Bug SW #896876: IP fragments sent by VFs are dropped
+#
+
+NIC=${1:-ens5f0}
+VF=${2:-ens5f2}
+REP=${3:-ens5f0_0}
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+IP1="7.7.7.1"
+IP2="7.7.7.2"
+
+enable_switchdev_if_no_rep $REP
+bind_vfs
+
+function cleanup() {
+    ip netns del ns0 2> /dev/null
+    ifconfig $REP 0
+}
+
+cleanup
+ifconfig $REP $IP1/24 up
+ip netns add ns0
+ip link set $VF netns ns0
+ip netns exec ns0 ifconfig $VF $IP2/24 up
+
+SIZE=2222
+
+title "Test ping REP($IP1) -> VF($IP2)"
+ping -q -c 10 -i 0.2 -w 2 -s $SIZE $IP2 && success || err
+
+title "Test ping VF($IP2) -> REP($IP1)"
+ip netns exec ns0 ping -q -c 10 -i 0.2 -w 2 -s $SIZE $IP1 && success || err
+
+cleanup
+test_done
