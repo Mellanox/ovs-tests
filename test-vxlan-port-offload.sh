b890055c4e1871d2bb01681f5018685e7d4a4554
Roi Dayan <roid@mellanox.com>
2018-03-20 12:14:53 +0200

    Add new text addingvxlan rule after driver reload
    Signed-off-by: Roi Dayan <roid@mellanox.com>

---
+++ test-vxlan-port-offload.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+#
+# Test insert vxlan rule after driver reload
+# - create vxlan interface
+# - reload driver
+# - add vxlan rule
+#
+
+NIC=${1:-ens5f0}
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+
+function tc_filter() {
+    eval2 tc filter $@ && success || err
+}
+
+function __test_vxlan() {
+    local ip_src=$1
+    local ip_dst=$2
+    local skip
+
+    title " - create vxlan interface"
+    # note: we support adding decap to vxlan interface only.
+    vx=vxlan1
+    vxlan_port=4789
+    ip link del $vx >/dev/null 2>&1
+    ip link add $vx type vxlan dstport $vxlan_port external
+    ip link set dev $vx up
+    tc qdisc add dev $vx ingress
+
+    ip addr flush dev $NIC
+    ip addr add $ip_src/16 dev $NIC
+    ifconfig $NIC up
+    ip neigh add $ip_dst lladdr e4:11:22:11:55:55 dev $NIC
+
+    reset_tc_nic $NIC
+    reset_tc_nic $REP
+
+    ip a show dev $vx
+
+    title " - reload modules"
+    reload_modules
+    enable_switchdev
+    bind_vfs
+
+    title " - add vxlan rule"
+    skip='skip_sw'
+        title "- skip:$skip"
+        reset_tc $REP
+        reset_tc $vx
+        title "    - encap"
+        tc_filter add dev $REP protocol 0x806 parent ffff: \
+                    flower \
+                            $skip \
+                            dst_mac e4:11:22:11:4a:51 \
+                            src_mac e4:11:22:11:4a:50 \
+                    action tunnel_key set \
+                    src_ip $ip_src \
+                    dst_ip $ip_dst \
+                    dst_port $vxlan_port \
+                    id 100 \
+                    action mirred egress redirect dev $vx
+        title "    - decap"
+        tc_filter add dev $vx protocol 0x806 parent ffff: \
+                    flower \
+                            $skip \
+                            dst_mac e4:11:22:11:4a:51 \
+                            src_mac e4:11:22:11:4a:50 \
+                            enc_src_ip $ip_src \
+                            enc_dst_ip $ip_dst \
+                            enc_dst_port $vxlan_port \
+                            enc_key_id 100 \
+                    action tunnel_key unset \
+                    action mirred egress redirect dev $REP
+
+    reset_tc $NIC
+    reset_tc $REP
+    reset_tc $vx
+    ip addr flush dev $NIC
+    ip link del $vx
+}
+
+function test_basic_vxlan_ipv4() {
+    __test_vxlan \
+                        20.1.11.1 \
+                        20.1.12.1
+}
+
+title "Test adding vxlan rule after creating vxlan interfaces and reloading modules"
+test_basic_vxlan_ipv4
+check_for_err "isn't an offloaded vxlan udp dport"
+
+test_done
