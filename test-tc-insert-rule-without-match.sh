8c292575091bf2d42d84a815204a91c1b58ebab0
Roi Dayan <roid@mellanox.com>
2018-03-27 12:51:45 +0300

    Add new test checking for adding rule without any match
    Signed-off-by: Roi Dayan <roid@mellanox.com>

---
+++ test-tc-insert-rule-without-match.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Bug SW #1338214: [TC] failure to create rule without any match
+#
+# In error we reproduce this syndrome:
+# BAD_PARAM           | 0x7E1580 |  create_flow_group: outer_headers valid bit is set, but headers are zero
+#
+# Currently in ConnectX-4 we will still get syndrome about missing dst mac.
+#
+
+NIC=${1:-ens5f0}
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+enable_switchdev
+REP=`get_rep 0`
+if [ -z "$REP" ]; then
+    fail "Missing rep $rep"
+fi
+
+function tc_filter() {
+    eval tc filter $@ 2>&1
+}
+
+reset_tc_nic $NIC
+start_check_syndrome
+
+title "Test rule without match"
+tc_filter add dev $NIC parent ffff: flower skip_sw action drop
+err=$?
+if [ $DEVICE_IS_CX4 == 1 ]; then
+    echo "In ConnectX-4 we expect to fail with syndrome of missing dst mac."
+    expect_syndrome "0x29cdba" && success
+else
+    # ConnectX-5
+    if [ $err == 0 ]; then
+        success
+    else
+        err "Failed to add rule"
+    fi
+    check_syndrome
+fi
+
+reset_tc_nic $NIC
+test_done
