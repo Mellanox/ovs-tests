82ee77e097c6e61faa19f36e570e1edc589a15e5
Roi Dayan <roid@mellanox.com>
2018-03-29 17:26:07 +0300

    test-vf-rep-udp-fragmented.sh: Add ipv6 and ipv4 vlan cases
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-vf-rep-udp-fragmented.sh
+++ test-vf-rep-udp-fragmented.sh
@@ -9,15 +9,12 @@ REP=${3:-ens5f0_0}
 my_dir="$(dirname "$0")"
 . $my_dir/common.sh
 
-IP1="7.7.7.1"
-IP2="7.7.7.2"
-
 enable_switchdev_if_no_rep $REP
 bind_vfs
 
 function cleanup() {
     ip netns del ns0 2> /dev/null
-    ifconfig $REP 0
+    ip add flush dev $REP
 }
 
 function start_tcpdump() {
@@ -37,7 +34,16 @@ function stop_tcpdump() {
 
 function test_frags() {
     # match fragmented packets (not first)
-    count=`tcpdump -nnr $tdtmpfile 'ip[6] = 0' | wc -l`
+    if [ "$_test" == "ipv4" ]; then
+        count=`tcpdump -nnr $tdtmpfile 'ip[6] = 0' | wc -l`
+    elif [ "$_test" == "ipv4vlan" ]; then
+        count=`tcpdump -nnr $tdtmpfile 'vlan 2 && ip[6] = 0' | wc -l`
+    elif [ "$_test" == "ipv6" ]; then
+        count=`tcpdump -nnr $tdtmpfile 'ip6[6] = 44' | wc -l`
+    else
+        fail "Mssing _test value"
+    fi
+
     if [[ $count = 0 ]]; then
         err "No fragmented packets"
         tcpdump -nnr $tdtmpfile
@@ -46,45 +52,97 @@ function test_frags() {
     fi
 }
 
+function config_ipv4() {
+    title "Config IPv4"
+    cleanup
+    IP1="7.7.7.1"
+    IP2="7.7.7.2"
+    ifconfig $REP $IP1/24 up
+    ip netns add ns0
+    ip link set $VF netns ns0
+    ip netns exec ns0 ifconfig $VF $IP2/24 up
+    _test="ipv4"
+    iperf_ext=""
+}
 
-cleanup
-ifconfig $REP $IP1/24 up
-ip netns add ns0
-ip link set $VF netns ns0
-ip netns exec ns0 ifconfig $VF $IP2/24 up
+function config_ipv4_vlan() {
+    title "Config IPv4 VLAN"
+    cleanup
+    IP1="7.7.7.1"
+    IP2="7.7.7.2"
+    ifconfig $REP $IP1/24 up
+    ip netns add ns0
+    VF_VLAN=${VF}.2
+    ip link set $VF netns ns0
+    ip netns exec ns0 vconfig add $VF 2
+    ip netns exec ns0 ifconfig $VF up
+    ip netns exec ns0 ifconfig $VF_VLAN $IP2/24 up
+    ip netns exec ns0 ip n add $IP1 dev $VF_VLAN lladdr ae:99:98:22:73:14
+    _test="ipv4vlan"
+    iperf_ext=""
+}
 
-title "Test fragmented packets REP->VF"
-start_tcpdump
-iperf -u -c $IP2 -b 1M -l 2000 -n 1M
-stop_tcpdump
-title " - verify with tcpdump"
-test_frags
+function config_ipv6() {
+    title "Config IPv6"
+    cleanup
+    IP1="2001:0db8:0:f101::1"
+    IP2="2001:0db8:0:f101::2"
+    ifconfig $REP inet6 add $IP1/64 up || fail "Failed to config ipv6"
+    ip netns add ns0
+    ip link set $VF netns ns0
+    ip netns exec ns0 ifconfig $VF inet6 add $IP2/64 up || fail "Failed to config ipv6"
+    _test="ipv6"
+    iperf_ext="-V"
+}
 
-title "Test fragmented packets VF->REP"
-start_tcpdump
-ip netns exec ns0 iperf -u -c $IP1 -b 1M -l 2000 -n 1M
-stop_tcpdump
-title " - verify with tcpdump"
-test_frags
+function run_cases() {
 
-# this should be the smallest packet that gets fragmented.
-# the second fragment will be with data of size 1.
-title "Test fragmented packets VF->REP 1473"
-start_tcpdump
-ip netns exec ns0 iperf -u -c $IP1 -b 1M -l 1473 -n 1M
-stop_tcpdump
-title " - verify with tcpdump"
-test_frags
+    # cannot config vlan on VF representor.
+    if [ "$_test" != "ipv4vlan" ]; then
+        title "Test fragmented packets REP->VF"
+        start_tcpdump
+        iperf -u -c $IP2 -b 1M -l 2000 -n 1M $iperf_ext
+        stop_tcpdump
+        title " - verify with tcpdump"
+        test_frags
+    fi
 
-# the driver needs to copy udp header (8 bytes) for fragmented packet
-# to match L4 inline header.
-# this case is we have 7 bytes to copy and left with padding of 1.
-title "Test fragmented packets VF->REP 1479"
-start_tcpdump
-ip netns exec ns0 iperf -u -c $IP1 -b 1M -l 1479 -n 1M
-stop_tcpdump
-title " - verify with tcpdump"
-test_frags
+    title "Test fragmented packets VF->REP"
+    start_tcpdump
+    ip netns exec ns0 iperf -u -c $IP1 -b 1M -l 2000 -n 1M $iperf_ext
+    stop_tcpdump
+    title " - verify with tcpdump"
+    test_frags
+
+    # this should be the smallest packet that gets fragmented.
+    # the second fragment will be with data of size 1.
+    title "Test fragmented packets VF->REP 1473"
+    start_tcpdump
+    ip netns exec ns0 iperf -u -c $IP1 -b 1M -l 1473 -n 1M $iperf_ext
+    stop_tcpdump
+    title " - verify with tcpdump"
+    test_frags
+
+    # the driver needs to copy udp header (8 bytes) for fragmented packet
+    # to match L4 inline header.
+    # this case is we have 7 bytes to copy and left with padding of 1.
+    title "Test fragmented packets VF->REP 1479"
+    start_tcpdump
+    ip netns exec ns0 iperf -u -c $IP1 -b 1M -l 1479 -n 1M $iperf_ext
+    stop_tcpdump
+    title " - verify with tcpdump"
+    test_frags
+}
+
+
+config_ipv4
+run_cases
+
+config_ipv4_vlan
+run_cases
+
+config_ipv6
+run_cases
 
 cleanup
 test_done
