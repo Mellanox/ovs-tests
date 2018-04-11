86ff7c751926998803037bcd7b2136b7b6229c80
Roi Dayan <roid@mellanox.com>
2018-04-11 14:50:30 +0300

    test-eswitch-add-del-different-flows.sh: Fix prio and handle
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-eswitch-add-del-different-flows.sh
+++ test-eswitch-add-del-different-flows.sh
@@ -59,7 +59,7 @@ function add_rules_vlan_drop() {
     for i in `seq $COUNT`; do
         num1=`printf "%02x" $((i / 100))`
         num2=`printf "%02x" $((i % 100))`
-        tc filter add dev $NIC1 protocol 802.1Q parent ffff: prio $i \
+        tc filter add dev $NIC1 protocol 802.1Q parent ffff: prio 1 handle $i \
             flower skip_sw indev $NIC1 \
             src_mac e1:22:33:44:${num1}:$num2 \
             dst_mac e2:22:33:44:${num1}:$num2 \
@@ -75,7 +75,7 @@ function del_rules() {
     for i in `seq $COUNT`; do
         num1=`printf "%02x" $((i / 100))`
         num2=`printf "%02x" $((i % 100))`
-        tc filter del dev $NIC1 parent ffff: prio $i || fail "Failed to del rule $i"
+        tc filter del dev $NIC1 parent ffff: prio 1 handle $i flower || fail "Failed to del rule $i"
     done
 }
 
