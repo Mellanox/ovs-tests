6311635a12a1e78be15601d173abf2f33c92311a
Roi Dayan <roid@mellanox.com>
2018-04-16 13:10:31 +0300

    test-eswitch-add-del-different-flows.sh: Use tc prio instead of handle
    In the asap mlnx ofed 4.2 backport we got prio 1 left over when using handle for
    rules.
    
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-eswitch-add-del-different-flows.sh
+++ test-eswitch-add-del-different-flows.sh
@@ -33,7 +33,7 @@ function add_rules() {
     for i in `seq $COUNT`; do
         num1=`printf "%02x" $((i / 100))`
         num2=`printf "%02x" $((i % 100))`
-        tc_filter add dev $NIC1 protocol ip parent ffff: prio 1 handle $i \
+        tc_filter add dev $NIC1 protocol ip parent ffff: prio $i \
             flower skip_sw indev $NIC1 \
             src_mac e1:22:33:44:${num1}:$num2 \
             dst_mac e2:22:33:44:${num1}:$num2 \
@@ -46,7 +46,7 @@ function add_rules_vlan() {
     for i in `seq $COUNT`; do
         num1=`printf "%02x" $((i / 100))`
         num2=`printf "%02x" $((i % 100))`
-        tc_filter add dev $NIC1 protocol 802.1Q parent ffff: prio 1 handle $i \
+        tc_filter add dev $NIC1 protocol 802.1Q parent ffff: prio $i \
             flower skip_sw indev $NIC1 \
             src_mac e1:22:33:44:${num1}:$num2 \
             dst_mac e2:22:33:44:${num1}:$num2 \
@@ -61,7 +61,7 @@ function add_rules_vlan_drop() {
     for i in `seq $COUNT`; do
         num1=`printf "%02x" $((i / 100))`
         num2=`printf "%02x" $((i % 100))`
-        tc_filter add dev $NIC1 protocol 802.1Q parent ffff: prio 1 handle $i \
+        tc_filter add dev $NIC1 protocol 802.1Q parent ffff: prio $i \
             flower skip_sw indev $NIC1 \
             src_mac e1:22:33:44:${num1}:$num2 \
             dst_mac e2:22:33:44:${num1}:$num2 \
@@ -77,7 +77,7 @@ function del_rules() {
     for i in `seq $COUNT`; do
         num1=`printf "%02x" $((i / 100))`
         num2=`printf "%02x" $((i % 100))`
-        tc_filter del dev $NIC1 parent ffff: prio 1 handle $i flower
+        tc_filter del dev $NIC1 parent ffff: prio $i
     done
 }
 
