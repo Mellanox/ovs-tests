1f746f48c32e411999f1862f60bacc19d4ab4cfa
Roi Dayan <roid@mellanox.com>
2018-04-16 12:55:29 +0300

    test-tc-insert-rules-pedit.sh: Add pedit munge set case
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-tc-insert-rules-pedit.sh
+++ test-tc-insert-rules-pedit.sh
@@ -25,14 +25,30 @@ function test_basic_header_rewrite() {
         pipe action mirred egress redirect dev $REP
 }
 
-function test_complex_header_rewrite() {
-    title "Add complex (macs, ips, ttl) pedit rule on representor"
+function test_complex_header_rewrite_add() {
+    title "Add complex (macs, ips, ttl add) pedit rule on representor"
     # EXCEED_LIM          | 0x2EDCC3 |  alloc_modify_header_context: actions number exceeds HW limit
     reset_tc_nic $REP
     tc_filter add dev $REP protocol ip parent ffff: prio 2 \
         flower skip_sw dst_mac aa:bb:cc:dd:ee:ff ip_proto tcp ip_ttl 40/ff dst_ip 7.7.7.3 ip_flags nofrag \
         action pedit ex \
-            munge ip ttl add 0xff\
+            munge ip ttl add 0xff \
+            munge ip dst set 1.1.1.2 \
+            munge ip src set 1.1.1.2 \
+            munge eth src set aa:ba:cc:dd:ee:fe \
+            munge eth dst set aa:b7:cc:dd:ee:fe pipe \
+        action mirred egress redirect dev $NIC
+    reset_tc_nic $REP
+}
+
+function test_complex_header_rewrite_set() {
+    title "Add complex (macs, ips, ttl set) pedit rule on representor"
+    # EXCEED_LIM          | 0x2EDCC3 |  alloc_modify_header_context: actions number exceeds HW limit
+    reset_tc_nic $REP
+    tc_filter add dev $REP protocol ip parent ffff: prio 2 \
+        flower skip_sw dst_mac aa:bb:cc:dd:ee:ff ip_proto tcp ip_ttl 40/ff dst_ip 7.7.7.3 ip_flags nofrag \
+        action pedit ex \
+            munge ip ttl set 0xff \
             munge ip dst set 1.1.1.2 \
             munge ip src set 1.1.1.2 \
             munge eth src set aa:ba:cc:dd:ee:fe \
@@ -46,8 +62,10 @@ start_check_syndrome
 enable_switchdev
 
 test_basic_header_rewrite
-test_complex_header_rewrite
+test_complex_header_rewrite_add
+test_complex_header_rewrite_set
 
+title "Check log"
 check_kasan
 check_syndrome
 test_done
