a1d39d1f6f8ec7f97fca2b391d990ef5678e0b7f
Roi Dayan <roid@mellanox.com>
2018-04-23 14:33:32 +0300

    test-eswitch-add-in-mode1-del-in-mode2.sh: Fix vlan and vxlan rules
    Use prio 15 and 16 to support the tc prio patch.
    Also add missing skip_sw to make sure we fail if hw fails.
    Add missing vxlan interface to be able to offload vxlan rule.
    
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-eswitch-add-in-mode1-del-in-mode2.sh
+++ test-eswitch-add-in-mode1-del-in-mode2.sh
@@ -39,8 +39,8 @@ function add_rules() {
 
 function add_vlan_rule() {
     title "- add vlan rule"
-    tc_filter add dev $NIC1 protocol 802.1Q parent ffff: prio 111 \
-                flower \
+    tc_filter add dev $NIC1 protocol 802.1Q parent ffff: prio 15 \
+                flower skip_sw \
                         dst_mac e4:11:22:11:4a:51 \
                         src_mac e4:11:22:11:4a:50 \
                         vlan_ethtype 0x800 \
@@ -49,18 +49,41 @@ function add_vlan_rule() {
                 action drop
 }
 
+vx=vxlan1
+ip_src=20.1.11.1
+ip_dst=20.1.12.1
+
+function create_vxlan_interface() {
+    # note: we support adding decap to vxlan interface only.
+    vxlan_port=4789
+    ip link del $vx >/dev/null 2>&1
+    ip link add $vx type vxlan dstport $vxlan_port external
+    ip link set dev $vx up
+    tc qdisc add dev $vx ingress
+    ip addr flush dev $NIC
+    ip addr add $ip_src/16 dev $NIC
+    ip neigh add $ip_dst lladdr e4:11:22:11:55:55 dev $NIC
+}
+
+function clean_vxlan_interface() {
+    ip addr flush dev $NIC
+    ip link del $vx
+}
+
 function add_vxlan_rule() {
     title "- add vxlan rule"
-    tc_filter add dev ${NIC} protocol 0x806 parent ffff: prio 112 \
-                flower \
+    create_vxlan_interface
+    tc_filter add dev $vx protocol 0x806 parent ffff: prio 16 \
+                flower skip_sw \
                         dst_mac e4:11:22:11:4a:51 \
                         src_mac e4:11:22:11:4a:50 \
-                        enc_src_ip 20.1.11.1 \
-                        enc_dst_ip 20.1.12.1 \
+                        enc_src_ip $ip_src \
+                        enc_dst_ip $ip_dst \
                         enc_key_id 100 \
                         enc_dst_port 4789 \
                 action tunnel_key unset \
                 action mirred egress redirect dev ${REP}
+    clean_vxlan_interface
 }
 
 
