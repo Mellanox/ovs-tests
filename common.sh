6453d35a8795af22f5e7d23094f6c64850a9a3c1
Roi Dayan <roid@mellanox.com>
2018-05-21 07:59:22 +0300

    common.sh: Add more err matches in the log
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- common.sh
+++ common.sh
@@ -443,7 +443,7 @@ function check_for_errors_log() {
     journalctl --sync &>/dev/null || sleep 0.5
     local rc=0
     local sec=`get_test_time_elapsed`
-    local look="health compromised|firmware internal error|assert_var|DEADLOCK|possible circular locking|WARNING:|RIP:"
+    local look="health compromised|firmware internal error|assert_var|DEADLOCK|possible circular locking|WARNING:|RIP:|BUG:|refcount > 1"
     local look_ahead="Call Trace:"
     local look_ahead_count=6
 
