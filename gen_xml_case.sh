4909a4af6c36ae24712a45834170bc4ec9c80cd3
Roi Dayan <roid@mellanox.com>
2018-07-02 12:51:52 +0300

    gen_xml_case.sh: Check more lines from the head of the test
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- gen_xml_case.sh
+++ gen_xml_case.sh
@@ -4,7 +4,7 @@ match=${1:-test-*.sh}
 
 for i in `ls -1 $match`; do
     tag=`echo $i | cut -d- -f 2`
-    issues=`head $i | grep -E -o "Bug SW #[0-9]*:" | grep -o "[0-9]*"`
+    issues=`head -n40 $i | grep -E -o "Bug SW #[0-9]*:" | grep -o "[0-9]*"`
     A=""
     bugs=""
     for j in $issues ; do
