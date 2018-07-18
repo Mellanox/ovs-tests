b5a26361582398962fb16458d6b87ff3a8294e5d
Roi Dayan <roid@mellanox.com>
2018-07-18 07:15:44 +0300

    test-vf-vf-fwd.sh: wait for iperf to start and break early if possible
    Wait for iperf to start before starting round tests.
    Break the round tests if iperf process exited.
    
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-vf-vf-fwd.sh
+++ test-vf-vf-fwd.sh
@@ -92,13 +92,18 @@ title "Test ping $VF($IP1) -> $VF2($IP2)"
 ip netns exec ns0 ping -q -c 10 -i 0.2 -w 2 $IP2 && success || err "ping failed"
 
 title "Test iperf $VF($IP1) -> $VF2($IP2)"
+killall iperf3
 timeout $TIMEOUT ip netns exec ns1 iperf3 -s --one-off -i 0 || err "iperf server failed" &
 sleep 1
 timeout $TIMEOUT ip netns exec ns0 iperf3 -c $IP2 -t $((TIMEOUT-10)) -B $IP1 -P 100 --cport 6000 -i 0 || err "iperf client failed" &
+sleep 1
 
 ovs-ofctl add-flow $BR "dl_dst=11:11:11:11:11:11,actions=drop"
 
 for r in `seq $ROUNDS`; do
+    if ! pidof iperf3 $>/dev/null ; then
+        err "iperf failed"
+    fi
     if [ $TEST_FAILED == 1 ]; then
         killall iperf3
         break
