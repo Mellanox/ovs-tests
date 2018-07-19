74194afd0333bd13958f56dd2630e8ac9ce17ecd
Roi Dayan <roid@mellanox.com>
2018-07-19 15:57:40 +0300

    test-tc-vxlan-decap-not-properly-offloaded.sh: clean temp files
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-tc-vxlan-decap-not-properly-offloaded.sh
+++ test-tc-vxlan-decap-not-properly-offloaded.sh
@@ -67,6 +67,7 @@ if [ -z "$DIF" ]; then
     err "Empty diff /tmp/fsdump_after_add /tmp/fsdump_after_del"
 fi
  
+rm -fr /tmp/fsdump_before_add /tmp/fsdump_after_add /tmp/fsdump_after_del
 ip addr flush dev $NIC
 ip addr flush dev $REP
 ip link del $VXLAN
