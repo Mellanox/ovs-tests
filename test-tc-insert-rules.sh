4a706388d721827b0dd007618971965cba700eef
Roi Dayan <roid@mellanox.com>
2018-08-07 08:48:25 +0300

    test-tc-insert-rules.sh: Skip vxlan decap in skip_sw policy
    This is to avoid test failure because of a known issue.
    Bug SW #1470595: [upstream] decap rule offload attempt with skip_sw fails
    Need to revert this after a fix.
    
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-tc-insert-rules.sh
+++ test-tc-insert-rules.sh
@@ -221,6 +221,11 @@ function __test_basic_vxlan() {
                     id 100 \
                     action mirred egress redirect dev $vx
         title "    - decap"
+        if [ "$skip" = "skip_sw" ]; then
+            warn "Skip vxlan decap with skip_sw policy because of open issue"
+            warn "Bug SW #1470595: [upstream] decap rule offload attempt with skip_sw fails"
+            continue
+        fi
         tc_filter add dev $vx protocol 0x806 parent ffff: `prio` \
                     flower \
                             $skip \
