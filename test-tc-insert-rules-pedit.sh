276669b0d5070625af485a3be8962dde852fdf8c
Roi Dayan <roid@mellanox.com>
2018-08-27 16:19:21 +0300

    test-tc-insert-rules-pedit.sh: Add pedit case with less munge operations
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-tc-insert-rules-pedit.sh
+++ test-tc-insert-rules-pedit.sh
@@ -24,7 +24,21 @@ function test_basic_header_rewrite() {
         pipe action mirred egress redirect dev $REP
 }
 
-function test_complex_header_rewrite_add() {
+function test_complex_header_rewrite_add1() {
+    title "Add complex (macs, ttl add) pedit rule rep->nic"
+    # EXCEED_LIM          | 0x2EDCC3 |  alloc_modify_header_context: actions number exceeds HW limit
+    reset_tc_nic $REP
+    tc_filter add dev $REP protocol ip parent ffff: prio 2 \
+        flower skip_sw dst_mac aa:bb:cc:dd:ee:ff ip_proto tcp ip_ttl 40/ff dst_ip 7.7.7.3 \
+        action pedit ex \
+            munge ip ttl add 0xff \
+            munge eth src set aa:ba:cc:dd:ee:fe \
+            munge eth dst set aa:b7:cc:dd:ee:fe pipe \
+        action mirred egress redirect dev $NIC
+    reset_tc_nic $REP
+}
+
+function test_complex_header_rewrite_add2() {
     title "Add complex (macs, ips, ttl add) pedit rule rep->nic"
     # EXCEED_LIM          | 0x2EDCC3 |  alloc_modify_header_context: actions number exceeds HW limit
     reset_tc_nic $REP
@@ -61,7 +75,8 @@ start_check_syndrome
 enable_switchdev
 
 test_basic_header_rewrite
-test_complex_header_rewrite_add
+test_complex_header_rewrite_add1
+test_complex_header_rewrite_add2
 test_complex_header_rewrite_set
 
 title "Check log"
