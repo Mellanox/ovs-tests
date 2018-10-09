53e4edae785b73e917c07ca4ab7c879ca029b172
Vlad Buslov <vladbu@mellanox.com>
2018-10-09 18:57:00 +0300

    test-tc-par-add-del.sh: test parallel rules update
    Add new test that updates rules in parallel and verifies that parallel rule
    insert/delete is handled correctly by tc. Tests with large amount of rules
    updated in batch mode to find any potential bugs and race conditions.
    
    Change-Id: If84943e13837b63a73ba2820297e843f963e78a0
    Signed-off-by: Vlad Buslov <vladbu@mellanox.com>

---
+++ test-tc-par-add-del.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# This verifies that parallel rule insert/delete is handled correctly by tc.
+# Tests with large amount of rules updated in batch mode to find any potential
+# bugs and race conditions.
+#
+
+total=${1:-100000}
+skip=$2
+rules_per_file=10000
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+. $my_dir/tc_tests_common.sh
+
+echo "setup"
+config_sriov 2 $NIC
+enable_switchdev_if_no_rep $REP
+
+require_interfaces NIC
+reset_tc_nic $NIC
+
+function par_test() {
+    local dup=$1
+    local force=''
+    local max_rules=$total
+    if [ $dup == 1 ]; then
+        max_rules=$rules_per_file
+        echo "Generating duplicate batches"
+        force='-force'
+    else
+        echo "Generating distinct batches"
+    fi
+
+    tc_batch $dup "dev $NIC" $total $rules_per_file
+    reset_tc_nic $NIC
+
+    echo "Insert rules in parallel"
+    ls ${TC_OUT}/add.* | xargs -n 1 -P 100 tc $force -b &>/dev/null
+    check_num_rules $max_rules $NIC
+
+    echo "Delete rules in parallel"
+    ls ${TC_OUT}/del.* | xargs -n 1 -P 100 tc $force -b &>/dev/null
+    check_num_rules 0 $NIC
+}
+
+title "Test distinct handles"
+par_test 0
+
+title "Test duplicate handles"
+par_test 1
+
+test_done
