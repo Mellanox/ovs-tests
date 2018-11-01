7025e19680fdae2f6d66e6007503d7c8e76a0a92
Roi Dayan <roid@mellanox.com>
2018-11-01 18:50:51 +0200

    test-ovs-dpctl-flows-ttl-tos.sh: Fix comparing tos ttl in ipv4
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-ovs-dpctl-flows-ttl-tos.sh
+++ test-ovs-dpctl-flows-ttl-tos.sh
@@ -10,12 +10,6 @@
 my_dir="$(dirname "$0")"
 . $my_dir/common.sh
 
-VM1_IP="7.7.7.1"
-VM2_IP="7.7.7.2"
-
-local_tun="2.2.2.2"
-remote_tun="2.2.2.3"
-
 function cleanup() {
     echo "cleanup"
     start_clean_openvswitch
@@ -31,8 +25,9 @@ cleanup
 echo "setup ovs"
 ovs-vsctl add-br brv-1
 ovs-vsctl add-port brv-1 $REP
-ovs-vsctl add-port brv-1 vxlan0 -- set interface vxlan0 type=vxlan options:local_ip=$local_tun options:remote_ip=$remote_tun options:key=42 options:dst_port=4789
+ovs-vsctl add-port brv-1 $NIC
 UFID="ufid:c5f9a0b1-3399-4436-b742-30825c64a1e5"
+ifconfig $VF 1.1.1.1/24 up
 
 # XXX
 # ovs-dpctl doesn't work as it hw-offload value is not read from other_config.
@@ -70,15 +65,19 @@ function compare_keys_with_sw_flow() {
     done
 }
 
-function compare_with_sw_flow() {
+function verify_key_in_flow() {
+    local key=$1
     ovs-dpctl del-flows && sleep 0.5
     add_sw_flow $flow
-    sw2=`echo $sw | cut -d" " -f1`
-    sw2=${sw2:13}
-    if [ "$m" != "$sw2" ]; then
+    in_m=`echo $m | grep -o "$key([^)]*)"`
+    in_sw=`echo $sw | grep -o "$key([^)]*)"`
+    if [ "$in_m" != "$in_sw" ]; then
+        m=`echo $m | cut -d" " -f1`
+        sw=`echo $sw | cut -d" " -f1`
+        sw=${sw:13}
         echo flow1 $m
-        echo flow2 $sw2
-        err "Expected flows to be the same"
+        echo flow2 $sw
+        err "Expected $key() to be the same"
     fi
 }
 
@@ -96,8 +95,7 @@ function test_ttl_mask_1() {
     flow="$UFID,recirc_id(0),in_port(3),eth(src=56:52:2d:21:4d:93,dst=92:c1:04:ce:fd:51),eth_type(0x0800),ipv4(src=1.1.1.1,ttl=64/1)"
     ovs-dpctl del-flows && sleep 0.5
     add_flow $flow
-    # not expecting ttl
-    compare_keys_with_sw_flow
+    verify_key_in_flow ipv4
 }
 
 function test_ttl_mask_ff() {
@@ -105,8 +103,7 @@ function test_ttl_mask_ff() {
     flow="$UFID,recirc_id(0),in_port(3),eth(src=56:52:2d:21:4d:93,dst=92:c1:04:ce:fd:51),eth_type(0x0800),ipv4(src=1.1.1.1,ttl=64/0xff)"
     ovs-dpctl del-flows && sleep 0.5
     add_flow $flow
-    # not expecting ttl
-    compare_keys_with_sw_flow
+    verify_key_in_flow ipv4
 }
 
 function test_tos_mask_0() {
@@ -114,6 +111,7 @@ function test_tos_mask_0() {
     flow="$UFID,recirc_id(0),in_port(3),eth(src=56:52:2d:21:4d:93,dst=92:c1:04:ce:fd:51),eth_type(0x0800),ipv4(src=1.1.1.1,tos=0x1/0,ttl=0/0)"
     ovs-dpctl del-flows && sleep 0.5
     add_flow $flow
+    # not expecting tos
     compare_keys_with_sw_flow
 }
 
@@ -122,7 +120,7 @@ function test_tos_mask_1() {
     flow="$UFID,recirc_id(0),in_port(3),eth(src=56:52:2d:21:4d:93,dst=92:c1:04:ce:fd:51),eth_type(0x0800),ipv4(src=1.1.1.1,tos=0x2/1,ttl=0/0)"
     ovs-dpctl del-flows && sleep 0.5
     add_flow $flow
-    compare_keys_with_sw_flow
+    verify_key_in_flow ipv4
 }
 
 function test_tos_mask_ff() {
@@ -130,7 +128,7 @@ function test_tos_mask_ff() {
     flow="$UFID,recirc_id(0),in_port(3),eth(src=56:52:2d:21:4d:93,dst=92:c1:04:ce:fd:51),eth_type(0x0800),ipv4(src=1.1.1.1,tos=0xff/0xff,ttl=0/0)"
     ovs-dpctl del-flows && sleep 0.5
     add_flow $flow
-    compare_keys_with_sw_flow
+    verify_key_in_flow ipv4
 }
 
 
@@ -144,6 +142,7 @@ test_tos_mask_0
 test_tos_mask_1
 test_tos_mask_ff
 
+ifconfig $VF 0
 check_syndrome
 cleanup
 test_done
