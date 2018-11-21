879b8f0801ea2166939df52128a431fb8d84de8e
Roi Dayan <roid@mellanox.com>
2018-11-21 14:46:34 +0200

    New ovs ct test bringing down-up the representor
    Signed-off-by: Roi Dayan <roid@mellanox.com>

---
+++ test-ovs-ct-scapy-udp-down-up.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+#
+# Test bring representor interface down and up during CT traffic.
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+pktgen=$my_dir/scapy-traffic-tester.py
+
+require_module act_ct
+
+IP1="7.7.7.1"
+IP2="7.7.7.2"
+
+enable_switchdev_if_no_rep $REP
+require_interfaces REP REP2
+unbind_vfs
+bind_vfs
+reset_tc $REP
+reset_tc $REP2
+
+function cleanup() {
+    ip netns del ns0 2> /dev/null
+    ip netns del ns1 2> /dev/null
+    reset_tc $REP &>/dev/null
+    reset_tc $REP2 &>/dev/null
+}
+trap cleanup EXIT
+
+function config_vf() {
+    local ns=$1
+    local vf=$2
+    local rep=$3
+    local ip=$4
+
+    echo "[$ns] $vf ($ip) -> $rep"
+    ifconfig $rep 0 up
+    ip netns add $ns
+    ip link set $vf netns $ns
+    ip netns exec $ns ifconfig $vf $ip/24 up
+}
+
+function config_ovs() {
+    local proto=$1
+
+    echo "setup ovs"
+    start_clean_openvswitch
+    ovs-vsctl add-br br-ovs
+    ovs-vsctl add-port br-ovs $REP
+    ovs-vsctl add-port br-ovs $REP2
+
+    ovs-ofctl add-flow br-ovs in_port=$REP,dl_type=0x0806,actions=output:$REP2
+    ovs-ofctl add-flow br-ovs in_port=$REP2,dl_type=0x0806,actions=output:$REP
+
+    ovs-ofctl add-flow br-ovs "table=0, $proto,ct_state=-trk actions=ct(table=1)"
+    ovs-ofctl add-flow br-ovs "table=1, $proto,ct_state=+trk+new actions=ct(commit),normal"
+    ovs-ofctl add-flow br-ovs "table=1, $proto,ct_state=+trk+est actions=normal"
+
+    ovs-ofctl dump-flows br-ovs
+}
+
+function run() {
+    title "Test OVS CT UDP"
+    config_vf ns0 $VF $REP $IP1
+    config_vf ns1 $VF2 $REP2 $IP2
+
+    proto="udp"
+    config_ovs $proto
+
+    t=4
+    echo "run traffic for $t seconds"
+    ip netns exec ns1 $pktgen -l -i $VF2 --src-ip $IP1 --time $((t+10)) &
+    pk1=$!
+    sleep 1
+    ip netns exec ns0 $pktgen -i $VF1 --src-ip $IP1 --dst-ip $IP2 --time $t --src-port-count 30 &
+    pk2=$!
+    sleep 1
+
+    for i in `seq 3`; do
+        ifconfig $REP down
+        ifconfig $REP up
+        sleep 1
+    done
+
+    sleep $((t+7))
+    kill $pk1 &>/dev/null
+    wait $pk1 $pk2 2>/dev/null
+
+    ovs-vsctl del-br br-ovs
+}
+
+
+run
+test_done
