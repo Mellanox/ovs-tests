1fa6fb5287ba1617a120e9c0af5104c6484fbbd1
Roi Dayan <roid@mellanox.com>
2018-12-13 18:43:32 +0200

    Move tc duplicate vlan and vxlan cases
    From test-tc-insert-rules.sh to test-tc-insert-rules-duplicates.sh.
    
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-tc-insert-rules-duplicates.sh
+++ test-tc-insert-rules-duplicates.sh
@@ -56,16 +56,78 @@ function test_basic_L4() {
             action drop
 }
 
+function test_duplicate_vlan() {
+    tc_filter add dev $REP protocol 802.1Q parent ffff: prio 11 \
+                flower \
+                        $skip \
+                        dst_mac e4:11:22:11:4a:51 \
+                        src_mac e4:11:22:11:4a:50 \
+                        action vlan push id 100 \
+                        action mirred egress redirect dev $NIC
+}
+
+function config_vxlan() {
+    vx=vxlan_dup_test
+    vxlan_port=4789
+    ip_dst=20.1.11.1
+    ip_src=20.1.12.1
+
+    ip link del $vx >/dev/null 2>&1
+    ip link add $vx type vxlan dev $NIC dstport $vxlan_port external
+    [ $? -ne 0 ] && err "Failed to create vxlan interface" && return 1
+    ip link set dev $vx up
+    tc qdisc add dev $vx ingress
+
+    ip addr flush dev $NIC
+    ip addr add $ip_src/16 dev $NIC
+    ifconfig $NIC up
+    ip neigh add $ip_dst lladdr e4:11:22:11:55:55 dev $NIC
+}
+
+function clean_vxlan() {
+    ip addr flush dev $NIC
+    ip link del $vx
+}
+
+__test_vxlan=0
+function test_duplicate_vxlan() {
+    if [ $__test_vxlan -eq 0 ]; then
+        config_vxlan || return 1
+    fi
+    let __test_vxlan+=1
+
+    tc_filter add dev $REP protocol 0x806 parent ffff: prio 1 \
+                flower \
+                        $skip \
+                        dst_mac e4:11:22:11:4a:51 \
+                        src_mac e4:11:22:11:4a:50 \
+                action tunnel_key set \
+                src_ip $ip_src \
+                dst_ip $ip_dst \
+                id 100 \
+                dst_port 4789 \
+                action mirred egress redirect dev $vx
+    local rc=$?
+
+    if [ $__test_vxlan -eq 2 ]; then
+        clean_vxlan
+    fi
+
+    return $rc
+}
+
 
 enable_switchdev
 
 # Execute all test_* functions
 for i in `declare -F | awk {'print $3'} | grep ^test_ | grep -v test_done` ; do
     title $i
-    eval $i && success || err "Adding rule"
-    title "- test duplicate rule fails"
+    reset_tc_nic $NIC
+    reset_tc_nic $REP
+    eval $i && success || err "Failed adding rule"
     eval $i 2>/dev/null && err "Expected to fail adding duplicate rule" || success
-    reset_tc_nic $nic
+    reset_tc_nic $NIC
+    reset_tc_nic $REP
 done
 
 check_kasan
