7852c00d34da08c1f282a7035cd8e975bc3c6641
Avihu Hagag <avihuh@mellanox.com>
2018-12-16 18:19:57 +0000

    test-ovs-vxlan-in-ns.sh: Add vxlan with dst port different than default
    Change-Id: I5aad8a5b20901c7656188966b9cf0c43acd1408d
    Signed-off-by: Avihu Hagag <avihuh@mellanox.com>

--- test-ovs-vxlan-in-ns.sh
+++ test-ovs-vxlan-in-ns.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Test ovs with vxlan rules default and non default ports
+#
+# Bug SW #1465595: [OPENVSWITCH] - ovs-dpctl dump-flows command failed when using non default vxlan port on upstream 4.18 rc2
 
 my_dir="$(dirname "$0")"
 . $my_dir/common.sh
@@ -24,28 +28,6 @@ function cleanup() {
 
 cleanup
 
-echo "setup veth and ns"
-ip link add veth0 type veth peer name veth1
-ip link add veth2 type veth peer name veth3
-
-ifconfig veth0 $VM1_IP/24 up
-ifconfig veth1 up
-ifconfig veth2 up
-
-ip netns add ns0
-ip link set veth3 netns ns0
-ip netns exec ns0 ifconfig veth3 $remote_tun/24 up
-
-ip netns exec ns0 ip link add name vxlan42 type vxlan id 42 dev veth3 remote $local_tun dstport 4789
-ip netns exec ns0 ifconfig vxlan42 $VM2_IP/24 up
-
-echo "setup ovs"
-ovs-vsctl add-br brv-1
-ovs-vsctl add-port brv-1 veth1
-ovs-vsctl add-port brv-1 vxlan0 -- set interface vxlan0 type=vxlan options:local_ip=$local_tun options:remote_ip=$remote_tun options:key=42 options:dst_port=4789
-
-ifconfig veth2 $local_tun/24 up
-
 function check_offloaded_rules() {
     local count=$1
     title " - check for $count offloaded rules"
@@ -55,10 +37,41 @@ function check_offloaded_rules() {
     if (( RES == $count )); then success; else err; fi
 }
 
-title "Test ping $VM1_IP -> $VM2_IP"
-ping -q -c 10 -i 0.2 -w 4 $VM2_IP && success || err
+function configure_vxlan() {
+    local vxlan_port=$1
 
-check_offloaded_rules 2
+    title "Test vxlan with port $vxlan_port"
 
-cleanup
+    echo "setup veth and ns"
+    ip link add veth0 type veth peer name veth1
+    ip link add veth2 type veth peer name veth3
+
+    ifconfig veth0 $VM1_IP/24 up
+    ifconfig veth1 up
+    ifconfig veth2 up
+
+    ip netns add ns0
+    ip link set veth3 netns ns0
+    ip netns exec ns0 ifconfig veth3 $remote_tun/24 up
+
+    ip netns exec ns0 ip link add name vxlan42 type vxlan id 42 dev veth3 remote $local_tun dstport $vxlan_port
+    ip netns exec ns0 ifconfig vxlan42 $VM2_IP/24 up
+
+    echo "setup ovs dst_port:$vxlan_port"
+    ovs-vsctl add-br brv-1
+    ovs-vsctl add-port brv-1 veth1
+    ovs-vsctl add-port brv-1 vxlan0 -- set interface vxlan0 type=vxlan options:local_ip=$local_tun options:remote_ip=$remote_tun options:key=42 options:dst_port=$vxlan_port
+
+    ifconfig veth2 $local_tun/24 up
+
+    echo "Test ping $VM1_IP -> $VM2_IP"
+    ping -q -c 10 -i 0.2 -w 4 $VM2_IP && success || err
+
+    check_offloaded_rules 2
+    cleanup
+}
+
+configure_vxlan 4789
+configure_vxlan 4000
+
 test_done
