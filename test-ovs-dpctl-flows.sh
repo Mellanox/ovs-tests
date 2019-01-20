582259b9fcc4c8b8d2c80d3b9c769d07fb4db790
Roi Dayan <roid@mellanox.com>
2019-01-20 14:23:50 +0200

    test-ovs-dpctl-flows.sh: Fix grep and do it uniq per case
    Change-Id: I376d239bceba43a87b51926205b4c035e9eaa477
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-ovs-dpctl-flows.sh
+++ test-ovs-dpctl-flows.sh
@@ -30,13 +30,13 @@ ovs-vsctl add-port brv-1 $REP
 ovs-vsctl add-port brv-1 $REP2
 UFID="ufid:c5f9a0b1-3399-4436-b742-30825c64a1e5"
 
-ipv4=1.1.1.1
-ipv6=2002:db8:0:f101::55
+ipv4="1.1.1.1"
+ipv6="2002:db8:0:f101::55"
 
 function add_flow() {
     local g=$1
-    m=`ovs-appctl dpctl/add-flow $flow 2 ; ovs_dpctl_dump_flows | grep -m1 $g*`
-    [ -z "$m" ] && m=`ovs-appctl dpctl/add-flow $flow 2 ; ovs_dpctl_dump_flows | grep -m1 $g*`
+    m=`ovs-appctl dpctl/add-flow $flow 2 ; ovs_dpctl_dump_flows | grep -m1 $g`
+    [ -z "$m" ] && m=`ovs-appctl dpctl/add-flow $flow 2 ; ovs_dpctl_dump_flows | grep -m1 $g`
     if [ -z "$m" ]; then
         err "Failed to add test flow: $flow"
         return 1
@@ -45,9 +45,9 @@ function add_flow() {
 }
 
 function add_sw_flow() {
-    local ip=$1
-    sw=`ovs-dpctl add-flow $flow 2 ; ovs-dpctl dump-flows | grep recirc | grep -m1 $ip*`
-    [ -z "$sw" ] && sw=`ovs-dpctl add-flow $flow 2 ; ovs-dpctl dump-flows | grep recirc | grep -m1 $ip*`
+    local g=$1
+    sw=`ovs-dpctl add-flow $flow 2 ; ovs-dpctl dump-flows | grep recirc | grep -m1 $g`
+    [ -z "$sw" ] && sw=`ovs-dpctl add-flow $flow 2 ; ovs-dpctl dump-flows | grep recirc | grep -m1 $g`
     if [ -z "$sw" ]; then
         err "Failed to add sw flow: $flow"
         return 1
@@ -56,11 +56,11 @@ function add_sw_flow() {
 }
 
 function verify_keys_in_flow() {
-    local ip=$1
+    local g=$1
     local keys="${@:2}"
     local key
     ovs-dpctl del-flows && sleep 0.5
-    add_sw_flow $ip
+    add_sw_flow $g
     for key in $keys; do
         in_m=`echo $m | grep -o "$key([^)]*)"`
         in_sw=`echo $sw | grep -o "$key([^)]*)"`
@@ -77,34 +77,34 @@ function verify_keys_in_flow() {
 
 function test_appctl_rule_tcp() {
     title 'Test appctl rule TCP'
-    flow="$UFID,recirc_id(0),in_port(3),eth(src=56:52:2d:21:4d:93/FF:FF:FF:FF:FF:FF,dst=92:c1:04:ce:fd:51/FF:FF:FF:FF:FF:FF),eth_type(0x0800),ipv4(src=$ipv4/255.255.255.0,dst=2.2.2.2/255.255.255.0,proto=0x6),tcp(src=8080/0xff00,dst=8080/0xff),tcp_flags(0xff/0xff)"
+    flow="$UFID,recirc_id(0),in_port(3),eth(src=56:52:2d:21:66:66/FF:FF:FF:FF:FF:FF,dst=92:c1:04:ce:fd:51/FF:FF:FF:FF:FF:FF),eth_type(0x0800),ipv4(src=$ipv4/255.255.255.0,dst=2.2.2.2/255.255.255.0,proto=0x6),tcp(src=8080/0xff00,dst=8080/0xff),tcp_flags(0xff/0xff)"
     ovs-dpctl del-flows && sleep 0.5
-    add_flow $ipv4 || return 1
-    verify_keys_in_flow $ipv4 eth eth_type ipv4 tcp tcp_flags
+    add_flow 66:66 || return 1
+    verify_keys_in_flow 66:66 eth eth_type ipv4 tcp tcp_flags
 }
 
 function test_appctl_rule_udp() {
     title 'Test appctl rule UDP'
-    flow="$UFID,recirc_id(0),in_port(3),eth(src=56:52:2d:21:4d:93/FF:FF:FF:FF:FF:FF,dst=92:c1:04:ce:fd:51/FF:FF:FF:FF:FF:FF),eth_type(0x0800),ipv4(src=$ipv4/255.255.255.0,dst=2.2.2.2/255.255.255.0,proto=0x11),udp(src=80/0xff00,dst=443/0xff)"
+    flow="$UFID,recirc_id(0),in_port(3),eth(src=56:52:2d:21:17:17/FF:FF:FF:FF:FF:FF,dst=92:c1:04:ce:fd:51/FF:FF:FF:FF:FF:FF),eth_type(0x0800),ipv4(src=$ipv4/255.255.255.0,dst=2.2.2.2/255.255.255.0,proto=0x11),udp(src=80/0xff00,dst=443/0xff)"
     ovs-dpctl del-flows && sleep 0.5
-    add_flow $ipv4 || return 1
-    verify_keys_in_flow $ipv4 eth eth_type ipv4 udp
+    add_flow 17:17 || return 1
+    verify_keys_in_flow 17:17 eth eth_type ipv4 udp
 }
 
 function test_appctl_rule_tcp_ipv6() {
     title 'Test appctl rule TCP ipv6'
-    flow="$UFID,recirc_id(0),in_port(3),eth(src=56:52:2d:21:4d:93/FF:FF:FF:FF:FF:FF,dst=92:c1:04:ce:fd:51/FF:FF:FF:FF:FF:FF),eth_type(0x86DD),ipv6(src=$ipv6,dst=2002:db8:0:f101::1,proto=0x6),tcp(src=8080/0xff00,dst=8080/0xff),tcp_flags(0xff/0xff)"
+    flow="$UFID,recirc_id(0),in_port(3),eth(src=56:52:2d:21:18:18/FF:FF:FF:FF:FF:FF,dst=92:c1:04:ce:fd:51/FF:FF:FF:FF:FF:FF),eth_type(0x86DD),ipv6(src=$ipv6,dst=2002:db8:0:f101::1,proto=0x6),tcp(src=8080/0xff00,dst=8080/0xff),tcp_flags(0xff/0xff)"
     ovs-dpctl del-flows && sleep 0.5
-    add_flow $ipv6 || return 1
-    verify_keys_in_flow $ipv6 eth eth_type ipv6 tcp tcp_flags
+    add_flow 18:18 || return 1
+    verify_keys_in_flow 18:18 eth eth_type ipv6 tcp tcp_flags
 }
 
 function test_appctl_rule_udp_ipv6() {
     title 'Test appctl rule UDP ipv6'
-    flow="$UFID,recirc_id(0),in_port(3),eth(src=56:52:2d:21:4d:93/FF:FF:FF:FF:FF:FF,dst=92:c1:04:ce:fd:51/FF:FF:FF:FF:FF:FF),eth_type(0x86DD),ipv6(src=$ipv6,dst=2002:db8:0:f101::1,proto=0x11),udp(src=80/0xff00,dst=443/0xff)"
+    flow="$UFID,recirc_id(0),in_port(3),eth(src=56:52:2d:21:19:19/FF:FF:FF:FF:FF:FF,dst=92:c1:04:ce:fd:51/FF:FF:FF:FF:FF:FF),eth_type(0x86DD),ipv6(src=$ipv6,dst=2002:db8:0:f101::1,proto=0x11),udp(src=80/0xff00,dst=443/0xff)"
     ovs-dpctl del-flows && sleep 0.5
-    add_flow $ipv4 || return 1
-    verify_keys_in_flow $ipv6 eth eth_type ipv6 udp
+    add_flow 19:19 || return 1
+    verify_keys_in_flow 19:19 eth eth_type ipv6 udp
 }
 
 start_check_syndrome
