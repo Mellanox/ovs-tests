3260d94bdbfb385f3993e6d79a018b26881c02e6
Vlad Buslov <vladbu@mellanox.com>
2019-01-21 15:25:18 +0200

    test-tc-rule-act-add.sh: measure memory consumption
    Extend test to also measure memory consumption per-rule when benchmarking
    insertion rate.
    
    Change-Id: Icad35a23e49a02295b91960ef578e292a3a744c4
    Signed-off-by: Vlad Buslov <vladbu@mellanox.com>

--- test-tc-perf-update.sh
+++ test-tc-perf-update.sh
@@ -22,23 +22,75 @@ reset_tc_nic $NIC
 
 function run_multi_tc() {
     local file_name=$1
-    local num_rules=$2
-    local baseline=$3
 
     # Spawn tc instance per batch file and measure execution time. Save 'time' output to out
     out=$( { time -p ls ${TC_OUT}/${file_name}.* | xargs -n 1 -P 100 tc -b; } 2>&1 1>/dev/null )
     # Extract 'real' time
     [[ $out =~ [^0-9]*([0-9]+\.[0-9]+) ]]
     real_time="${BASH_REMATCH[1]}"
-    # Calculate absolute difference between baseline and this test run (per cent).
-    read difference <<< $(awk -v t1="$real_time" -v t2="$baseline" 'BEGIN{diff=(t2-t1)/t1 * 100;abs=diff<0?-diff:diff; printf "%.0f", abs}')
 
-    if [[ -z $baseline ]]; then
-        err "Please set baseline in config file. Command executed in $real_time sec"
-    elif ((difference>10)); then
-        err "Command execution took $real_time sec, baseline is $baseline sec ($difference % difference)"
+    echo $real_time
+}
+
+function calc_abs_diff_per_cent() {
+    local baseline_res=$1
+    local new_res=$2
+
+    # Calculate absolute difference between baseline time and this test run (per cent).
+    read difference <<< $(awk -v v1="$new_res" -v v2="$baseline_res" 'BEGIN{diff=(v2-v1)/v1 * 100;abs=diff<0?-diff:diff; printf "%.0f", abs}')
+
+    echo $difference
+}
+
+function run_benchmark_time() {
+    local file_name=$1
+    local num_rules=$2
+    local baseline_time=$3
+
+    real_time=$(run_multi_tc $file_name)
+    difference_time=$(calc_abs_diff_per_cent $baseline_time $real_time)
+
+    if [[ -z $baseline_time ]]; then
+        err "Please set baseline time in config file. Command executed in $real_time sec"
+    elif ((difference_time>10)); then
+        err "Command execution took $real_time sec, baseline time is $baseline_time sec ($difference_time % difference)"
     else
-        success "Command execution took $real_time sec, baseline is $baseline sec ($difference % difference)"
+        success "Command execution took $real_time sec, baseline time is $baseline_time sec ($difference_time % difference)"
+    fi
+
+    check_num_rules $num_rules $NIC
+}
+
+function calc_used_mem() {
+    vmstat -s | grep -i "used memory" | awk {'print $1'}
+}
+
+function run_benchmark_time_mem() {
+    local file_name=$1
+    local num_rules=$2
+    local baseline_time=$3
+    local baseline_mem=$4
+
+    local used_mem_start=$(calc_used_mem)
+
+    real_time=$(run_multi_tc $file_name)
+    difference_time=$(calc_abs_diff_per_cent $baseline_time $real_time)
+
+    local used_mem_end=$(calc_used_mem)
+    (( total_mem = used_mem_end - used_mem_start ))
+    local difference_mem=$(calc_abs_diff_per_cent $baseline_mem $total_mem)
+
+    if [[ -z $baseline_time ]]; then
+        err "Please set baseline time in config file. Command executed in $real_time sec, used $total_mem K memory"
+    elif [[ -z $baseline_mem ]]; then
+        err "Please set baseline memory consumption in config file. Command used $total_mem K memory"
+    elif ((difference_time>10)); then
+        err "Command execution took $real_time sec, baseline time is $baseline_time sec ($difference_time % difference)"
+    elif ((difference_mem>10)); then
+        (( mem_per_rule = total_mem / num_rules))
+        err "Command execution used $total_mem K memory ($mem_per_rule K per rule), baseline memory is $baseline_mem K ($difference_mem % difference)"
+    else
+        success "Command execution took $real_time sec, baseline time is $baseline_time sec ($difference_time % difference), used $total_mem K memory ($difference_mem % difference)"
     fi
 
     check_num_rules $num_rules $NIC
@@ -49,21 +101,22 @@ function perf_test() {
     local classifier=$2
     local baseline_ins=$3
     local baseline_del=$4
+    local baseline_mem=$5
 
     tc_batch 0 "dev $NIC" $total $rules_per_file "$classifier"
-    reset_tc_nic $NIC $classifier
+    reset_tc_nic $NIC
 
     echo "Insert rules"
-    run_multi_tc add $total $baseline_ins
+    run_benchmark_time_mem add $total $baseline_ins $baseline_mem
 
     echo "Delete rules"
-    run_multi_tc del 0 $baseline_del
+    run_benchmark_time del 0 $baseline_del
 }
 
 title "Test $num_tc tc instance(s) with $total L2 rules"
-perf_test $per_file "" $PERF_TC_L2_INSERT $PERF_TC_L2_DELETE
+perf_test $per_file "" $PERF_TC_L2_INSERT $PERF_TC_L2_DELETE $PERF_TC_L2_MEMORY
 
 title "Test $num_tc tc instance(s) with $total L2+5tuple rules"
-perf_test $per_file "src_ip 192.168.111.1 dst_ip 192.168.111.2 ip_proto udp dst_port 1 src_port 1" $PERF_TC_5T_INSERT $PERF_TC_5T_DELETE
+perf_test $per_file "src_ip 192.168.111.1 dst_ip 192.168.111.2 ip_proto udp dst_port 1 src_port 1" $PERF_TC_5T_INSERT $PERF_TC_5T_DELETE $PERF_TC_5T_MEMORY
 
 test_done
