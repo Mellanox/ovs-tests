b2939b631344e3fc5681e29059bfb470bcbd1ec0
Roi Dayan <roid@mellanox.com>
2019-01-22 18:18:22 +0200

    Add new test test-tc-insert-fail-hw.sh
    Change-Id: Icb0828a53c312c6284e3d10f3b3d0ff0256b30d5
    Signed-off-by: Roi Dayan <roid@mellanox.com>

---
+++ test-tc-insert-fail-hw.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#
+# This test reproduce an issue we add rule to rhashtable but on error flow didnt
+# clean it.
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+require_module act_simple
+
+enable_switchdev_if_no_rep $REP
+bind_vfs
+
+function cleanup() {
+    ip n d 1.1.1.2 dev $VF lladdr aa:bb:cc:dd:ee:ff &>/dev/null
+    reset_tc $REP
+    killall iperf &>/dev/null
+    killall iperf &>/dev/null
+    wait &>/dev/null
+}
+
+function test_tc_filter() {
+    local a
+    local err
+    local opnotsupp
+
+    a=`eval tc filter $@ 2>&1`
+    err=$?
+    [ -n "$a" ] && echo $a
+
+    echo "$a" | grep -q "Operation not supported" && true || false
+    opnotsupp=$?
+
+    if [ $opnotsupp -ne 0 ]; then
+        fail "Expected operation not supported error"
+    fi
+}
+
+function test_insert_hw_fail_exists() {
+    for i in 1 2 ; do
+        test_tc_filter add dev $REP protocol ip parent ffff: prio 1 \
+            flower skip_sw dst_mac aa:bb:cc:dd:ee:ff \
+            action simple sdata '"unsupported action"'
+    done
+}
+
+function test_insert_hw_fail_during_traffic() {
+    title "add 10 rule to prio 1 with skip_sw that will fail on action in hw (but will match traffic in classify), during traffic"
+    timeout 6 iperf -c 1.1.1.2 -i 1 -t 5 -u -l 64 -b 1G -P 10 &>/dev/null &
+
+    printf 'working...  '
+    for i in `seq 10`; do
+        tc filter add dev $REP protocol ip parent ffff: prio 1 \
+            flower skip_sw dst_mac aa:bb:cc:dd:ee:ff \
+            action simple sdata '"unsupported action"' &>/dev/null && fail "expected to fail"
+    done
+
+    wait
+}
+
+
+trap cleanup EXIT
+
+ifconfig $VF 1.1.1.1/24 up
+ip n r 1.1.1.2 dev $VF lladdr aa:bb:cc:dd:ee:ff
+
+title "add rule to create flower instance in prio 1 (won't match)"
+reset_tc_nic $REP
+tc_filter add dev $REP protocol ip parent ffff: prio 1 flower skip_hw dst_mac cc:cc:cc:cc:cc:cc action drop
+
+test_insert_hw_fail_exists
+test_insert_hw_fail_during_traffic
+
+test_done
