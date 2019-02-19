ff941c4a0777ba2bbf4b8956a63b4004387461ef
Dmytro Linkin <dmitrolin@mellanox.com>
2019-02-19 16:55:15 +0000

    test-tc-insert-rules-pedit-ttl-ip.sh: Added ipv6 check
    Change-Id: I1352d79c6df539754a008881ceebb8b69af0a024
    Signed-off-by: Dmytro Linkin <dmitrolin@mellanox.com>

--- test-tc-insert-rules-pedit-ttl-ip.sh
+++ test-tc-insert-rules-pedit-ttl-ip.sh
@@ -8,7 +8,7 @@
 
 SRC_ADDR='7.7.7.1'
 DST_ADDR='7.7.7.2'
-
+SRC_ADDR6='fe84::964e:1234:51b6:90f1'
 
 my_dir="$(dirname "$0")"
 . $my_dir/common.sh
@@ -40,7 +40,7 @@ function tc_filter_failure() {
 }
 
 function test_tcp_rewrite() {
-    title "Test header rewrite for TCP proto"
+    title "Test IPv4 header rewrite for TCP proto"
 
     title "- ttl field only"
     tc_filter_success add dev $REP protocol ip parent ffff: prio 1 \
@@ -56,7 +56,6 @@ function test_tcp_rewrite() {
         action pedit ex \
             munge ip ttl add 0xff \
             munge ip src set $SRC_ADDR \
-            munge ip dst set $DST_ADDR \
         pipe action mirred egress redirect dev $REP
     reset_tc $REP
 
@@ -68,10 +67,41 @@ function test_tcp_rewrite() {
             munge ip dst set $DST_ADDR \
         pipe action mirred egress redirect dev $REP
     reset_tc $REP
+
+}
+
+function test_tcp_rewrite_ipv6() {
+    title "Test IPv6 header rewrite for TCP proto"
+
+    title "- hoplimit field only"
+    tc_filter_success add dev $REP protocol ipv6 parent ffff: prio 1 \
+        flower skip_sw ip_proto tcp \
+        action pedit ex \
+            munge ip6 hoplimit add 0xff \
+        pipe action mirred egress redirect dev $REP
+    reset_tc $REP
+
+    title "- hoplimit & addr fields"
+    tc_filter_success add dev $REP protocol ipv6 parent ffff: prio 1 \
+        flower skip_sw ip_proto tcp \
+        action pedit ex \
+            munge ip6 hoplimit add 0xff \
+            munge ip6 src set $SRC_ADDR6 \
+        pipe action mirred egress redirect dev $REP
+    reset_tc $REP
+
+    title "- addr field only"
+    tc_filter_success add dev $REP protocol ipv6 parent ffff: prio 1 \
+        flower skip_sw ip_proto tcp \
+        action pedit ex \
+            munge ip6 src set $SRC_ADDR6 \
+        pipe action mirred egress redirect dev $REP
+    reset_tc $REP
+
 }
 
 function test_sctp_rewrite() {
-    title "Test header rewrite for SCTP proto"
+    title "Test IPv4 header rewrite for SCTP proto"
 
     title "- ttl field only"
     tc_filter_success add dev $REP protocol ip parent ffff: prio 1 \
@@ -81,22 +111,51 @@ function test_sctp_rewrite() {
         pipe action mirred egress redirect dev $REP
     reset_tc $REP
 
-    title "- ttl & addr fields (must be RTNETLINK error message)"
+    title "- ttl & addr fields (must be error message)"
     tc_filter_failure add dev $REP protocol ip parent ffff: prio 1 \
         flower skip_sw ip_proto sctp \
         action pedit ex \
             munge ip ttl add 0xff \
             munge ip src set $SRC_ADDR \
-            munge ip dst set $DST_ADDR \
         pipe action mirred egress redirect dev $REP
     reset_tc $REP
 
-    title "- addr fields only (must be RTNETLINK error message)"
+    title "- addr fields only (must be error message)"
     tc_filter_failure add dev $REP protocol ip parent ffff: prio 1 \
         flower skip_sw ip_proto sctp \
         action pedit ex \
             munge ip src set $SRC_ADDR \
             munge ip dst set $DST_ADDR \
+        pipe action mirred egress redirect dev $REP
+    reset_tc $REP
+
+}
+
+function test_sctp_rewrite_ipv6() {
+    title "Test IPv6 header rewrite for SCTP proto"
+
+    title "- hoplimit field only"
+    tc_filter_success add dev $REP protocol ipv6 parent ffff: prio 1 \
+        flower skip_sw ip_proto sctp \
+        action pedit ex \
+            munge ip6 hoplimit add 0xff \
+        pipe action mirred egress redirect dev $REP
+    reset_tc $REP
+
+    title "- hoplimit & addr fields (must be error message)"
+    tc_filter_failure add dev $REP protocol ipv6 parent ffff: prio 1 \
+        flower skip_sw ip_proto sctp \
+        action pedit ex \
+            munge ip6 hoplimit add 0xff \
+            munge ip6 src set $SRC_ADDR6 \
+        pipe action mirred egress redirect dev $REP
+    reset_tc $REP
+
+    title "- addr field only (must be error message)"
+    tc_filter_failure add dev $REP protocol ipv6 parent ffff: prio 1 \
+        flower skip_sw ip_proto sctp \
+        action pedit ex \
+            munge ip6 src set $SRC_ADDR6 \
         pipe action mirred egress redirect dev $REP
     reset_tc $REP
 
@@ -107,7 +166,9 @@ enable_switchdev
 reset_tc $REP
 
 test_tcp_rewrite
+test_tcp_rewrite_ipv6
 test_sctp_rewrite
+test_sctp_rewrite_ipv6
 
 title "Check log"
 check_syndrome
