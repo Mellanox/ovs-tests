512c49d7694343eda43bce113831041e2b641594
Eli Britstein <elibr@mellanox.com>
2019-03-17 06:05:29 +0000

    Add test: test-tc-vlan-modify.sh
    Issue: 1695125
    Change-Id: I19388218c338521b9be24f9fcba2c275ef11c525
    Signed-off-by: Eli Britstein <elibr@mellanox.com>

---
+++ test-tc-vlan-modify.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Task #1695125: Upstream 5.2: VLAN header rewrite
+#
+# Modify vlan id by tc-modify and pop+push
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+require_interfaces REP NIC
+
+function run() {
+    reset_tc_nic $REP
+
+    title "Add TC mirred rule for VLAN modify 10->11"
+    tc_filter add dev $REP protocol 802.1q ingress prio 1 flower skip_sw \
+        dst_mac e4:11:22:33:44:70 \
+        vlan_id 10 \
+        action vlan modify id 11 pipe \
+        action mirred egress redirect dev $NIC
+    reset_tc_nic $REP
+
+    title "Add TC mirred rule for VLAN pop+push 11"
+    tc_filter add dev $REP protocol 802.1q ingress prio 1 flower skip_sw \
+        dst_mac e4:11:22:33:44:70 \
+        vlan_id 10 \
+        action vlan pop pipe action vlan push id 11 pipe \
+        action mirred egress redirect dev $NIC
+    reset_tc_nic $REP
+}
+
+run
+test_done
