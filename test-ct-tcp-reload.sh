d0bb5cdc617a61f31d3077885b61be248a81b601
Roi Dayan <roid@mellanox.com>
2019-03-24 14:33:43 +0200

    Add new ct test test-ct-tcp-reload.sh
    Change-Id: I1e22954a9ac37dd01c7e5a0cebbfb7f432446e5d
    Signed-off-by: Roi Dayan <roid@mellanox.com>

---
+++ test-ct-tcp-reload.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+#
+# Test CT fwd with tcp traffic and reload the driver.
+#
+# This reproduces an issue with remaining object the fc cache.
+# 507138.876287] BUG mlx5_fc_cache (Tainted: G    B      OE    ): Objects
+# remaining in mlx5_fc_cache on __kmem_cache_shutdown()
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+require_module act_ct
+echo 1 > /proc/sys/net/netfilter/nf_conntrack_tcp_be_liberal
+
+IP1="7.7.7.1"
+IP2="7.7.7.2"
+
+enable_switchdev_if_no_rep $REP
+require_interfaces REP REP2
+unbind_vfs
+bind_vfs
+reset_tc $REP
+reset_tc $REP2
+
+mac1=`cat /sys/class/net/$VF/address`
+mac2=`cat /sys/class/net/$VF2/address`
+
+test "$mac1" || fail "no mac1"
+test "$mac2" || fail "no mac2"
+
+function cleanup() {
+    ip netns del ns0 2> /dev/null
+    ip netns del ns1 2> /dev/null
+    reset_tc $REP
+    reset_tc $REP2
+}
+trap cleanup EXIT
+
+function config_vf() {
+    local ns=$1
+    local vf=$2
+    local rep=$3
+    local ip=$4
+
+    echo "[$ns] $vf ($ip) -> $rep"
+    ifconfig $rep 0 up
+    ip netns add $ns
+    ip link set $vf netns $ns
+    ip netns exec $ns ifconfig $vf $ip/24 up
+}
+
+function run() {
+    title "Test CT TCP"
+    config_vf ns0 $VF $REP $IP1
+    config_vf ns1 $VF2 $REP2 $IP2
+
+    echo "add arp rules"
+    tc_filter add dev $REP ingress protocol arp prio 1 flower \
+        action mirred egress redirect dev $REP2
+
+    tc_filter add dev $REP2 ingress protocol arp prio 1 flower \
+        action mirred egress redirect dev $REP
+
+    echo "add ct rules"
+    tc_filter add dev $REP ingress protocol ip prio 2 flower \
+        dst_mac $mac2 ct_state -trk \
+        action ct action goto chain 1
+
+    tc_filter add dev $REP ingress protocol ip chain 1 prio 2 flower \
+        dst_mac $mac2 ct_state +trk+new \
+        action mirred egress redirect dev $REP2
+
+    tc_filter add dev $REP ingress protocol ip chain 1 prio 2 flower \
+        dst_mac $mac2 ct_state +trk+est \
+        action mirred egress redirect dev $REP2
+
+    # chain0,ct -> chain1,fwd
+    tc_filter add dev $REP2 ingress protocol ip prio 2 flower \
+        dst_mac $mac1 \
+        action ct action goto chain 1
+
+    tc_filter add dev $REP2 ingress protocol ip prio 2 chain 1 flower \
+        action mirred egress redirect dev $REP
+
+    fail_if_err
+
+    echo $REP
+    tc filter show dev $REP ingress
+    echo $REP2
+    tc filter show dev $REP2 ingress
+
+    t=10
+    echo "run traffic for $t seconds"
+    ip netns exec ns1 timeout $((t+1)) iperf -s &
+    sleep 0.5
+    ip netns exec ns0 timeout $((t+1)) iperf -t $t -c $IP2  &
+
+    echo "sniff packets on $REP"
+    sleep 2
+    # first 4 packets not offloaded until conn is in established state.
+    timeout $t tcpdump -qnnei $REP -c 10 'tcp' &
+    pid=$!
+
+    sleep $t
+    killall -9 iperf &>/dev/null
+    wait $! 2>/dev/null
+
+    # test sniff timedout
+    wait $pid
+    rc=$?
+    if [[ $rc -eq 124 ]]; then
+        :
+    elif [[ $rc -eq 0 ]]; then
+        err "Didn't expect to see packets"
+    else
+        err "Tcpdump failed"
+    fi
+
+    sleep 1
+    reload_modules
+    config_sriov 2
+}
+
+
+start_check_syndrome
+run
+check_syndrome
+test_done
