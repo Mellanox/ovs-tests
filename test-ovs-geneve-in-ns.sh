2e2653c5c0332c9ede77cdd1f612d4a576a73a1b
Yevgeny Kliteynik <kliteyn@mellanox.com>
2019-04-16 01:12:07 +0300

    Added GENEVE tests
    Issue: 1668596
    Change-Id: If638c5a8c711568e112277316e119cfc51555b06
    Signed-off-by: Yevgeny Kliteynik <kliteyn@mellanox.com>

---
+++ test-ovs-geneve-in-ns.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+#
+# Test OVS inserting geneve rule with IPv4 addresses
+# - create geneve interface in namespace
+# - add geneve interface to OVS
+# - ping between two interfaces
+# - validate that OVS has offloaded the flows (check existing rules)
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+
+VM1_IP="7.7.7.1"
+VM2_IP="7.7.7.2"
+
+local_tun="2.2.2.2"
+remote_tun="2.2.2.3"
+
+
+function cleanup() {
+    echo "cleanup"
+    start_clean_openvswitch
+    ip l del dev genev_sys_6081 &>/dev/null
+    ip netns del ns0 &> /dev/null
+
+    for i in `seq 0 7`; do
+        ip link del veth$i &> /dev/null
+    done
+}
+
+function check_offloaded_rules() {
+    local count=$1
+    title " - check for $count offloaded rules"
+    if [ "$USE_DPCTL" = 1 ]; then
+        RES="ovs_dpctl_dump_flows | grep 0x0800 | grep -v drop"
+    else
+        RES="ovs_appctl_dpctl_dump_flows | grep 0x0800 | grep -v drop"
+    fi
+    eval $RES
+    RES=`eval $RES | wc -l`
+    if (( RES == $count )); then
+        success
+    else
+        ovs-dpctl dump-flows | grep 0x0800
+        err
+    fi
+}
+
+function configure_geneve() {
+    local geneve_port=$1
+
+    title "Test geneve with port $geneve_port"
+    cleanup
+
+    echo "setup veth and ns"
+    ip link add veth0 type veth peer name veth1 || fail "Failed to configure veth"
+    ip link add veth2 type veth peer name veth3
+
+    ifconfig veth0 $VM1_IP/24 up
+    ifconfig veth1 up
+
+    ifconfig veth2 $local_tun/24 up
+
+    echo "setup ovs dst_port:$geneve_port"
+    ovs-vsctl add-br brv-1
+    ovs-vsctl add-port brv-1 veth1
+    ovs-vsctl add-port brv-1 geneve_ovs -- set interface geneve_ovs type=geneve options:local_ip=$local_tun options:remote_ip=$remote_tun options:key=42 options:dst_port=$geneve_port
+
+    ip netns add ns0
+    ip link set veth3 netns ns0
+    ip netns exec ns0 ifconfig veth3 $remote_tun/24 up
+
+    ip netns exec ns0 ip link add dev geneve42 type geneve vni 42 remote $local_tun dstport $geneve_port
+    ip netns exec ns0 ifconfig geneve42 $VM2_IP/24 up
+
+    sleep 1
+
+    title "Test ping $VM1_IP -> $VM2_IP"
+    ping -q -c 10 -i 0.2 -w 4 $VM2_IP && success || err
+
+    check_offloaded_rules 2
+    cleanup
+}
+
+configure_geneve 6081
+
+test_done
---
+++ test-tc-insert-rules-geneve.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+#
+# Test insert geneve rule with TLV options
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+
+function tc_filter() {
+    eval2 tc filter $@ && success
+}
+
+function __test_geneve() {
+    local ip_src=$1
+    local ip_dst=$2
+    local skip
+
+    title " - create geneve interface"
+    gv=geneve1
+    geneve_port=6081
+    ip link del $gv >/dev/null 2>&1
+    ip link add dev $gv type geneve dstport $geneve_port external
+    [ $? -ne 0 ] && err "Failed to create geneve interface" && return 1
+    ip link set dev $gv up
+    tc qdisc add dev $gv ingress
+
+    ip a show dev $gv
+
+    enable_switchdev
+    ifconfig $NIC up
+    ifconfig $REP up
+    reset_tc $NIC
+    reset_tc $REP
+
+    m=`ip addr show $gv 2>&1`
+    [ $? -ne 0 ] && fail $m
+
+    ip addr flush dev $NIC
+    ip addr add $ip_src/16 dev $NIC
+    ip neigh add $ip_dst lladdr e4:11:22:11:55:55 dev $NIC
+
+    reset_tc $REP
+    reset_tc $gv
+    title "    - encap"
+    tc_filter add dev $REP protocol 0x806 parent ffff: prio 1 \
+                    flower \
+                            skip_sw \
+                            dst_mac e4:11:22:11:4a:51 \
+                            src_mac e4:11:22:11:4a:50 \
+                    action tunnel_key set \
+                    src_ip $ip_src \
+                    dst_ip $ip_dst \
+                    dst_port $geneve_port \
+                    id 100 \
+                    geneve_opts 1234:56:0708090a \
+                    action mirred egress redirect dev $gv
+
+    title "    - decap"
+    tc_filter add dev $gv protocol 0x806 parent ffff: prio 2 \
+                    flower \
+                            dst_mac e4:11:22:11:4a:51 \
+                            src_mac e4:11:22:11:4a:50 \
+                            enc_src_ip $ip_src \
+                            enc_dst_ip $ip_dst \
+                            enc_dst_port $geneve_port \
+                            enc_key_id 100 \
+                            geneve_opts 0102:34:05060708 \
+                    action tunnel_key unset \
+                    action mirred egress redirect dev $REP
+    verify_in_hw $gv 2
+
+    reset_tc $NIC
+    reset_tc $REP
+    reset_tc $gv
+    ip addr flush dev $NIC
+    ip link del $gv
+}
+
+function test_geneve_ipv4() {
+    __test_geneve \
+                        20.1.11.1 \
+                        20.1.12.1
+}
+
+title "Test adding geneve rules"
+test_geneve_ipv4
+
+test_done
