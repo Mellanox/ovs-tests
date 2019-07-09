be3999d5dfa38342b73fe50e25dac13d6977352e
Roi Dayan <roid@mellanox.com>
2019-07-09 16:54:23 +0300

    New test test-ovs-ct-scapy-udp-aging-ovs.sh
    Change-Id: Iec8e1bd92d64be352e1f88921c76c83790163498
    Signed-off-by: Roi Dayan <roid@mellanox.com>

---
+++ test-ovs-ct-scapy-udp-aging-ovs.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+#
+# Test CT with OVS aging
+# Test OVS aging before conntrack aging
+# Expected result not to get any kernel trace.
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+pktgen=$my_dir/scapy-traffic-tester.py
+
+require_module act_ct
+
+IP1="7.7.7.1"
+IP2="7.7.7.2"
+
+enable_switchdev_if_no_rep $REP
+require_interfaces REP REP2
+unbind_vfs
+bind_vfs
+reset_tc $REP
+reset_tc $REP2
+
+function cleanup() {
+    ip netns del ns0 2> /dev/null
+    ip netns del ns1 2> /dev/null
+    reset_tc $REP
+    reset_tc $REP2
+}
+trap cleanup EXIT
+
+function config_vf() {
+    local ns=$1
+    local vf=$2
+    local rep=$3
+    local ip=$4
+
+    echo "[$ns] $vf ($ip) -> $rep"
+    ifconfig $rep 0 up
+    ip netns add $ns
+    ip link set $vf netns $ns
+    ip netns exec $ns ifconfig $vf $ip/24 up
+}
+
+function config_ovs() {
+    local proto=$1
+
+    echo "setup ovs"
+    start_clean_openvswitch
+    ovs-vsctl add-br br-ovs
+    ovs-vsctl add-port br-ovs $REP
+    ovs-vsctl add-port br-ovs $REP2
+
+    ovs-ofctl add-flow br-ovs in_port=$REP,dl_type=0x0806,actions=output:$REP2
+    ovs-ofctl add-flow br-ovs in_port=$REP2,dl_type=0x0806,actions=output:$REP
+
+    ovs-ofctl add-flow br-ovs "table=0, $proto,ct_state=-trk actions=ct(table=1,nat)"
+    ovs-ofctl add-flow br-ovs "table=1, $proto,ct_state=+trk+new actions=ct(commit),normal"
+    ovs-ofctl add-flow br-ovs "table=1, $proto,ct_state=+trk+est actions=normal"
+
+    ovs-ofctl dump-flows br-ovs --color
+}
+
+function run() {
+    title "Test OVS CT aging"
+    config_vf ns0 $VF $REP $IP1
+    config_vf ns1 $VF2 $REP2 $IP2
+
+    proto="udp"
+    config_ovs $proto
+
+    # TODO verify ovs aging is shorter than ct aging
+    # assume ovs aging 10 seconds
+
+    t=10
+    port_count=100
+
+    echo "run traffic for $t seconds"
+    ip netns exec ns1 $pktgen -l -i $VF2 --src-ip $IP1 --time $((t+1)) &
+    pk1=$!
+    sleep 1
+    ip netns exec ns0 $pktgen -i $VF1 --src-ip $IP1 --dst-ip $IP2 --time $t --src-port-count $port_count &
+    pk2=$!
+
+    sleep $t
+    kill $pk1 &>/dev/null
+    wait $pk1 $pk2 2>/dev/null
+
+    # sleep ovs aging
+    title "- wait for ovs aging"
+    sleep 12
+    count=`ovs_dump_flows | wc -l`
+    if [ $count != 0 ]; then
+        ovs_dump_flows
+        err "Expected 0 rules"
+    fi
+
+    ovs-vsctl del-br br-ovs
+
+    # wait for traces as merging & offloading is done in workqueue.
+    sleep 3
+}
+
+
+run
+test_done
