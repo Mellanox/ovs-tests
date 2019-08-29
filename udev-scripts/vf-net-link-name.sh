c0a0900c2ce9002156e37a2cf6e8e2de97822c0d
Roi Dayan <roid@mellanox.com>
2019-08-29 11:08:43 +0300

    Update vf-net-link-name.sh
    Change-Id: I8da19a3842a2464480c8eb423269e005139429cb
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- udev-scripts/vf-net-link-name.sh
+++ udev-scripts/vf-net-link-name.sh
@@ -13,17 +13,18 @@ fi
 
 # for pf and uplink rep fall to slot or path.
 if [ -n "$ID_NET_NAME_SLOT" ]; then
-    echo "NAME=$ID_NET_NAME_SLOT"
+    echo NAME="${ID_NET_NAME_SLOT%%np[[:digit:]]}"
     exit
 fi
 
 if [ -n "$ID_NET_NAME_PATH" ]; then
-    echo "NAME=$ID_NET_NAME_PATH"
+    echo NAME="${ID_NET_NAME_PATH%%np[[:digit:]]}"
     exit
 fi
 
 function get_name() {
-    udevadm info -q property -p /sys/class/net/$1 | grep $2 | cut -d= -f2
+    local a=`udevadm info -q property -p /sys/class/net/$1 | grep $2 | cut -d= -f2`
+    echo ${a%%np[[:digit:]]}
 }
 
 # for vf rep get parent slot/path.
