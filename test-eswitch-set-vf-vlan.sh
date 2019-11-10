2354e5a6dd2c0a25f626d471d4845e14f8bb8736
Roi Dayan <roid@mellanox.com>
2019-11-10 11:03:48 +0200

    Add test-eswitch-set-vf-vlan.sh
    Change-Id: Iaa5fbe12ca72b3328ac83657aa1c85a6d4587f1e
    Signed-off-by: Roi Dayan <roid@mellanox.com>

---
+++ test-eswitch-set-vf-vlan.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Test set vf vlan 0 works on switchdev but no other vlan id
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+config_sriov 2
+enable_switchdev_if_no_rep $REP
+
+title "Test set vf vlan id 200"
+ip link set $NIC vf 1 vlan 200 2>/dev/null && err "Expected to fail"
+
+title "Test set vf vlan id 0"
+ip link set $NIC vf 1 vlan 0 || err "Failed to set vf vlan id 0"
+
+test_done
