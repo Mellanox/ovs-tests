d8e3f795a7a8ebecf187bc416fa03f5284eac302
Roi Dayan <roid@mellanox.com>
2019-11-19 10:22:06 +0200

    test-eswitch-nic-netdev-tx.sh: Add check of sysfs counter
    Change-Id: Ifa3fcc9f3bc7ec6c2837ef56b84153406eff3ea5
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-eswitch-nic-netdev-tx.sh
+++ test-eswitch-nic-netdev-tx.sh
@@ -19,13 +19,26 @@ ifconfig $NIC 1.1.1.1/24 up
 enable_switchdev
 
 title "Verify traffic with TX counter"
+
+# pre check sysfs. had an issue ifconfig shows no progress and calling ethtool
+# cause an update.
+sysfs_count1=`cat /sys/class/net/$NIC/statistics/tx_packets`
 count1=`get_tx_pkts $NIC`
+
 ip n r 1.1.1.101 dev $NIC lladdr e4:11:22:33:44:55
 ping -i 0.01 -c 100 -w 3 -q 1.1.1.101 &>/dev/null
+
+sysfs_count2=`cat /sys/class/net/$NIC/statistics/tx_packets`
 count2=`get_tx_pkts $NIC`
+
 ((diff=count2-count1))
 if [ "$diff" -lt 100 ]; then
     err "Nic $NIC tx is not increasing (diff: $diff)"
+fi
+
+((diff=sysfs_count2-sysfs_count1))
+if [ "$diff" -lt 100 ]; then
+    err "Nic $NIC sysfs tx is not increasing (diff: $diff)"
 fi
 
 ip n del 1.1.1.101 dev $NIC
