83a65aedd09ad46f4000be6238e0f8ebac8b1c0a
Roi Dayan <roid@mellanox.com>
2019-12-25 14:20:22 +0200

    test-eswitch-no-carrier.sh: Add case after configuring two ports
    The issue reproduced after configuring two ports but not with one.
    
    Change-Id: I0f67351b023375e36f596c6cdba04161f1a344af
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-eswitch-no-carrier.sh
+++ test-eswitch-no-carrier.sh
@@ -7,26 +7,66 @@
 my_dir="$(dirname "$0")"
 . $my_dir/common.sh
 
+function config_port() {
+    config_sriov 2
+    unbind_vfs
+    enable_switchdev_if_no_rep $REP
+    set_macs 2
+    bind_vfs
+    sleep 1
+}
 
-unbind_vfs
-enable_switchdev_if_no_rep $REP
-set_macs 2
-bind_vfs
-sleep 1
+function do_test() {
+    # bring up rep is what triggers the issue
+    require_interfaces REP
+    ip link set dev $REP up
+    switch_mode_legacy
+    sleep 1
+    require_interfaces NIC VF
+    ip link set dev $NIC up
+    ip link set dev $VF up
+    sleep 1
+}
 
-# bring up rep is what triggers the issue
-require_interfaces REP
-ip link set dev $REP up
-switch_mode_legacy
-sleep 1
-require_interfaces NIC VF
-ip link set dev $NIC up
-ip link set dev $VF up
-carrier=`cat /sys/class/net/$VF/carrier`
+function test_carrier() {
+    carrier=`cat /sys/class/net/$VF/carrier`
 
-if [ "$carrier" == "0" ]; then
-    ip link show dev $VF
-    err "VF $VF has no carrier"
+    if [ "$carrier" == "0" ]; then
+        ip link show dev $VF
+        err "VF $VF has no carrier"
+        return
+    fi
+    success
+}
+
+function config_two_ports() {
+    config_sriov 0
+    config_sriov 0 $NIC2
+    config_sriov 2
+    config_sriov 2 $NIC2
+    unbind_vfs
+    unbind_vfs $NIC2
+    enable_switchdev
+    bind_vfs
+    config_sriov 0 $NIC2
+}
+
+
+title "Test one port config"
+config_port
+do_test
+test_carrier
+
+title "Test two ports config"
+# we saw issue start reproducing after configuring two ports.
+config_two_ports
+do_test
+test_carrier
+if [ $TEST_FAILED == 1 ]; then
+    # next run port one also fails. so for consistency between runs,
+    # reload the modules.
+    reload_modules
+    config_port
 fi
 
 test_done
