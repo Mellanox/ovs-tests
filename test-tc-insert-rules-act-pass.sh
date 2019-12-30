41fac90ad8b6d539a85ceb27d56adb8818017468
Roi Dayan <roid@mellanox.com>
2019-12-30 16:10:18 +0200

    Add test test-tc-insert-rules-act-pass.sh
    Change-Id: Iaafb6852b9d635d7859015fedd76179b3bfafb18
    Signed-off-by: Roi Dayan <roid@mellanox.com>

---
+++ test-tc-insert-rules-act-pass.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Test rule with action pass on vf
+#
+# e1652e37318e net/mlx5e: Support accept action on nic table
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+enable_switchdev
+unbind_vfs
+bind_vfs
+require_interfaces VF
+
+title "Test action pass rule on $VF"
+reset_tc $VF
+tc_filter add dev $VF protocol ip parent ffff: prio 1 handle 1 flower skip_sw dst_ip 1.1.1.1 action pass
+reset_tc $VF
+
+test_done
