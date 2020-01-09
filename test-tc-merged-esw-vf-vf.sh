5956d8a9f0abdd6d4c2e9d68236ea8249dc1c87b
Roi Dayan <roid@mellanox.com>
2020-01-09 11:26:02 +0200

    test-tc-merged-esw-vf-vf.sh: cleanup
    Change-Id: I7a881d85bbbedd72b94b0333df098c5c1cac581d
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-tc-merged-esw-vf-vf.sh
+++ test-tc-merged-esw-vf-vf.sh
@@ -18,10 +18,12 @@ REP2=`get_rep 0 $NIC2`
 title "- add redirect rule $REP -> $REP2"
 reset_tc $REP
 tc_filter add dev $REP protocol ip ingress prio 1 flower skip_sw dst_mac e4:11:22:11:4a:51 action mirred egress redirect dev $REP2
+reset_tc $REP
 
 title "- add redirect rule $REP2 -> $REP"
 reset_tc $REP2
 tc_filter add dev $REP2 protocol ip ingress prio 1 flower skip_sw dst_mac e4:11:22:11:4a:51 action mirred egress redirect dev $REP
+reset_tc $REP2
 
 disable_sriov_port2
 check_syndrome
