4dd665b2742487f1ac172d5a723b62a9831e1b6c
Roi Dayan <roid@mellanox.com>
2020-03-01 10:44:23 +0200

    Add test test-eswitch-with-ipv4-ttl-modify-enabled.sh
    Change-Id: Ib55b6009e5b831bb9b4087f2055a15932e9b7caf
    Signed-off-by: Roi Dayan <roid@mellanox.com>

---
+++ test-eswitch-with-ipv4-ttl-modify-enabled.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Bug SW #2094097: [Upstream] Fail to switch mode to switchdev while ESWITCH_IPV4_TTL_MODIFY_ENABLE is enabled
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+require_mlxconfig
+
+function set_eswitch_ipv4_ttl_modify_enable() {
+    local mode=$1
+    fw_config ESWITCH_IPV4_TTL_MODIFY_ENABLE=$mode
+}
+
+
+function cleanup() {
+    title "- disable ipv4 ttl modify"
+    set_eswitch_ipv4_ttl_modify_enable false
+    fw_reset
+    config_sriov 2
+}
+
+
+title "Test set switchdev dev when eswitch ipv5 ttl modify enabled"
+trap cleanup EXIT
+start_check_syndrome
+
+title "- enable ipv4 ttl modify"
+set_eswitch_ipv4_ttl_modify_enable true || fail "Cannot set eswitch ipv4 ttl modify enable"
+fw_reset
+
+title "- set switchdev"
+config_sriov 2
+enable_switchdev_if_no_rep $REP
+
+trap - EXIT
+cleanup
+check_syndrome
+test_done
