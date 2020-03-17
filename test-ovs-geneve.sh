f03cae48930e6e4b7a00ea4fbc5a3e379059ebb9
Roi Dayan <roid@mellanox.com>
2020-03-17 09:51:23 +0200

    Add test ovs with geneve
    Change-Id: Id1f705c9f7cb8ae18fb89898d6b5ee757dab7602
    Signed-off-by: Roi Dayan <roid@mellanox.com>

---
+++ test-ovs-geneve.sh
@@ -0,0 +1,185 @@
+#!/bin/bash
+#
+# Test OVS with geneve traffic
+#
+# Require external server
+#
+# IGNORE_FROM_TEST_ALL
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+require_module act_ct
+
+REMOTE_SERVER=${REMOTE_SERVER:-$1}
+REMOTE_NIC=${REMOTE_NIC:-$2}
+
+require_remote_server
+
+IP=1.1.1.7
+REMOTE=1.1.1.8
+
+LOCAL_TUN=7.7.7.7
+REMOTE_IP=7.7.7.8
+TUN_ID=42
+
+enable_switchdev_if_no_rep $REP
+require_interfaces REP NIC
+unbind_vfs
+bind_vfs
+
+
+function set_nf_liberal() {
+    nf_liberal="/proc/sys/net/netfilter/nf_conntrack_tcp_be_liberal"
+    if [ -e $nf_liberal ]; then
+        echo 1 > $nf_liberal
+        echo "`basename $nf_liberal` set to: `cat $nf_liberal`"
+    else
+        echo "Cannot find $nf_liberal"
+    fi
+}
+
+function cleanup_remote() {
+    on_remote ip a flush dev $REMOTE_NIC
+    on_remote ip l del dev geneve1 &>/dev/null
+}
+
+function cleanup() {
+    ip a flush dev $NIC
+    ip netns del ns0 &>/dev/null
+    ip netns del ns1 &>/dev/null
+    cleanup_remote
+    sleep 0.5
+}
+trap cleanup EXIT
+
+function config() {
+    cleanup
+    set_nf_liberal
+    conntrack -F
+    ifconfig $NIC $LOCAL_TUN/24 up
+    # WA SimX bug? interface not receiving traffic from tap device to down&up to fix it.
+    for i in $NIC $VF $REP ; do
+            ifconfig $i down
+            ifconfig $i up
+            reset_tc $i
+    done
+    ip netns add ns0
+    ip link set dev $VF netns ns0
+    ip netns exec ns0 ifconfig $VF $IP/24 up
+
+    echo "Restarting OVS"
+    start_clean_openvswitch
+
+    ovs-vsctl add-br br-ovs
+    ovs-vsctl add-port br-ovs $REP
+    ovs-vsctl add-port br-ovs geneve1 -- set interface geneve1 type=geneve options:local_ip=$LOCAL_TUN options:remote_ip=$REMOTE_IP options:key=$TUN_ID options:dst_port=6081
+}
+
+function config_remote() {
+    on_remote ip link del geneve1 2>/dev/null
+    on_remote ip link add geneve1 type geneve id $TUN_ID remote $LOCAL_TUN dstport 6081
+    on_remote ip a flush dev $REMOTE_NIC
+    on_remote ip a add $REMOTE_IP/24 dev $REMOTE_NIC
+    on_remote ip a add $REMOTE/24 dev geneve1
+    on_remote ip l set dev geneve1 up
+    on_remote ip l set dev $REMOTE_NIC up
+}
+
+function add_openflow_rules() {
+#    ovs-ofctl del-flows br-ovs
+#    ovs-ofctl add-flow br-ovs arp,actions=normal
+#    ovs-ofctl add-flow br-ovs icmp,actions=normal
+    ovs-ofctl dump-flows br-ovs --color
+}
+
+function test_have_traffic() {
+    local pid=$1
+    wait $pid
+    local rc=$?
+    if [[ $rc -eq 0 ]]; then
+        :
+    elif [[ $rc -eq 124 ]]; then
+        err "Expected to see packets"
+    else
+        err "Tcpdump failed rc $rc"
+    fi
+}
+
+function test_no_traffic() {
+    local pid=$1
+    wait $pid
+    local rc=$?
+    if [[ $rc -eq 124 ]]; then
+        :
+    elif [[ $rc -eq 0 ]]; then
+        err "Didn't expect to see packets"
+    else
+        err "Tcpdump failed rc $rc"
+    fi
+}
+
+function run() {
+    config
+    config_remote
+    add_openflow_rules
+    #ip a show dev $NIC
+    #ip netns exec ns0 ip a s dev $VF
+
+    # icmp
+    ip netns exec ns0 ping -q -c 1 -i 0.1 -w 1 $REMOTE
+    if [ $? -ne 0 ]; then
+        err "ping failed"
+        return
+    fi
+
+    # initial traffic
+    ssh2 $REMOTE_SERVER timeout 4 iperf3 -s &
+    pid1=$!
+    sleep 0.5
+    ip netns exec ns0 timeout 3 iperf3 -c $REMOTE -t 2 &
+    pid2=$!
+
+    sleep 4
+    kill -9 $pid1 $pid2 &>/dev/null
+    wait $pid1 $pid2 &>/dev/null
+
+    # traffic
+    ssh2 $REMOTE_SERVER timeout 15 iperf3 -s &
+    pid1=$!
+    sleep 0.5
+    ip netns exec ns0 timeout 15 iperf3 -c $REMOTE -t 14 -P3 &
+    pid2=$!
+
+    # verify pid
+    sleep 2
+    kill -0 $pid2 &>/dev/null
+    if [ $? -ne 0 ]; then
+        err "iperf failed"
+        return
+    fi
+
+    # verify traffic
+    ip netns exec ns0 timeout 12 tcpdump -qnnei $VF -c 30 ip &
+    tpid1=$!
+    timeout 12 tcpdump -qnnei $REP -c 10 ip &
+    tpid2=$!
+    timeout 12 tcpdump -qnnei genev_sys_6081 -c 10 ip &
+    tpid3=$!
+
+    sleep 15
+    title "Verify traffic on $VF"
+    test_have_traffic $tpid1
+    title "Verify offload on $REP"
+    test_no_traffic $tpid2
+    title "Verify offload on genev_sys_6081"
+    test_no_traffic $tpid3
+
+    kill -9 $pid1 $pid2 &>/dev/null
+    echo "wait for bgs"
+    wait &>/dev/null
+}
+
+run
+start_clean_openvswitch
+test_done
