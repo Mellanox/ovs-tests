Mellanox/ovs-tests

commit 8e9d565e8268a88e41fa3947a60b3c19115c365e

common.sh: Move config_vf() to common, v2
43 test files define identical config_vf function.
Make them use the one moved to common.sh.
config_vf functions, which differs, still left in files.

Change-Id: Id99559153406d1e0e839d9ecd731d62bc1cffbc8
Signed-off-by: Dmytro Linkin <dmitrolin@mellanox.com>

common.sh
@@ -368,6 +368,19 @@ function title() {
     kmsg $@
 }
 
+function config_vf() {
+    local ns=$1
+    local vf=$2
+    local rep=$3
+    local ip=$4
+
+    echo "[$ns] $vf ($ip) -> $rep"
+    ifconfig $rep 0 up
+    ip netns add $ns
+    ip link set $vf netns $ns
+    ip netns exec $ns ifconfig $vf $ip/24 up
+}
+
 function config_reps() {
     local want=$1
     local nic=$2

test-ct-icmp-frag.sh
@@ -33,19 +33,6 @@ function cleanup() {
 }
 trap cleanup EXIT
 
-function config_vf() {
-    local ns=$1
-    local vf=$2
-    local rep=$3
-    local ip=$4
-
-    echo "[$ns] $vf ($ip) -> $rep"
-    ifconfig $rep 0 up
-    ip netns add $ns
-    ip link set $vf netns $ns
-    ip netns exec $ns ifconfig $vf $ip/24 up
-}
-
 function run() {
     title "Test CT ICMP"
     config_vf ns0 $VF $REP $IP1

test-ct-icmp-fwd.sh
@@ -34,19 +34,6 @@ function cleanup() {
 }
 trap cleanup EXIT
 
-function config_vf() {
-    local ns=$1
-    local vf=$2
-    local rep=$3
-    local ip=$4
-
-    echo "[$ns] $vf ($ip) -> $rep"
-    ifconfig $rep 0 up
-    ip netns add $ns
-    ip link set $vf netns $ns
-    ip netns exec $ns ifconfig $vf $ip/24 up
-}
-
 function run() {
     title "Test CT ICMP fwd"
     config_vf ns0 $VF $REP $IP1

test-ct-icmp.sh
@@ -32,19 +32,6 @@ function cleanup() {
 }
 trap cleanup EXIT
 
-function config_vf() {
-    local ns=$1
-    local vf=$2
-    local rep=$3
-    local ip=$4
-
-    echo "[$ns] $vf ($ip) -> $rep"
-    ifconfig $rep 0 up
-    ip netns add $ns
-    ip link set $vf netns $ns
-    ip netns exec $ns ifconfig $vf $ip/24 up
-}
-
 function run() {
     title "Test CT ICMP"
     config_vf ns0 $VF $REP $IP1

test-ct-tcp-pedit.sh
@@ -33,19 +33,6 @@ function cleanup() {
 }
 trap cleanup EXIT
 
-function config_vf() {
-    local ns=$1
-    local vf=$2
-    local rep=$3
-    local ip=$4
-
-    echo "[$ns] $vf ($ip) -> $rep"
-    ifconfig $rep 0 up
-    ip netns add $ns
-    ip link set $vf netns $ns
-    ip netns exec $ns ifconfig $vf $ip/24 up
-}
-
 function run() {
     title "Test CT TCP pedit"
     config_vf ns0 $VF $REP $IP1

test-ct-tcp-reload.sh
@@ -37,19 +37,6 @@ function cleanup() {
 }
 trap cleanup EXIT
 
-function config_vf() {
-    local ns=$1
-    local vf=$2
-    local rep=$3
-    local ip=$4
-
-    echo "[$ns] $vf ($ip) -> $rep"
-    ifconfig $rep 0 up
-    ip netns add $ns
-    ip link set $vf netns $ns
-    ip netns exec $ns ifconfig $vf $ip/24 up
-}
-
 function run() {
     title "Test CT TCP"
     config_vf ns0 $VF $REP $IP1

test-ct-tcp-vf-mirror-pedit.sh
@@ -36,19 +36,6 @@ function cleanup() {
 }
 trap cleanup EXIT
 
-function config_vf() {
-    local ns=$1
-    local vf=$2
-    local rep=$3
-    local ip=$4
-
-    echo "[$ns] $vf ($ip) -> $rep"
-    ifconfig $rep 0 up
-    ip netns add $ns
-    ip link set $vf netns $ns
-    ip netns exec $ns ifconfig $vf $ip/24 up
-}
-
 function run() {
     title "Test CT TCP VF mirror"
     config_vf ns0 $VF $REP $IP1

test-ct-tcp.sh
@@ -33,19 +33,6 @@ function cleanup() {
 }
 trap cleanup EXIT
 
-function config_vf() {
-    local ns=$1
-    local vf=$2
-    local rep=$3
-    local ip=$4
-
-    echo "[$ns] $vf ($ip) -> $rep"
-    ifconfig $rep 0 up
-    ip netns add $ns
-    ip link set $vf netns $ns
-    ip netns exec $ns ifconfig $vf $ip/24 up
-}
-
 function get_pkts() {
     # single table tc show doesn't have nested keys attribute
     s1=`tc -j -p -s  filter show dev $REP protocol ip ingress | jq '.[] | select(.options.ct_state == "+trk+est") | .options.actions[0].stats.packets' || 0`

commit 37fcad43ff40776032c3912860953b5959a6249b

common.sh: Fix config_vf() for ipv6 config
Also use ip -netns arg instead of ip netns exec while there.

Change-Id: Ic4c30a5d89fe6526942a13850d268b2c1e98b927
Fixes: 8e9d565e8268 ("common.sh: Move config_vf() to common, v2")
Signed-off-by: Roi Dayan <roid@mellanox.com>

common.sh
@@ -373,12 +373,20 @@ function config_vf() {
     local vf=$2
     local rep=$3
     local ip=$4
+    local prefix=24
+
+    if [[ "$ip" == *":"* ]]; then
+        # ipv6
+        prefix=64
+    fi
 
     echo "[$ns] $vf ($ip) -> $rep"
-    ifconfig $rep 0 up
+    ip address flush dev $rep
+    ip link set dev $rep up
     ip netns add $ns
     ip link set $vf netns $ns
-    ip netns exec $ns ifconfig $vf $ip/24 up
+    ip -netns $ns address replace dev $vf $ip/$prefix
+    ip -netns $ns link set $vf up
 }
 
 function config_reps() {