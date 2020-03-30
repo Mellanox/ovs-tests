c71fe651fdb997f295c1c114c8f0e41a01d4372c
Roi Dayan <roid@mellanox.com>
2020-03-30 12:21:50 +0300

    test-tc-insert-rules-geneve.sh: Add case for partial data mask on chain 1
    Change-Id: Ie4f964d45062188a0a4e144a162f68a090487831
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-tc-insert-rules-geneve.sh
+++ test-tc-insert-rules-geneve.sh
@@ -129,6 +129,21 @@ function __test_geneve() {
     verify_not_in_hw $gv 4
     reset_tc $gv
 
+    title "- decap geneve_opts chain 1 partial data mask"
+    tc_filter_success add dev $gv protocol 0x806 parent ffff: prio 3 chain 1 \
+                    flower $tc_verbose \
+                            dst_mac e4:11:22:11:4a:51 \
+                            src_mac e4:11:22:11:4a:50 \
+                            enc_src_ip $ip_src \
+                            enc_dst_ip $ip_dst \
+                            enc_dst_port $geneve_port \
+                            enc_key_id 100 \
+                            geneve_opts 0102:80:00010004/ffff:ff:7fffffff \
+                    action tunnel_key unset \
+                    action mirred egress redirect dev $REP
+    verify_in_hw $gv 3
+    reset_tc $gv
+
     ip addr flush dev $NIC
     ip link del $gv
 }
