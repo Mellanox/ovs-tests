800d04bf4cc224d3a412fe82cac1afb87db13d24
Roi Dayan <roid@mellanox.com>
2020-04-01 14:21:42 +0300

    test-tc-insert-rule-rep-uplink-vlan.sh: verify hw rules actions are correct
    Change-Id: I5a50a6ac710d240f88b091044448044afc0383eb
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-tc-insert-rule-rep-uplink-vlan.sh
+++ test-tc-insert-rule-rep-uplink-vlan.sh
@@ -17,20 +17,35 @@ function run() {
     vlan_dev=${NIC}.$vlan
 
     ip link add link $NIC name $vlan_dev type vlan id $vlan
+    reset_tc $REP $vlan_dev
 
     title "Add TC mirred rule from $REP to $vlan_dev"
-    reset_tc $REP
     tc_filter add dev $REP protocol ip ingress prio 10 flower \
-        dst_mac e4:11:22:33:44:70 ip_proto udp \
+        dst_mac e4:66:77:88:44:70 ip_proto udp \
         action mirred egress redirect dev $vlan_dev
     verify_in_hw $REP 10
 
+
     title "Add TC mirred rule from $vlan_dev to $REP"
-    reset_tc $vlan_dev
     tc_filter add dev $vlan_dev protocol ip ingress prio 11 flower \
         dst_mac e4:11:22:33:44:60 ip_proto udp \
         action mirred egress redirect dev $REP
     verify_in_hw $vlan_dev 11
+
+    title "verify hw rules"
+    i=0 && mlxdump -d $PCI fsdump --type FT --gvmi=$i > /tmp/port$i --no_zero || err "mlxdump failed"
+
+    # expect action count+fwd+vlan_pop
+    grep -B7 "outer_headers.dmac_47_16.*:0xe4112233" /tmp/port0 | grep -q "action.*:0x8c"
+    if [ $? != 0 ]; then
+        err "Invalid action in hw rule for vlan pop"
+    fi
+
+    # expect action count+fwd+vlan_push
+    grep -B10 "outer_headers.dmac_47_16.*:0xe4667788" /tmp/port0 | grep -q "action.*:0x10c"
+    if [ $? != 0 ]; then
+        err "Invalid action in hw rule for vlan push"
+    fi
 
     title "Add TC drop rule on $vlan_dev"
     reset_tc $vlan_dev
