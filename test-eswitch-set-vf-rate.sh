94b5a99e7fdc6042f9546dcbb07b99ab0cf3ffc8
Roi Dayan <roid@mellanox.com>
2020-04-30 16:08:01 +0300

    test-eswitch-set-vf-rate.sh: Test in legacy
    also update commit info.
    
    Change-Id: I712d8d11ac2fa6057908c5026c9294c4b1068f3a
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-eswitch-set-vf-rate.sh
+++ test-eswitch-set-vf-rate.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #
 # Reported by Tonghao Zhang <xiangxia.m.yue@gmail.com>
-# [PATCH net 2/2] net/mlx5: Avoid panic when setting VF rate
-#
+# 24319258660a net/mlx5: Avoid panic when setting vport rate
 
 my_dir="$(dirname "$0")"
 . $my_dir/common.sh
 
 config_sriov 2
+enable_legacy
 bind_vfs
 # expecting not to crash
 ip link set dev $VF vf 0 min_tx_rate 1 max_tx_rate 2 2>/dev/null
 ip link set dev $VF vf 0 min_tx_rate -1 max_tx_rate -1 2>/dev/null
+enable_switchdev
 test_done
