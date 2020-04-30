b025ab0d43cfa653132929ba8999c136f7c109b2
Roi Dayan <roid@mellanox.com>
2020-04-30 16:07:48 +0300

    test-eswitch-set-vf-mac.sh: Update commit info
    Change-Id: I1280e0bc6748f49123e60a2e11053c87fe826ed5
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-eswitch-set-vf-mac.sh
+++ test-eswitch-set-vf-mac.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #
 # Reported by Tonghao Zhang <xiangxia.m.yue@gmail.com>
-# [PATCH 1/2] net/mlx5: Avoid panic when setting vport mac, getting vport config
-#
+# 6e77c413e8e7 net/mlx5: Avoid panic when setting vport mac, getting vport config
 
 my_dir="$(dirname "$0")"
 . $my_dir/common.sh
