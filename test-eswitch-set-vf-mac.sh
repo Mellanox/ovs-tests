90c6cbd7fb15623f8f0c7c4f9bfceafc04681bc1
Roi Dayan <roid@mellanox.com>
2020-04-30 14:52:07 +0300

    test-eswitch-set-vf-mac.sh: Fix test in legacy
    The original bug was in set vf mac in legacy.
    
    Change-Id: I8feb2fd0cbb20b10e359e97e78b842cc4d0f5e3e
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-eswitch-set-vf-mac.sh
+++ test-eswitch-set-vf-mac.sh
@@ -8,8 +8,10 @@ my_dir="$(dirname "$0")"
 . $my_dir/common.sh
 
 config_sriov 2
+enable_legacy
 bind_vfs
 mac1=`cat /sys/class/net/$VF/address`
 # expecting not to crash
 ip link set dev $VF vf 0 mac $mac1 2>/dev/null
+enable_switchdev
 test_done
