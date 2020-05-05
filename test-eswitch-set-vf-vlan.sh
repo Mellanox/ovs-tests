b52376798199068b02c7808862958b7e42fedc13
Roi Dayan <roid@mellanox.com>
2020-05-05 22:11:02 +0300

    test-eswitch-set-vf-vlan.sh: update the prints
    Change-Id: Ia137aad42a93befff05dbed52a3048b0dba57f3f
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-eswitch-set-vf-vlan.sh
+++ test-eswitch-set-vf-vlan.sh
@@ -9,10 +9,10 @@ my_dir="$(dirname "$0")"
 config_sriov 2
 enable_switchdev_if_no_rep $REP
 
-title "Test set vf vlan id 200"
+title "Test set vf vlan id 200 on $NIC - expected to fail"
 ip link set $NIC vf 1 vlan 200 2>/dev/null && err "Expected to fail"
 
-title "Test set vf vlan id 0"
+title "Test set vf vlan id 0 on $NIC"
 ip link set $NIC vf 1 vlan 0 || err "Failed to set vf vlan id 0"
 
 test_done
