7fef303b2fe479fdcb6d06fa19f93ea39f6259e1
Roi Dayan <roid@mellanox.com>
2020-05-10 10:40:24 +0300

    test-ethtool-basic.sh: Fix not to use static nic name
    Change-Id: I93cc5445510ce5e914711f6a75c3828177ab4ad4
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-ethtool-basic.sh
+++ test-ethtool-basic.sh
@@ -58,7 +58,7 @@ function test_rss()
     ethtool -X $NIC hfunc toeplitz || err "Failed to set hfunc to toeplitz"
     ethtool -x $NIC | grep -q "toeplitz: on" || err "Expected hfunc toeplitz"
 
-    local backup_hkey=`ethtool -x ens1f0np0 | grep -A1 "RSS hash key:" | tail -1`
+    local backup_hkey=`ethtool -x $NIC | grep -A1 "RSS hash key:" | tail -1`
     ethtool -X $NIC hkey $hkey1 || err "Failed to set hkey1"
     ethtool -X $NIC hkey $hkey2 || err "Failed to set hkey2"
     ethtool -x $NIC | grep -q $hkey2 || err "Used hkey is not equal to hkey set by previous command"
