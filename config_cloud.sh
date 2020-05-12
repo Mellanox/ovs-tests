c597099ca7d586f29006d5947373931fe521216d
Roi Dayan <roid@mellanox.com>
2020-05-12 10:44:46 +0300

    Add more example config files
    Change-Id: I19aef3833149273f23c89f984f18ddbd2df132d0
    Signed-off-by: Roi Dayan <roid@mellanox.com>

---
+++ config_cloud.sh
@@ -0,0 +1,8 @@
+NIC=enp0s8f0
+NIC2=enp0s8f1
+VF=enp0s8f0v0
+VF1=$VF
+VF2=enp0s8f0v1
+REP=${NIC}_0
+REP2=${NIC}_1
+B2B=1
---
+++ config_dev139_cx5_2.sh
@@ -0,0 +1,10 @@
+NIC=ens1f0np0
+NIC2=ens1f1np1
+VF=ens1f2np0
+VF1=$VF
+VF2=ens1f3np0
+REP=ens1f0np0_0
+REP2=ens1f0np0_1
+B2B=1
+REMOTE_SERVER=10.212.138.1
+REMOTE_NIC=ens1f0
