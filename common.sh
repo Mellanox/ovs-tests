1e129a65ed8035e3dbb2d4396115dbd503f3deb4
Roi Dayan <roid@mellanox.com>
2020-05-17 15:58:00 +0300

    common.sh: Use single ssh command to disable remote sriov
    Change-Id: I6486a657907a9db3c54352d12c3c07c7af9054b2
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- common.sh
+++ common.sh
@@ -235,10 +235,11 @@ function remote_disable_sriov() {
     local nic1=$REMOTE_NIC
     local nic2=$REMOTE_NIC2
     echo "Disabling sriov in remote server"
-    on_remote "echo 0 > /sys/class/net/$nic1/device/sriov_numvfs" &>/dev/null
+    local cmd="echo 0 > /sys/class/net/$nic1/device/sriov_numvfs"
     if [ -n "$nic2" ]; then
-        on_remote "echo 0 > /sys/class/net/$nic2/device/sriov_numvfs" &>/dev/null
+        cmd+="; echo 0 > /sys/class/net/$nic2/device/sriov_numvfs"
     fi
+    on_remote "$cmd" &>/dev/null
 }
 
 function config_remote_bonding() {
