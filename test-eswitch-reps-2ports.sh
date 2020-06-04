4380282507894ac1d5e3bd37d6ea13245174e318
Roi Dayan <roid@mellanox.com>
2020-06-04 22:07:33 +0300

    test-eswitch-reps-2ports.sh: test less vfs
    There was no real reason to test 8 vfs per port or other number.
    Shorten it now so the test will take less than a minute to run.
    
    Also disable sriov on both nics on start to make sure we test clean.
    
    Change-Id: I53d54b2e6631e748d794fe1d5974987c2740ac41
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-eswitch-reps-2ports.sh
+++ test-eswitch-reps-2ports.sh
@@ -15,15 +15,16 @@ trap cleanup EXIT
 start_check_syndrome
 disable_sriov_autoprobe
 
+config_sriov 0 $NIC
 config_sriov 0 $NIC2
 
-title "Config 8 VFs on $NIC"
-config_reps 8 $NIC
-count_reps 9 $NIC
+title "Config 3 VFs on $NIC"
+config_reps 3 $NIC
+count_reps 4 $NIC
 
-title "Config 8 VFs on $NIC2"
-config_reps 8 $NIC2
-count_reps 18 $NIC
+title "Config 3 VFs on $NIC2"
+config_reps 3 $NIC2
+count_reps 8 $NIC
 
 echo "Cleanup"
 config_sriov 0 $NIC2
