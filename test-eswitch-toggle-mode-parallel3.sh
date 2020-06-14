54bd5de6ac382d39015a7c42f45ec2d726552ebd
Roi Dayan <roid@mellanox.com>
2020-06-14 19:39:00 +0300

    test-eswitch-toggle-mode-parallel3.sh: Ignore errors while toggling
    It is expected to fail when we race and we expect not to crash.
    
    Change-Id: Icd5b7789d62a75a8eb9ff52684e52af367f1a3dc
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-eswitch-toggle-mode-parallel3.sh
+++ test-eswitch-toggle-mode-parallel3.sh
@@ -16,7 +16,9 @@ function reconfig() {
 }
 
 title "Test toggle mode and disable sriov in parallel"
+# first iteration check we can set switchdev
 reconfig
+enable_switchdev $NIC
 
 tmp1="/tmp/a$$"
 tmp2="/tmp/b$$"
@@ -24,13 +26,14 @@ tmp2="/tmp/b$$"
 for i in 1 2 3 4 5 ; do
     title " - config in parallel $i"
     rm -fr $tmp1 $tmp2
-    enable_switchdev $NIC && touch $tmp1 &
+    # ignore errors in case parallel will give not supported error
+    # we just expect not to crash
+    __ignore_errors=1
+    enable_switchdev $NIC &
     sleep 0.1
-    config_sriov 0 $NIC && touch $tmp2 &
+    config_sriov 0 $NIC &
     wait
-    if [ ! -f $tmp1 ] || [ ! -f $tmp2 ]; then
-        err
-    fi
+    __ignore_errors=0
     reconfig
 done
 
