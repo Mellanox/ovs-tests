7f36e8018b74878246462312e9fca76272106f06
Roi Dayan <roid@mellanox.com>
2020-06-16 21:16:28 +0300

    Add new test to check for unexpected module dependencies
    Change-Id: Icd7e52f890afcbb80d3618ffd447815f1c8be3bb
    Signed-off-by: Roi Dayan <roid@mellanox.com>

---
+++ test-mod-depends.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Verify mlx5_core has only expected module dependencies
+# i.e. not dependent on act_ct or nf_flow_table
+#
+#  #2188070: [CT offload] [mlx5] always depend on act_ct/flow_table, which pulls conntrack
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+
+function run() {
+    title "Look for unexpected mlx5_core dependent modules"
+
+    local expected="ptp|tls|mlxfw|pci_hyperv_intf"
+    local dependent=`lsmod | grep mlx5_core | grep -v ^mlx5_core | awk {'print $1'} | grep -vE -w "$expected" | xargs echo`
+
+    if [ -n "$dependent" ]; then
+        err "Found dependent modules: $dependent"
+        return
+    fi
+
+    success
+}
+
+
+run
+test_done
