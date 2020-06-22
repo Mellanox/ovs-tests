116766aa45c07018dd6829a73b34917eac7ca545
Roi Dayan <roid@mellanox.com>
2020-06-22 12:10:00 +0300

    Add test test-tc-insert-rules-goto2.sh
    Change-Id: I8eee26ea7929013e73fba9262a4fd761e52877a8
    Signed-off-by: Roi Dayan <roid@mellanox.com>

---
+++ test-tc-insert-rules-goto2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Test chains on uplink rep
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+function test_goto_fwd() {
+    title "Test goto fwd"
+
+    reset_tc $NIC
+    tc_filter add dev $NIC root prio 1 chain 1 protocol ip flower skip_sw action goto chain 5
+    reset_tc $NIC
+}
+
+function test_goto_back() {
+    title "Test goto back"
+
+    reset_tc $NIC
+    tc_filter add dev $NIC root prio 1 chain 5 protocol ip flower skip_sw action goto chain 1
+    reset_tc $NIC
+}
+
+enable_switchdev
+test_goto_fwd
+test_goto_back
+check_kasan
+test_done
