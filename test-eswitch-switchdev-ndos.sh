6ca7e6025cd46a423629b0a27f35b6a68d258d74
Roi Dayan <roid@mellanox.com>
2020-06-22 14:00:30 +0300

    test-eswitch-switchdev-ndos.sh: add vlan over uplink rep case
    Change-Id: I3582f66181388ea32118d909c9a39c04c0670090
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-eswitch-switchdev-ndos.sh
+++ test-eswitch-switchdev-ndos.sh
@@ -39,6 +39,19 @@ function check_bridge_link_get() {
     success
 }
 
+function check_create_vlan() {
+    # check can create vlan on uplink rep
+    local vlan_dev=${NIC}.5
+    ip link add link $NIC name $vlan_dev type vlan id 5
+    local rc=$?
+    ip link del $vlan_dev &>/dev/null
+    if [ $rc != 0 ]; then
+        err "Failed to create vlan interface"
+    else
+        success
+    fi
+}
+
 function do_test() {
     title "Verify legacy ndos are blocked in switchdev mode"
 
@@ -57,9 +70,12 @@ function do_test() {
     title "- test state"
     check_ndo state auto
 
-    title "- test vlan"
+    title "- test vf vlan"
     check_ndo vlan 1
     set_ndo vlan 0
+
+    title "- test vlan"
+    check_create_vlan
 }
 
 
