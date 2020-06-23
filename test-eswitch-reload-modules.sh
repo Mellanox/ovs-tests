00c4e71f1975cac4c1110d9aee882607ee56ef7f
Roi Dayan <roid@mellanox.com>
2020-06-23 15:21:02 +0300

    Add test test-eswitch-reload-modules.sh
    Change-Id: I36276c0178d6b293786de8779f0264a153720ebd
    Signed-off-by: Roi Dayan <roid@mellanox.com>

---
+++ test-eswitch-reload-modules.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Test reload mlx5 modules multiple times
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+function reload2() {
+    __ignore_errors=1
+    reload_modules
+    __ignore_errors=0
+    check_kasan
+}
+
+function do_test() {
+    reload2
+    set_macs 2
+    enable_switchdev
+}
+
+for i in `seq 5`; do
+    title "test iteration $i"
+    do_test
+    sleep 2
+done
+
+test_done
