15d98a9792a2713b6a8442358c1590cd878f3e9c
Roi Dayan <roid@mellanox.com>
2020-06-24 15:25:05 +0300

    Add test test-eswitch-hw-tc-offload.sh
    Change-Id: Ie3448dde0d378082b4c92e3ba9e4578093c10610
    Signed-off-by: Roi Dayan <roid@mellanox.com>

---
+++ test-eswitch-hw-tc-offload.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Verify default hw-tc-offload on uplink rep is on
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+require_interfaces NIC
+reload_modules
+config_sriov 2
+enable_switchdev
+value=`ethtool -k enp0s8f0 | grep hw-tc-offload | awk {'print $2'}`
+
+if [ "$value" != "on" ]; then
+    err "Expected hw-tc-offload=on"
+fi
+
+test_done
