37665418d631174c7fc350950416fa38c9e8ac6e
Roi Dayan <roid@mellanox.com>
2020-07-15 13:12:01 +0300

    test-all.py: fix dry run
    Change-Id: Ic871cf1b3c8b5f7142a7b34d33bc47eb4b5a9fb3
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-all.py
+++ test-all.py
@@ -769,7 +769,9 @@ def main():
             reason = test.reason
         elif args.dry:
             res = 'DRY'
+            logname = ''
         else:
+            logname = os.path.join(LOGDIR, test.test_log)
             try:
                 reason = test.run(args.html)
                 res = 'TEST PASSED'
@@ -788,7 +790,6 @@ def main():
         print("%s " % total_seconds, end=' ')
 
         test.status = format_result(res, reason, html=True)
-        logname = os.path.join(LOGDIR, test.test_log)
         print("%s %s" % (format_result(res, reason), logname))
 
         if args.stop and failed:
