1ae5c056b7f1d3b577ad625ade247d20e7a6c1fe
Roi Dayan <roid@mellanox.com>
2020-07-16 17:11:53 +0300

    test-eswitch-devlink-reload.sh: no need to change number of channels
    same crash.
    
    Change-Id: Ic604699b4e127d678a38689e094ac360b9e7fd9f
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-eswitch-devlink-reload.sh
+++ test-eswitch-devlink-reload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Change number of channels and do devlink reload.
+# Test devlink reload command.
 # Expect not to crash.
 #
 # Bug SW #2241106: [ASAP, OFED 5.1, Korg 5.6] Call trace and kernel panic calling devlink reload
@@ -10,16 +10,6 @@ my_dir="$(dirname "$0")"
 
 config_sriov 2 $NIC
 enable_switchdev
-
-ch=$(ethtool -l $NIC | grep Combined | tail -1 | cut -f2-)
-if [ "$ch" == 4 ]; then
-    ch=1
-else
-    ch=4
-fi
-
-title "Change channels to $ch"
-ethtool -L $NIC combined $ch || fail "Failed to change channels"
 
 title "Reload pci $PCI"
 devlink dev reload pci/$PCI || fail "Failed to reload pci device"
