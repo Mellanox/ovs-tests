7e1b8126e282c2f13c39ede02d03d1248825a533
Roi Dayan <roid@mellanox.com>
2020-07-23 15:51:05 +0300

    test-tc-insert-rule-rep-uplink-vlan.sh: Only veirfy hw rules in dmfs
    Currently cannot dump rules in sw steering to verify so skip.
    
    Change-Id: I0700b1d179b7fe9ca6899f1ef34b2b87da35c916
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-tc-insert-rule-rep-uplink-vlan.sh
+++ test-tc-insert-rule-rep-uplink-vlan.sh
@@ -13,6 +13,23 @@ my_dir="$(dirname "$0")"
 enable_switchdev
 require_interfaces REP NIC
 
+function verify_hw_rules() {
+    title "verify hw rules"
+    i=0 && mlxdump -d $PCI fsdump --type FT --gvmi=$i > /tmp/port$i --no_zero || err "mlxdump failed"
+
+    # expect action count+fwd+vlan_pop
+    grep -B7 "outer_headers.dmac_47_16.*:0xe4112233" /tmp/port0 | grep -q "action.*:0x8c"
+    if [ $? != 0 ]; then
+        err "Invalid action in hw rule for vlan pop"
+    fi
+
+    # expect action count+fwd+vlan_push
+    grep -B10 "outer_headers.dmac_47_16.*:0xe4667788" /tmp/port0 | grep -q "action.*:0x10c"
+    if [ $? != 0 ]; then
+        err "Invalid action in hw rule for vlan push"
+    fi
+}
+
 function run() {
     vlan=5
     vlan_dev=${NIC}.$vlan
@@ -33,19 +50,9 @@ function run() {
         action mirred egress redirect dev $REP
     verify_in_hw $vlan_dev 11
 
-    title "verify hw rules"
-    i=0 && mlxdump -d $PCI fsdump --type FT --gvmi=$i > /tmp/port$i --no_zero || err "mlxdump failed"
-
-    # expect action count+fwd+vlan_pop
-    grep -B7 "outer_headers.dmac_47_16.*:0xe4112233" /tmp/port0 | grep -q "action.*:0x8c"
-    if [ $? != 0 ]; then
-        err "Invalid action in hw rule for vlan pop"
-    fi
-
-    # expect action count+fwd+vlan_push
-    grep -B10 "outer_headers.dmac_47_16.*:0xe4667788" /tmp/port0 | grep -q "action.*:0x10c"
-    if [ $? != 0 ]; then
-        err "Invalid action in hw rule for vlan push"
+    mode=`get_flow_steering_mode $NIC`
+    if [ "$mode" == "dmfs" ]; then
+        verify_hw_rules
     fi
 
     title "Add TC drop rule on $vlan_dev"
