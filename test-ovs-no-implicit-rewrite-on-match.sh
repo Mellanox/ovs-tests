8c51b38a6b3f99cf82984859cdc7984ea89937d8
Roi Dayan <roid@mellanox.com>
2020-08-11 17:36:42 +0300

    test-ovs-no-implicit-rewrite-on-match.sh: Force kill for iperf
    We got an issue iperf sw worked and when got offloaded just got stuck.
    Nice kill didnt close iperf so force kill it.
    Also fail the test if iperf failed.
    
    Change-Id: Ic39cf4bae29917adc125da70cfa90a4163ee8670
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- test-ovs-no-implicit-rewrite-on-match.sh
+++ test-ovs-no-implicit-rewrite-on-match.sh
@@ -66,12 +66,14 @@ function test_traffic() {
 	shift
 	local iperf_extra=$@
 
-	timeout 4 iperf -c $FAKE_VM2_IP $iperf_extra -i 999 -t 1
+	timeout -k1 4 iperf -c $FAKE_VM2_IP $iperf_extra -i 999 -t 1
+        [ $? -ne 0 ] && fail "Iperf failed"
 
 	timeout 2 tcpdump -nnei $dev -c 3 'tcp' &
 	tdpid=$!
 
-	timeout 4 iperf -c $FAKE_VM2_IP $iperf_extra -i 999 -t 3 && success || err
+	timeout -k1 4 iperf -c $FAKE_VM2_IP $iperf_extra -i 999 -t 3 && success || err
+        [ $? -ne 0 ] && fail "Iperf failed"
 	check_offloaded_rules 2
 
 	title "Verify with tcpdump"
