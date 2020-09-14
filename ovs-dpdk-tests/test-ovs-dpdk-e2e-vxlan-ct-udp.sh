de1ed3423ebb666f258173af7e75531f435d9b63
Salem Sol <salems@nvidia.com>
2020-09-14 10:19:06 +0300

    ovs-dpdk-tests: Add new ovs-dpdk e2e-cache udp vxlan test
    Added a new OVS-DPDK test for e2e-cache UDP traffic over VxLAN.
    
    Change-Id: Idcd6b9a291ced8952a3d5b5a7e8e2c51bfff272a
    Signed-off-by: Salem Sol <salems@nvidia.com>

---
+++ ovs-dpdk-tests/test-ovs-dpdk-e2e-vxlan-ct-udp.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+#
+# Test OVS-DPDK with vxlan UDP traffic with CT
+#
+# E2E-CACHE
+#
+# Require external server
+#
+# IGNORE_FROM_TEST_ALL
+
+my_dir="$(dirname "$0")"
+. $my_dir/../common.sh
+. $my_dir/common-dpdk.sh
+
+REMOTE_SERVER=${REMOTE_SERVER:-$1}
+REMOTE_NIC=${REMOTE_NIC:-$2}
+require_remote_server
+
+IP=1.1.1.7
+REMOTE=1.1.1.8
+
+LOCAL_TUN=7.7.7.7
+REMOTE_IP=7.7.7.8
+VXLAN_ID=42
+
+config_sriov 2
+enable_switchdev
+require_interfaces REP NIC
+unbind_vfs
+bind_vfs
+
+
+function cleanup_remote() {
+    on_remote ip a flush dev $REMOTE_NIC
+    on_remote ip l del dev vxlan1 &>/dev/null
+}
+
+function cleanup() {
+    ip a flush dev $NIC
+    ip netns del ns0 &>/dev/null
+    ip netns del ns1 &>/dev/null
+    cleanup_remote
+    sleep 0.5
+}
+trap cleanup EXIT
+
+function config() {
+    cleanup
+    echo "Restarting OVS"
+    start_clean_openvswitch
+
+    config_simple_bridge_with_rep 0
+    config_remote_bridge_tunnel $VXLAN_ID $REMOTE_IP
+    config_local_tunnel_ip $LOCAL_TUN br-phy
+    config_ns ns0 $VF $IP
+}
+
+function config_remote() {
+    on_remote ip link del vxlan1 &>/dev/null
+    on_remote ip link add vxlan1 type vxlan id $VXLAN_ID remote $LOCAL_TUN dstport 4789
+    on_remote ip a flush dev $REMOTE_NIC
+    on_remote ip a add $REMOTE_IP/24 dev $REMOTE_NIC
+    on_remote ip a add $REMOTE/24 dev vxlan1
+    on_remote ip l set dev vxlan1 up
+    on_remote ip l set dev $REMOTE_NIC up
+}
+
+function add_openflow_rules() {
+    ovs-ofctl del-flows br-int
+    ovs-ofctl add-flow br-int "arp,actions=NORMAL"
+    ovs-ofctl add-flow br-int "table=0,udp,ct_state=-trk,actions=ct(zone=5, table=1)"
+    ovs-ofctl add-flow br-int "table=1,udp,ct_state=+trk+new,actions=ct(zone=5, commit),NORMAL"
+    ovs-ofctl add-flow br-int "table=1,udp,ct_state=+trk+est,ct_zone=5,actions=normal"
+    echo -e "\nOVS flow rules:"
+    ovs-ofctl dump-flows br-int --color
+}
+
+function run() {
+    config
+    config_remote
+    add_openflow_rules
+
+    echo -e "\nTesting UDP traffic"
+    t=15
+    # traffic
+    ip netns exec ns0 timeout $((t+2)) iperf -s -u &
+    pid1=$!
+    sleep 2
+    on_remote timeout $((t+2)) iperf -c $IP -t $t -P 10 -u &
+    pid2=$!
+
+    # verify pid
+    sleep 5
+    kill -0 $pid2 &>/dev/null
+    if [ $? -ne 0 ]; then
+        err "iperf failed"
+        return
+    fi
+
+    x=$(ovs-appctl dpctl/dump-e2e-stats | grep 'add merged flows messages to HW' | awk '{print $8}')
+    echo "Number of offload messages: ";echo $x
+
+    if [ $x -ne 11 ]; then
+        err "offloads failed"
+    fi
+
+    kill -9 $pid1 &>/dev/null
+    killall iperf &>/dev/null
+    echo "wait for bgs"
+    wait
+
+    sleep 15
+    # check deletion from DB
+    y=$(ovs-appctl dpctl/dump-e2e-stats | grep 'merged flows in e2e cache' | awk '{print $7}')
+    echo "Number of DB entries: ";echo $y
+
+    if [ $y -ne 0 ]; then
+        err "deletion from DB failed"
+    fi
+
+    # check deletion from HW
+    z=$(ovs-appctl dpctl/dump-e2e-stats | grep 'delete merged flows messages to HW' | awk '{print $8}')
+    echo "Number of delete HW messages: ";echo $z
+
+    if [ $z -ne 11 ]; then
+        err "offloads failed"
+    fi
+}
+
+run
+start_clean_openvswitch
+test_done
