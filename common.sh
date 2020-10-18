e7f5a30ca5e7de0cae55e2023e43e24d006d432f
Roi Dayan <roid@nvidia.com>
2020-10-18 10:00:09 +0300

    Add test for TC CT with e2e_cache
    Change-Id: I1500037ce585e1b773d8a0e0c39fc0c29d12d61f
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- common.sh
+++ common.sh
@@ -469,8 +469,14 @@ function reset_tc() {
     for nic1 in $@ ; do
         ethtool_hw_tc_offload $nic1
         tc qdisc del dev $nic1 ingress >/dev/null 2>&1  || true
-        tc qdisc add dev $nic1 ingress
+        tc qdisc add dev $nic1 ingress $TC_ARG
     done
+}
+
+function reset_tc_cacheable() {
+    TC_ARG="cacheable"
+    reset_tc $@
+    unset TC_ARG
 }
 
 function log() {
