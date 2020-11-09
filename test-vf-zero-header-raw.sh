300fddd21e246c43e7fbc94d73b9c022751cdfaf
Roi Dayan <roid@nvidia.com>
2020-11-09 13:05:38 +0200

    test-vf-zero-header-raw.sh: Add scapy warmup
    For slow vms this is needed or first case generate false failure
    because python took a long time.
    
    Change-Id: I36f5078b5a5f80d8381d11ceae5b57639c0a2995
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- test-vf-zero-header-raw.sh
+++ test-vf-zero-header-raw.sh
@@ -17,19 +17,24 @@ bind_vfs
 ifconfig $REP up
 ifconfig $VF up
 
+FAKE_MAC="a2:b4:c0:f0:fc:8b"
+
+function send_pkt() {
+    local D=$1
+    local pkt="Ether(dst=\"$FAKE_MAC\")/$D"
+
+    python -c "from scapy.all import sendp, Ether, IP, Dot1Q; sendp($pkt, iface=\"$VF\")"
+}
 
 function run_test() {
-    D=$1
-    FAKE_MAC="a2:b4:c0:f0:fc:8b"
-
+    local D=$1
     # verify we have packet with fragment offset > 0
     timeout 4 tcpdump -nnvei $REP -c 1 ether "dst $FAKE_MAC and ip[6:2] & 0x1fff > 0" &
     sleep 1
     tdpid=$!
 
-    pkt="Ether(dst=\"$FAKE_MAC\")/$D"
     title "sending $pkt"
-    python -c "from scapy.all import sendp, Ether, IP, Dot1Q; sendp($pkt, iface=\"$VF\")"
+    send_pkt $D
     wait $tdpid
     rc=$?
     if [[ $rc -eq 0 ]]; then
@@ -46,6 +51,11 @@ UDP=17
 
 PROTOS="ICMP TCP UDP"
 SIZES="1 7 8 9"
+
+echo "Sending icmp packet for python scapy warmup"
+# warmup for slow debug VMs
+M1="IP(frag=5,dst=\"2.2.2.2\",proto=1)/(\"X\"*1)"
+send_pkt $M1
 
 for P in $PROTOS; do
     title "Test procotol $P"
