3e35ee2432bc4d16db554fd35f2abba850f81271
Roi Dayan <roid@nvidia.com>
2020-11-19 12:40:52 +0200

    Update udev scripts
    Change-Id: Iad5995379f7cf43cc23e5b93b9afee18a9d53666
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- udev-scripts/vf-net-link-name.sh
+++ udev-scripts/vf-net-link-name.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-SWID=$1
+SWID=$2
 # might be pf0vf1 so only get vf number
-PORT=${2##*f}
-PORT_NAME=$2
+PORT=${1##*f}
+PORT_NAME=$1
+
+# need the PATH for BF ARM lspci to work
+PATH=/bin:/sbin:/usr/bin:/usr/sbin
 
 if [[ "$ID_NET_DRIVER" != *"mlx5"* ]]; then
     exit 1
@@ -11,27 +14,94 @@ fi
 
 is_bf=`lspci -s 00:00.0 2> /dev/null | grep -wq "PCI bridge: Mellanox Technologies" && echo 1 || echo 0`
 if [ $is_bf -eq 1 ]; then
-    echo NAME=${2/vf-1/hpf}
-    exit 0
+	echo NAME=`echo ${1} | sed -e "s/\(pf[[:digit:]]\+\)$/\1hpf/"`
+	exit 0
 fi
 
 # for pf and uplink rep fall to slot or path.
-if [ -n "$ID_NET_NAME_SLOT" ]; then
-    NAME="${ID_NET_NAME_SLOT%%np[[:digit:]]}"
-elif [ -n "$ID_NET_NAME_PATH" ]; then
-    NAME="${ID_NET_NAME_PATH%%np[[:digit:]]}"
+udevversion=`/sbin/udevadm --version`
+skip=0
+if [ "$ID_NET_DRIVER" == "mlx5e_rep" ]; then
+    if [ "$udevversion" == "219" ] || [ "$udevversion" == "229" ]; then
+        skip=1
+    fi
 fi
 
-if [ -n "$NAME" ]; then
-    NAME=`echo $NAME | sed 's/npf.vf/_/'`
-    NAME=`echo $NAME | sed 's/np.v/v/'`
-    echo NAME=$NAME
-    exit
+if [ "$skip" == "0" ]; then
+	if [ -n "$ID_NET_NAME_SLOT" ]; then
+	    NAME="${ID_NET_NAME_SLOT%%np[[:digit:]]}"
+	elif [ -n "$ID_NET_NAME_PATH" ]; then
+	    NAME="${ID_NET_NAME_PATH%%np[[:digit:]]}"
+	fi
+
+	if [ -n "$NAME" ]; then
+	    NAME=`echo $NAME | sed 's/npf.vf/_/'`
+	    NAME=`echo $NAME | sed 's/np.v/v/'`
+	    echo NAME=$NAME
+	    exit
+	fi
 fi
 
-function get_name() {
-    local a=`udevadm info -q property -p /sys/class/net/$1 | grep $2 | cut -d= -f2`
+if [ -z "$SWID" ]; then
+    exit 0
+fi
+
+# for SF mdev devices
+function get_sf_rep_name() {
+    b=`udevadm info -q property -p /sys/bus/pci/devices/$1/net/* | grep "ID_PATH=" | cut -d- -f2 | cut -d: -f2`
+    d=`udevadm info -q property -p /sys/bus/pci/devices/$1/net/* | grep "ID_PATH=" | cut -d- -f2 | cut -d: -f3 | cut -d. -f1`
+    f=`udevadm info -q property -p /sys/bus/pci/devices/$1/net/* | grep "ID_PATH=" | cut -d- -f2 | cut -d: -f3 | cut -d. -f2`
+    echo ${b}_${d}_${f}_${PORT_NAME##*p}
+}
+
+# get phys_switch_id by mdev
+function get_mdev_swid() {
+    rep_name=`cat /sys/bus/mdev/devices/$1/devlink-compat-config/netdev 2>/dev/null`
+    cat /sys/class/net/${rep_name}/phys_switch_id 2>/dev/null
+}
+
+# get phys_port_name by mdev
+function get_mdev_port_name() {
+    rep_name=`cat /sys/bus/mdev/devices/$1/devlink-compat-config/netdev 2>/dev/null`
+    cat /sys/class/net/${rep_name}/phys_port_name 2>/dev/null
+}
+
+# try at most two times
+for cnt in {1..2}; do
+    # wait for mdev to be created
+    sleep 0.5
+    for dev in `ls -l /sys/class/net/*/device | cut -d "/" -f9-`; do
+        if [ -h /sys/bus/mdev/devices/${dev} ]; then
+            for pci in `ls /sys/bus/pci/devices/*`; do
+                # searching for pci dev that owns this mdev
+                if [ -a /sys/bus/pci/devices/${pci}/${dev} ]; then
+                    _swid=`get_mdev_swid $dev`
+                    _portname=`get_mdev_port_name $dev`
+                    if [ "$_swid" = "$SWID" ] && [ "$_portname" = "$PORT_NAME" ]
+                    then
+                        echo "NAME=`get_sf_rep_name $pci`"
+                        exit
+                    fi
+                fi
+            done
+        fi
+    done
+done
+
+# for VFs
+function get_pci_name() {
+    local a=`udevadm info -q property -p /sys/bus/pci/devices/$1/net/* | grep $2 | cut -d= -f2`
     echo ${a%%np[[:digit:]]}
+}
+
+# get phys_switch_id by pci
+function get_pci_swid() {
+    cat /sys/bus/pci/devices/$1/net/*/phys_switch_id | head -1 2>/dev/null
+}
+
+# get phys_port_name by pci
+function get_pci_port_name() {
+    cat /sys/bus/pci/devices/$1/net/*/phys_port_name | head -1 2>/dev/null
 }
 
 # for vf rep get parent slot/path.
@@ -41,19 +111,21 @@ parent_phys_port_name=${parent_phys_port_name//pf}
 parent_phys_port_name="p$parent_phys_port_name"
 # try at most two times
 for cnt in {1..2}; do
-    for i in `ls -1 /sys/class/net/*/phys_switch_id`; do
-        nic=`echo $i | cut -d/ -f 5`
-        _swid=`cat $i`
-        _portname=`cat /sys/class/net/$nic/phys_port_name`
+    for pci in `ls -l /sys/class/net/*/device | cut -d "/" -f9-`; do
+        if [ -h /sys/bus/pci/devices/${pci}/physfn ]; then
+            continue
+        fi
+        _swid=`get_pci_swid $pci`
+        _portname=`get_pci_port_name $pci`
         if [ -z $_portname ]; then
             # no uplink rep so no phys port name
-            _portname=$parent_phys_port_name
+            continue
         fi
         if [ "$_swid" = "$SWID" ] && [ "$_portname" = "$parent_phys_port_name" ]
         then
-            parent_path=`get_name $nic ID_NET_NAME_SLOT`
+            parent_path=`get_pci_name $pci ID_NET_NAME_SLOT`
             if [ -z "$parent_path" ]; then
-                parent_path=`get_name $nic ID_NET_NAME_PATH`
+                parent_path=`get_pci_name $pci ID_NET_NAME_PATH`
             fi
             echo "NAME=${parent_path}_$PORT"
             exit
