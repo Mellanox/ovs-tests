007f66c2306d27e256e909bf34e0bcedb87bd574
Roi Dayan <roid@nvidia.com>
2021-01-12 14:30:04 +0200

    Add new test test-eswitch-toggle-mode-fail.sh
    Change-Id: I764fa23afbd5df860f17142a012f6161efc9654e
    Signed-off-by: Roi Dayan <roid@nvidia.com>

---
+++ test-eswitch-toggle-mode-fail.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Test toggle e-switch mode and expect to fail
+# Purpose to test the error flow. for issue, memleak, etc.
+#
+# Bug SW #2431774: memleak when failing to change to switchdev mode
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+
+enable_legacy
+bind_vfs
+__ignore_errors=1
+switch_mode_switchdev
+__ignore_errors=0
+
+m=`get_eswitch_mode`
+if [ "$m" == "legacy" ]; then
+    success "Expected to fail"
+else
+    err "Expected to fail"
+fi
+
+test_done
