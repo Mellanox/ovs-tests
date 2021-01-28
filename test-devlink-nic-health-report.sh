27b898632c5da523bb8e7abce3d7776fc4e86a02
Roi Dayan <roid@nvidia.com>
2021-01-28 12:35:11 +0200

    Add test test-devlink-nic-health-report.sh
    Change-Id: I3ad7ef5e9ddf784bc92e73ae62aab4c01381a490
    Signed-off-by: Roi Dayan <roid@nvidia.com>

---
+++ test-devlink-nic-health-report.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Test devlink dump nic health report
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+
+enable_legacy $NIC2
+
+items=`devlink -j port show $NIC2 | jq '.port | to_entries[].key'`
+if [ -z "$items" ] || [ "$items" == "null" ]; then
+    fail "Cannot find devlink port for $NIC2"
+fi
+
+for item in $items ; do
+    title "Check health reporters for $item"
+    for report in tx rx; do
+        tmp=`devlink -j health | jq ".health[$item][] | select(.reporter == \"$report\")"`
+        if [ -z "$tmp" ]; then
+            err "Missing devlink $report reporter for $item"
+            continue
+        fi
+    done
+done
+
+test_done
