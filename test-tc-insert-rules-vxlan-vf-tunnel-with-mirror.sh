1f793bc26c8676e8ec44c3bcf409817241d68ab0
Vlad Buslov <vladbu@nvidia.com>
2021-02-22 15:58:35 +0200

    test-tc-insert-rules-vxlan-vf-tunnel-with-mirror.sh: New test
    Verify adding TC encap rule with mirroring which causes NULL-ptr
    dereference on current upstream.
    
    Issue: 2495074
    Change-Id: I82b366274a6a3ac4c6f8d89cee10d16a304c5375
    Signed-off-by: Vlad Buslov <vladbu@nvidia.com>

---
+++ test-tc-insert-rules-vxlan-vf-tunnel-with-mirror.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#
+# Verify encap mirroring rules insertion on setup with VF as tunnel endpoint
+#
+# #2495074: [Upstream][Stacked Devices] Kernel null pointer dereference with vf mirroring
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+config_sriov 3
+enable_switchdev
+REP3=`get_rep 2`
+require_interfaces REP REP2 REP3 NIC
+unbind_vfs
+bind_vfs
+VF3=`get_vf 2`
+
+function test_vxlan_mirror_encap() {
+    local ip_src="20.1.11.1"
+    local ip_dst="20.1.12.1"
+    local vxlan_port="4789"
+
+    vx=vxlan1
+    ip link del $vx >/dev/null 2>&1
+    ip link add $vx type vxlan dev $VF1 dstport $vxlan_port external
+    [ $? -ne 0 ] && err "Failed to create vxlan interface" && return 1
+    ip link set dev $vx up
+    tc qdisc add dev $vx ingress
+
+    ip addr flush dev $VF1
+    ip addr add $ip_src/16 dev $VF1
+    ifconfig $VF1 up
+    ip neigh replace $ip_dst lladdr e4:11:22:11:55:55 dev $VF1
+    ifconfig $NIC up
+
+    reset_tc $NIC $REP
+
+    title "    - encap"
+    tc_filter_success add dev $REP protocol 0x806 parent ffff: prio 1 \
+                      flower \
+                          dst_mac e4:11:22:11:4a:51 \
+                          src_mac e4:11:22:11:4a:50 \
+                      action mirred egress mirror dev $REP3 pipe \
+                      action tunnel_key set \
+                          src_ip $ip_src \
+                          dst_ip $ip_dst \
+                          dst_port $vxlan_port \
+                          id 100 \
+                      action mirred egress redirect dev $vx
+
+    reset_tc $NIC $REP $vx
+    ip neigh del $ip_dst lladdr e4:11:22:11:55:55 dev $VF1
+    ip addr flush dev $VF1
+    ip link del $vx
+}
+
+test_vxlan_mirror_encap
+
+check_for_errors_log
+check_syndrome
+check_kasan
+test_done
