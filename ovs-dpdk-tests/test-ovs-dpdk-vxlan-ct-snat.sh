445dcec6b10f0d7e3bea9e546ee5c7c553e91b1f
Salem Sol <salems@nvidia.com>
2021-03-10 20:27:36 +0200

    ovs-dpdk-tests: Add new ovs-dpdk SNAT tests
    Add new tests to check CT with SNAT functionality,
    for both MT flows and e2e.
    
    Change-Id: I6353fb0440c6493cb1128db981e1475fbf7179da
    Signed-off-by: Salem Sol <salems@nvidia.com>

---
+++ ovs-dpdk-tests/test-ovs-dpdk-vxlan-ct-snat.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+#
+# Test OVS with vxlan traffic and SNAT rules
+#
+# Require external server
+#
+# IGNORE_FROM_TEST_ALL
+
+my_dir="$(dirname "$0")"
+. $my_dir/../common.sh
+. $my_dir/common-dpdk.sh
+
+REMOTE_SERVER=${REMOTE_SERVER:-$1}
+REMOTE_NIC=${REMOTE_NIC:-$2}
+require_remote_server
+
+IP=1.1.1.7
+REMOTE=1.1.1.8
+
+LOCAL_TUN=7.7.7.7
+REMOTE_IP=7.7.7.8
+VXLAN_ID=42
+VXLAN_MAC=e4:11:22:33:44:55
+SNAT_IP=5.5.1.1
+SNAT_ROUTE=5.5.1.0
+
+config_sriov 2
+require_interfaces REP NIC
+unbind_vfs
+bind_vfs
+
+
+function cleanup_remote() {
+    on_remote ip a flush dev $REMOTE_NIC
+    on_remote ip l del dev vxlan1 &>/dev/null
+}
+
+function cleanup() {
+    ip a flush dev $NIC
+    ip netns del ns0 &>/dev/null
+    cleanup_e2e_cache
+    cleanup_remote
+    sleep 0.5
+}
+trap cleanup EXIT
+
+function config() {
+    cleanup
+    set_e2e_cache_enable false
+    echo "Restarting OVS"
+    start_clean_openvswitch
+
+    config_simple_bridge_with_rep 0
+    config_remote_bridge_tunnel $VXLAN_ID $REMOTE_IP
+    config_local_tunnel_ip $LOCAL_TUN br-phy
+    config_ns ns0 $VF $IP
+    ip netns exec ns0 ip r a $SNAT_ROUTE/24 dev $VF
+    ip netns exec ns0 arp -s $SNAT_IP $VXLAN_MAC
+}
+
+function config_remote() {
+    on_remote ip link del vxlan1 &>/dev/null
+    on_remote ip link add vxlan1 type vxlan id $VXLAN_ID remote $LOCAL_TUN dstport 4789
+    on_remote ip a flush dev $REMOTE_NIC
+    on_remote ip a add $REMOTE_IP/24 dev $REMOTE_NIC
+    on_remote ip a add $REMOTE/24 dev vxlan1
+    on_remote ip l set dev vxlan1 address $VXLAN_MAC
+    on_remote ip l set dev vxlan1 up
+    on_remote ip l set dev $REMOTE_NIC up
+}
+
+function add_openflow_rules() {
+    ovs-ofctl add-flow br-int "arp,actions=normal"
+    ovs-ofctl add-flow br-int "icmp,actions=normal"
+    ovs-ofctl add-flow br-int "table=0,ip,ct_state=-trk,actions=ct(table=1)"
+    ovs-ofctl add-flow br-int "table=1,ip,ct_state=+trk+new,actions=ct(commit,nat(src=${SNAT_IP}:2000-2010)),normal"
+    ovs-ofctl add-flow br-int "table=1,ip,ct_state=+trk+est,actions=ct(nat),normal"
+    ovs-ofctl dump-flows br-int --color
+}
+
+function run() {
+    config
+    config_remote
+    add_openflow_rules
+
+    # icmp
+    ip netns exec ns0 ping -q -c 1 -w 1 $REMOTE
+    if [ $? -ne 0 ]; then
+        err "ping failed"
+        return
+    fi
+
+    t=15
+    # traffic
+    ip netns exec ns0 timeout $((t+2)) iperf3 -s &
+    pid1=$!
+    sleep 2
+    on_remote timeout $((t+2)) iperf3 -c $IP -t $t -P 5 &
+    pid2=$!
+
+    # verify pid
+    sleep 2
+    kill -0 $pid2 &>/dev/null
+    if [ $? -ne 0 ]; then
+        err "iperf3 failed"
+        return
+    fi
+
+    sleep $((t-4))
+    # check offloads
+    check_dpdk_offloads $IP
+    check_offloaded_connections 5
+
+    kill -9 $pid1 &>/dev/null
+    killall iperf3 &>/dev/null
+    echo "wait for bgs"
+    wait
+}
+
+run
+start_clean_openvswitch
+test_done
