87e3fcefec9b3ac0a260cd585be7a5ecf5fd98a4
Roi Dayan <roid@nvidia.com>
2021-03-14 15:47:08 +0200

    Add test for bonding in lacp mode
    Change-Id: I4070188bec6378a46137b7bf0397cd17f9c9a7b1
    Signed-off-by: Roi Dayan <roid@nvidia.com>

---
+++ test-vf-lag-lacp.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+#
+# Basic VF LAG test with tc shared block. bonding mode lacp.
+#
+# Bug SW #2336116: [for-upstream bond] Call traces when create "802.3ad" or "balance-xor" bond
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+require_module bonding
+require_interfaces NIC NIC2
+
+config_sriov 2
+# TODO require vf lag support ?
+reset_tc $NIC
+
+local_ip="2.2.2.2"
+remote_ip="2.2.2.3"
+dst_mac="e4:1d:2d:fd:8b:02"
+dst_port=1234
+id=98
+
+function config_shared_block() {
+    for i in bond0 $NIC $NIC2 ; do
+        tc qdisc del dev $i ingress
+        tc qdisc add dev $i ingress_block 22 ingress || err "Failed to add ingress_block"
+    done
+}
+
+function config() {
+    echo "- Config"
+    config_sriov 2
+    config_sriov 2 $NIC2
+    enable_switchdev
+    enable_switchdev $NIC2
+    reset_tc $NIC $NIC2 $REP
+    config_bonding $NIC $NIC2 802.3ad
+    config_shared_block
+}
+
+function clean_shared_block() {
+    for i in bond0 $NIC $NIC2 ; do
+        tc qdisc del dev $i ingress_block 22 ingress &>/dev/null
+    done
+}
+
+function cleanup() {
+    clean_shared_block
+    clear_bonding
+    ifconfig $NIC down
+}
+
+function config_vxlan() {
+    local nic1="bond0"
+    ip link add vxlan1 type vxlan id $id dev $nic1 dstport $dst_port || err "Failed to add vxlan interface"
+    ip link set vxlan1 up
+    ip addr add ${local_ip}/24 dev $nic1
+    tc qdisc add dev vxlan1 ingress
+    ip link set $nic1 up
+    ip n add $remote_ip lladdr $dst_mac dev $nic1 || err "Failed to add neigh entry"
+}
+
+function clean_vxlan() {
+    local nic1="bond0"
+    ip link del dev vxlan1 2> /dev/null
+    ip n del ${remote_ip} dev $nic1 2>/dev/null
+    ip addr flush dev $nic1
+}
+
+function add_vxlan_rule() {
+    local local_ip="$1"
+    local remote_ip="$2"
+
+    echo "local_ip $local_ip remote_ip $remote_ip"
+
+    # tunnel key set
+    ifconfig $NIC up
+    reset_tc $REP $NIC vxlan1
+
+    # encap
+    title "- encap"
+    tc_filter_success add dev $REP protocol arp parent ffff: prio 8 \
+        flower dst_mac $dst_mac skip_sw \
+        action tunnel_key set \
+            id $id src_ip ${local_ip} dst_ip ${remote_ip} dst_port ${dst_port} \
+        action mirred egress redirect dev vxlan1
+
+    # decap
+    title "- decap"
+    tc_filter_success add dev vxlan1 protocol arp parent ffff: prio 9 \
+        flower dst_mac $dst_mac \
+            enc_src_ip $remote_ip \
+            enc_dst_ip $local_ip \
+            enc_dst_port $dst_port \
+            enc_key_id $id \
+        action tunnel_key unset \
+            id $id src_ip ${local_ip} dst_ip ${remote_ip} dst_port ${dst_port} \
+        action mirred egress redirect dev $REP
+
+    reset_tc $REP $NIC vxlan1
+}
+
+function test_add_vxlan_rule() {
+    config_vxlan
+    add_vxlan_rule $local_ip $remote_ip
+    clean_vxlan
+}
+
+function test_add_drop_rule() {
+    tc_filter_success add block 22 protocol arp parent ffff: prio 5 \
+        flower dst_mac $dst_mac action drop
+}
+
+function test_add_redirect_rule() {
+    title "- bond0 -> $REP"
+    tc_filter_success add block 22 protocol arp parent ffff: prio 3 \
+        flower dst_mac $dst_mac \
+        action mirred egress redirect dev $REP
+
+    title "- $REP -> bond0"
+    tc_filter_success add dev $REP protocol arp parent ffff: prio 3 \
+        flower dst_mac $dst_mac skip_sw \
+        action mirred egress redirect dev bond0
+}
+
+function do_cmd() {
+    title $1
+    eval $1
+}
+
+
+trap cleanup EXIT
+cleanup
+config
+fail_if_err
+do_cmd test_add_drop_rule
+do_cmd test_add_redirect_rule
+do_cmd test_add_vxlan_rule
+cleanup
+test_done
