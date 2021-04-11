be2575cf12286d398245faa47a941c347af4058a
Ariel Levkovich <lariel@nvidia.com>
2021-04-11 16:51:03 +0300

    Add new test test-ovs-ct-tcp-ct-state-inv-rel.sh
    Testing offload of ct_state -inv and -rel
    
    Issue: 2595983
    Change-Id: I2d6a5494d15009261ecaaa217e9947fc2469fed9
    Signed-off-by: Ariel Levkovich <lariel@nvidia.com>

---
+++ test-ovs-ct-tcp-ct-state-inv-rel.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+#
+# Test OVS CT ct_state=-rel-inv match with TCP traffic
+#
+# Bug SW #2595983: CT HWOL: not all ct_state()s are supported
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+require_module act_ct
+
+IP1="7.7.7.1"
+IP2="7.7.7.2"
+
+config_sriov 2
+enable_switchdev
+require_interfaces REP REP2
+unbind_vfs
+bind_vfs
+reset_tc $REP
+reset_tc $REP2
+
+function cleanup() {
+    conntrack -F &>/dev/null
+    ip netns del ns0 2> /dev/null
+    ip netns del ns1 2> /dev/null
+    reset_tc $REP
+    reset_tc $REP2
+}
+trap cleanup EXIT
+
+function config_ovs() {
+    echo "setup ovs"
+    start_clean_openvswitch
+    ovs-vsctl add-br br-ovs
+    ovs-vsctl add-port br-ovs $REP
+    ovs-vsctl add-port br-ovs $REP2
+
+    zone=99
+
+    ovs-ofctl del-flows br-ovs
+    ovs-ofctl add-flow br-ovs arp,actions=normal
+    ovs-ofctl add-flow br-ovs "table=0, ip,ct_state=-trk actions=ct(table=1,zone=$zone)"
+    ovs-ofctl add-flow br-ovs "table=1, ip,ct_state=+trk+new actions=ct(zone=$zone, commit),normal"
+    ovs-ofctl add-flow br-ovs "table=1, ip,ct_zone=$zone,ct_state=+trk+est-rel-inv actions=normal"
+
+    ovs-ofctl dump-flows br-ovs --color
+}
+
+function run() {
+    title "Test OVS CT TCP"
+    config_vf ns0 $VF $REP $IP1
+    config_vf ns1 $VF2 $REP2 $IP2
+    config_ovs
+
+    t=12
+
+    title "Run traffic for $t seconds"
+    ip netns exec ns1 timeout $((t+1)) iperf -s &
+    sleep 0.5
+    ip netns exec ns0 timeout $((t+1)) iperf -t $t -c $IP2 -P 3 &
+
+    sleep 2
+    pidof iperf &>/dev/null || err "iperf failed"
+
+    echo "sniff packets on $VF2"
+    ip netns exec ns1 timeout 4 tcpdump -qnnei $VF2 -c 10 tcp &
+    pid1=$!
+
+    echo "sniff packets on $REP"
+    timeout 4 tcpdump -qnnei $REP -c 10 'tcp' &
+    pid=$!
+
+    title "Validate ct state invalid"
+    ovs_dump_tc_flows --names
+    ovs_dump_tc_flows --names | grep -i "in_port(${REP})" | grep -q "ct_state.*-inv" || err "Expected -inv match on ${REP} in_port"
+    ovs_dump_tc_flows --names | grep -i "in_port(${REP2})" | grep -q "ct_state.*-inv" || err "Expected -inv match on ${REP2} in_port"
+
+    ovs_dump_tc_flows --names | grep -i "in_port(${REP})" | grep -q "ct_state.*-rel" || err "Expected -rel match on ${REP} in_port"
+    ovs_dump_tc_flows --names | grep -i "in_port(${REP2})" | grep -q "ct_state.*-rel" || err "Expected -rel match on ${REP2} in_port"
+
+    sleep $t
+    killall -9 iperf &>/dev/null
+    wait $! 2>/dev/null
+
+    title "Verify traffic on $VF2"
+    verify_have_traffic $pid1
+
+    title "Verify no traffic on $REP"
+    verify_no_traffic $pid
+
+    ovs-vsctl del-br br-ovs
+}
+
+cleanup
+run
+test_done
