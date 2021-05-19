a128d33cbc92e5f2a8ab684301db3506db01a032
Dima Chumak <dchumak@nvidia.com>
2021-05-19 07:50:24 +0300

    Add test-tc-hairpin-unbind-vf.sh
    Check that send/receive queues of a hairpin object are destroyed when
    peer_mdev is removed (corresponds to VF in the test).
    
    When hairpin queues aren't destroyed at VF unbinding, it triggers page
    reclaim error:
    
      mlx5_core 0000:08:00.2: reclaim_pages:514:(pid 6103): failed reclaiming pages: err -110
      mlx5_core 0000:08:00.2: mlx5_reclaim_root_pages:653:(pid 6103): failed reclaiming pages (-110) for func id 0x0
      ------------[ cut here ]------------
      FW pages counter is 4 after reclaiming all pages
      ...
      Call Trace:
       ? mlx5_satisfy_startup_pages+0x290/0x290 [mlx5_core]
       ? mlx5_cmd_teardown_hca+0xa2/0xd0 [mlx5_core]
       ? mlx5_cmd_init_hca+0x280/0x280 [mlx5_core]
       mlx5_function_teardown+0x30/0x60 [mlx5_core]
       mlx5_uninit_one+0xa7/0xc0 [mlx5_core]
       remove_one+0x5f/0xc0 [mlx5_core]
       pci_device_remove+0x9f/0x1d0
       device_release_driver_internal+0x1e0/0x490
       unbind_store+0x19f/0x200
       ? sysfs_file_ops+0x170/0x170
       kernfs_fop_write_iter+0x2bc/0x450
    
    Issue: 2661425
    Change-Id: Ia2da72db98fe7f6857409b1a6067254e08cf4fa5
    Signed-off-by: Dima Chumak <dchumak@nvidia.com>

---
+++ test-tc-hairpin-unbind-vf.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Testing hairpin by adding rules to TC
+# Creating rule from P1 -> V1 and then unbinding V1
+#
+# Bug SW #2661425: VF unbind fails at reclaiming pages due to hairpin flow
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+function test_hairpin_unbind_vf() {
+    local nic=$1
+    local vf=$2
+
+    title "Reset TC rules on $nic"
+    reset_tc $nic
+
+    title "Add hairpin rule $nic to $vf"
+    tc_filter_success add dev $nic protocol ip parent ffff: \
+          prio 1 flower skip_sw ip_proto udp \
+          action mirred egress redirect dev $vf
+
+    title "Show TC rules on $nic"
+    tc -s filter show dev $nic ingress
+
+    title "Unbind VFs on $nic"
+    unbind_vfs $nic
+
+    title "Show TC rules on $nic"
+    tc -s filter show dev $nic ingress
+
+    title "Reset TC rules on $nic"
+    reset_tc $nic
+}
+
+start_check_syndrome
+
+title "Test hairpin rules in legacy mode"
+disable_sriov
+enable_sriov
+
+test_hairpin_unbind_vf $NIC $VF
+
+check_syndrome
+test_done
