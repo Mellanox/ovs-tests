0e4994c54337fc51a8716fb9b6adfc67916781be
Roi Dayan <roid@nvidia.com>
2021-05-31 14:38:02 +0300

    Add test-eswitch-reload-modules-set-legacy.sh
    Change-Id: Id2fbcd46232aca015ee32531737b448c1fea504e
    Signed-off-by: Roi Dayan <roid@nvidia.com>

---
+++ test-eswitch-reload-modules-set-legacy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Test reload mlx5 during mode change to legacy modules multiple times
+#
+# Unload modules can race with any other action like
+# - set mode legacy
+# - set mode switchdev
+# - create SFs
+# - etc
+#
+# Bug SW #2661537: [SF] Call Trace and Syndrome and memory leak with kernel panic when creating SFs and then doing restart driver
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+function reload2() {
+    sleep ".$RANDOM"
+    __ignore_errors=1
+    reload_modules
+    __ignore_errors=0
+    check_kasan
+}
+
+function do_test() {
+    enable_legacy &
+    reload2
+    wait
+    set_macs 2
+    enable_switchdev
+}
+
+config_sriov
+enable_switchdev
+for i in `seq 5`; do
+    title "test iteration $i"
+    do_test
+    sleep 2
+done
+
+test_done
