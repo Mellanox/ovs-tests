a0e02d3e4239ccec75ea8128fac6e118cff745e1
Mahmoud AbuZant <mahmoudabuz@nvidia.com>
2021-06-01 11:49:38 +0300

    test-eswitch-sf.sh: use mlxdevm command if ofed driver
    ofed 5.4 supports creating sf using mlxdevm.
    
    Issue: 2675729
    Change-Id: Icf4dc2292e1302e844ac2c3b8197bbcbef8dffc6
    Signed-off-by: Mahmoud AbuZant <mahmoudabuz@nvidia.com>

--- test-eswitch-sf.sh
+++ test-eswitch-sf.sh
@@ -8,14 +8,20 @@
 my_dir="$(dirname "$0")"
 . $my_dir/common.sh
 
+cmd="devlink"
+
+if is_ofed ; then
+   cmd="mlxdevm"
+fi
+
 function sf_port_add_del_test() {
     title "Test sf port add delete commands"
 
-    devlink port add pci/$PCI flavour pcisf pfnum 0 sfnum 88 || fail "Failed to add SF"
+    $cmd port add pci/$PCI flavour pcisf pfnum 0 sfnum 88 || fail "Failed to add SF"
     sleep 1
     local rep=`devlink port show | grep "pfnum 0 sfnum 88" | grep -E -o "netdev [a-z0-9]+" | awk {'print $2'}`
-    devlink port show $rep || err "Failed to show SF"
-    devlink port del $rep || err "Failed to del SF"
+    $cmd port show $rep || err "Failed to show SF"
+    $cmd port del $rep || err "Failed to del SF"
 }
 
 verify_mlxconfig_for_sf
