72e5c4421f2c839ed2055aa2db51f729a19dd08d
Roi Dayan <roid@nvidia.com>
2021-06-03 16:04:58 +0300

    Add test test-vf-lag-reload.sh
    Change-Id: I5f5d72d5a7e9084a1f74ad898406a642b9065176
    Signed-off-by: Roi Dayan <roid@nvidia.com>

---
+++ test-vf-lag-reload.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Basic VF LAG test with tc shared block
+#
+# Bug SW #2677225: [CentOS 7.6] driver restart with bonding configured causes system panic
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+require_module bonding
+require_interfaces NIC NIC2
+
+function config_shared_block() {
+    for i in bond0 $NIC $NIC2 ; do
+        tc qdisc del dev $i ingress
+        tc qdisc add dev $i ingress_block 22 ingress || err "Failed to add ingress_block"
+    done
+}
+
+function config() {
+    echo "- Config"
+    config_sriov 2
+    config_sriov 2 $NIC2
+    enable_switchdev
+    enable_switchdev $NIC2
+    config_bonding $NIC $NIC2
+    config_shared_block
+}
+
+function clean_shared_block() {
+    for i in bond0 $NIC $NIC2 ; do
+        tc qdisc del dev $i ingress_block 22 ingress &>/dev/null
+    done
+}
+
+function cleanup() {
+    clean_shared_block
+    clear_bonding
+}
+
+trap cleanup EXIT
+cleanup
+config
+fail_if_err
+reload_modules
+cleanup
+test_done
