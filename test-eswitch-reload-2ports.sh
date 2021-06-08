02596c1b3501357c248fd81674bc7c9dac2fcff1
Roi Dayan <roid@nvidia.com>
2021-06-08 15:45:56 +0300

    Add test-eswitch-reload-2ports.sh
    Change-Id: I9b694205567f24bd515b3216c67cf6dfee736015
    Signed-off-by: Roi Dayan <roid@nvidia.com>

---
+++ test-eswitch-reload-2ports.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Test reload mlx5 modules while both ports in switchdev
+#
+# Bug SW #2681985: [ASAP, OFED5.4] call trace and memory leak appeared after stop driver in switchdev mode
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+disable_sriov
+enable_sriov
+enable_switchdev
+enable_switchdev $NIC2
+reload_modules
+disable_sriov_port2
+enable_switchdev
+
+check_kasan
+test_done
