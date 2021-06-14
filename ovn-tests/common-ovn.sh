235de6d992970c778453950cce4a092ce85c97c7
Mamduh Alassi <mamduhala@nvidia.com>
2021-06-14 09:37:48 +0300

    ovn-tests: Add ovn vf to vf ping traffic test
    ovn-tests/test-ovn-geneve-vf-vf-ping.sh add test that create a network
    topology that uses OVN as SDN controller for OVS, where ovn
    creates the flow rules.
    
    In addtion to ovn utilities ovn-tests/common-ovn.sh
    
    Change-Id: Ied416de87b3a71791be100904107db73a086ca47
    Signed-off-by: Mamduh Alassi <mamduhala@nvidia.com>

---
+++ ovn-tests/common-ovn.sh
@@ -0,0 +1,93 @@
+OVN_BRIDGE_INT="br-int"
+OVN_SYSTEM_ID=$(hostname)
+OVN_CTL="/usr/share/ovn/scripts/ovn-ctl"
+
+function require_ovn() {
+    [ "${OVN}" != 1 ] && fail "OVN NOT CONFIGURED"
+}
+
+function ovn_start_northd_central() {
+    local ip=$1
+
+    $OVN_CTL start_northd
+    ovn-nbctl set-connection ptcp:6641:$ip
+    ovn-sbctl set-connection ptcp:6642:$ip
+}
+
+function ovn_stop_northd_central() {
+    $OVN_CTL stop_northd
+}
+
+function ovn_start_ovn_controller() {
+    $OVN_CTL start_controller
+}
+
+function ovn_stop_ovn_controller() {
+    $OVN_CTL stop_controller
+}
+
+function ovn_set_ovs_config() {
+    local system_id=$1
+    local ovn_remote_ip=$2
+    local encap_ip=$3
+    local encap_type=$4
+
+    ovs-vsctl set open . external-ids:system-id=$system_id
+    ovs-vsctl set open . external-ids:ovn-remote=tcp:$ovn_remote_ip:6642
+    ovs-vsctl set open . external-ids:ovn-encap-ip=$encap_ip
+    ovs-vsctl set open . external-ids:ovn-encap-type=$encap_type
+}
+
+function ovn_remove_ovs_config() {
+    ovs-vsctl remove open . external-ids system-id
+    ovs-vsctl remove open . external-ids ovn-remote
+    ovs-vsctl remove open . external-ids ovn-encap-ip
+    ovs-vsctl remove open . external-ids ovn-encap-type
+}
+
+function ovn_add_switch() {
+    local switch=$1
+
+    ovn-nbctl ls-add $switch
+}
+
+function ovn_add_port_to_switch() {
+    local switch=$1
+    local port=$2
+
+    ovn-nbctl lsp-add $switch $port
+}
+
+function ovn_set_switch_port_addresses() {
+    local port=$1
+    local mac=$2
+    # IP is optional
+    local ip=$3
+
+    ovn-nbctl lsp-set-addresses $port "$mac $ip"
+}
+
+function ovn_delete_switch_port() {
+    local port=$1
+
+    ovn-nbctl lsp-del $port
+}
+
+function ovn_delete_switch() {
+    local switch=$1
+    ovn-nbctl ls-del $switch
+}
+
+function ovs_add_port_to_switch() {
+    local br=$1
+    local port=$2
+
+    ovs-vsctl add-port $br $port
+}
+
+function ovn_bind_ovs_port() {
+    local ovs_port=$1
+    local ovn_port=$2
+
+    ovs-vsctl set Interface $ovs_port external_ids:iface-id=$ovn_port
+}
---
+++ ovn-tests/test-ovn-geneve-vf-vf-ping.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+#
+# Test traffic between VFs configured with OVN and OVS
+#
+# Scrum Task #2686101: Implement ovn ping test with VFs
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/../common.sh
+. $my_dir/common-ovn.sh
+
+require_ovn
+
+# IPs and MACs
+IP1="7.7.7.1"
+IP2="7.7.7.2"
+
+MAC1="50:54:00:00:00:01"
+MAC2="50:54:00:00:00:02"
+
+# switch and ports
+SWITCH="sw0"
+PORT1="sw0-port1"
+PORT2="sw0-port2"
+
+OVN_CENTRAL_IP="127.0.0.1"
+OVN_TUNNEL="geneve"
+
+# stop OVN, clean namespaces, ovn network topology, and ovs br-int interfaces
+function cleanup() {
+    # Remove OVN topology
+    ovn_delete_switch_port $PORT1
+    ovn_delete_switch_port $PORT2
+    ovn_delete_switch $SWITCH
+
+    # Stop ovn
+    ovn_remove_ovs_config
+    ovn_stop_ovn_controller
+    ovn_stop_northd_central
+
+    # Clean namespaces
+    ip netns del ns0 2>/dev/null
+    ip netns del ns1 2>/dev/null
+
+    # Remove IPs from VFs
+    for i in $VF $VF2; do
+        ifconfig $i 0 &>/dev/null
+    done
+
+    # Clean ovs br-int
+    ovs_clear_bridges
+}
+
+function pre_test() {
+    # Verify required ENV vars
+    test -z "$NIC" && fail "Missing NIC"
+    test -z "$VF" && fail "Missing VF"
+    test -z "$REP" && fail "Missing REP"
+    test -z "$VF2" && fail "Missing VF2"
+    test -z "$REP2" && fail "Missing REP2"
+
+    # Verfiy NIC
+    require_interfaces NIC
+
+    # switchdev mode for NIC
+    unbind_vfs
+    enable_switchdev
+    bind_vfs
+
+    # Verify VFs and REPs
+    require_interfaces VF VF2 REP REP2
+
+    # Start OVN
+    ovn_start_northd_central $OVN_CENTRAL_IP
+    ovn_start_ovn_controller
+    ovn_set_ovs_config $OVN_SYSTEM_ID $OVN_CENTRAL_IP $OVN_CENTRAL_IP $OVN_TUNNEL
+}
+
+function run_test() {
+    # Add network topology to OVN
+    ovn_add_switch $SWITCH
+    ovn_add_port_to_switch $SWITCH $PORT1
+    ovn_add_port_to_switch $SWITCH $PORT2
+    ovn_set_switch_port_addresses $PORT1 $MAC1 $IP1
+    ovn_set_switch_port_addresses $PORT2 $MAC2 $IP2
+
+    ovn-nbctl show
+
+    # Add REP to OVS
+    ovs_add_port_to_switch $OVN_BRIDGE_INT $REP
+    ovs_add_port_to_switch $OVN_BRIDGE_INT $REP2
+
+    ovs-vsctl show
+
+    # Bind OVS ports to OVN
+    ovn_bind_ovs_port $REP $PORT1
+    ovn_bind_ovs_port $REP2 $PORT2
+
+    ovn-sbctl show
+
+    # Move VFs to namespaces and set MACs and IPS
+    config_vf ns0 $VF $REP $IP1 $MAC1
+    config_vf ns1 $VF2 $REP2 $IP2 $MAC2
+
+    # Traffic between VFs
+    ip netns exec ns0 ping -w 4 $IP2 && success || err
+}
+
+cleanup
+pre_test
+
+# trap for existing script to clean up
+trap cleanup EXIT
+
+title "Test OVN ping $VF($IP1) -> $VF2($IP2)"
+start_check_syndrome
+run_test
+
+check_syndrome
+
+test_done
