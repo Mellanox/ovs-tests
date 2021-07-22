942b6bbb64bc5af1b12a2515b5c849cf9167ac2c
Chris Mi <cmi@nvidia.com>
2021-07-22 09:38:13 +0300

    test-ovs-sflow-vxlan.sh: New test for sflow vxlan offload
    Change-Id: Iaa1be9c6c1d1a47c74b302f158098b90bc1191fb
    Signed-off-by: Chris Mi <cmi@nvidia.com>

---
+++ test-ovs-sflow-vxlan.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+#
+# Test OVS sFlow
+#
+# The test setup is exactly same as this one:
+#   http://docs.openvswitch.org/en/latest/howto/sflow/
+# sflowtool must be installed on Monitoring host to receive the sflow.
+# You can download it from "from git@github.com:sflow/sflowtool.git".
+# sFlow will create a UDP connection to send the sampled packets.
+# So SFLOW_AGENT on Host 1 should be able to create a UDP connection
+# to SFLOW_TARGET on Monitoring Host. It doesn't matter if SFLOW_AGENT
+# is mellanox port or not.
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+#
+# It is eth1 on Host 1 in the diagram of
+#   http://docs.openvswitch.org/en/latest/_images/sflow.png
+#
+SFLOW_AGENT=${SFLOW_AGENT:-$1}
+
+# IP address of eth0 on Monitoring Host
+SFLOW_TARGET=${REMOTE_SERVER:-$2}
+
+# default port number
+SFLOW_PORT=6343
+
+# trunc size
+SFLOW_HEADER=96
+
+IP1="1.1.1.7"
+IP2="1.1.1.8"
+
+LOCAL_TUN=7.7.7.7
+REMOTE_IP=7.7.7.8
+VXLAN_ID=42
+
+log "Remote server $REMOTE_SERVER"
+on_remote true || fail "Remote command failed"
+on_remote which sflowtool || \
+    fail "sflowtool missing on remote host $REMOTE_SERVER"
+on_remote pkill sflowtool
+
+config_sriov 2
+enable_switchdev
+require_interfaces REP
+unbind_vfs
+bind_vfs
+reset_tc $REP
+
+function cleanup_remote() {
+    on_remote ip a flush dev $REMOTE_NIC
+    on_remote ip l del dev vxlan1 &>/dev/null
+}
+
+function cleanup() {
+    ovs_conf_remove tc-policy &>/dev/null
+    ip netns del ns0 2> /dev/null
+    reset_tc $REP
+    cleanup_remote
+}
+trap cleanup EXIT
+
+function config_ovs() {
+    title "setup ovs"
+
+    SFLOW_SAMPLING=$1
+
+    start_clean_openvswitch
+    ifconfig $NIC $LOCAL_TUN/24 up
+    ovs-vsctl add-br br-ovs
+    ovs-vsctl add-port br-ovs $REP
+    ovs-vsctl add-port br-ovs vxlan1 -- set interface vxlan1 type=vxlan options:local_ip=$LOCAL_TUN options:remote_ip=$REMOTE_IP options:key=$VXLAN_ID options:dst_port=4789
+
+    title "create sFlow"
+    ovs-vsctl -- --id=@sflow create sflow agent=\"$SFLOW_AGENT\" \
+              target=\"$SFLOW_TARGET:$SFLOW_PORT\" header=$SFLOW_HEADER \
+              sampling=$SFLOW_SAMPLING polling=10 \
+              -- set bridge br-ovs sflow=@sflow
+
+    ovs-vsctl list sflow
+}
+
+function config_remote() {
+    on_remote ip link del vxlan1 &>/dev/null
+    on_remote ip link add vxlan1 type vxlan id $VXLAN_ID dev $REMOTE_NIC dstport 4789
+    on_remote ip a flush dev $REMOTE_NIC
+    on_remote ip a add $REMOTE_IP/24 dev $REMOTE_NIC
+    on_remote ip a add $IP2/24 dev vxlan1
+    on_remote ip l set dev vxlan1 up
+    on_remote ip l set dev $REMOTE_NIC up
+}
+
+function verify_ovs() {
+    count=$(ovs_dump_ovs_flows | grep 0x0800 | grep sFlow | wc -l)
+    if (( count != 2 )); then
+        ovs_dump_ovs_flows --names
+        err "No ovs sample offloaded rules"
+    fi
+}
+
+function verify_tc_policy_skip_hw() {
+    count=$(ovs_dump_tc_flows | grep 0x0800 | grep sFlow | wc -l)
+    if (( count != 2 )); then
+        ovs_dump_tc_flows --names
+        err "No tc sample offloaded rules"
+    fi
+}
+
+function verify_tc_policy_none() {
+    count=$(ovs_dump_offloaded_flows | grep 0x0800 | grep sFlow | wc -l)
+    if (( count != 2 )); then
+        ovs_dump_offloaded_flows --names
+        err "No offloaded sample offloaded rules"
+    fi
+}
+
+function run() {
+    local file=/tmp/sflow.txt
+    local t=10
+    interval=$1
+
+    ssh2 $SFLOW_TARGET timeout $((t+2)) sflowtool -p $SFLOW_PORT \
+        -L localtime,srcIP,dstIP > $file&
+    sleep 1
+
+    title "run ping for $t seconds"
+    ip netns exec ns0 ping $IP2 -q -i $interval -w $t
+
+    wait
+
+    if grep $IP1 $file | grep $IP2 > /dev/null; then
+        success2 "get the expected IP addresses: $IP1, $IP2"
+    else
+        err "fail to get the expected IP addresses"
+    fi
+
+    #
+    # If packet interval is 0.001, send 10 seconds, the total packet number
+    # is 10 / 0.001 * 2 = 20000, (REP and vxlan). If the sampling rate is 1/2,
+    # we expect to receive 10000 sFlow packets.
+    #
+    n=$(awk 'END {print NR}' $file)
+    expected=$(echo $t/$interval*2/$SFLOW_SAMPLING | bc)
+    deviation=$((expected/10)) # 10% deviation
+
+    str="get $n packets, expected $expected"
+    (( n >= expected - deviation && n <= expected + deviation )) && \
+        success2 $str || err $str
+}
+
+config_vf ns0 $VF $REP $IP1
+config_remote
+start_clean_openvswitch
+
+title "Test OVS sFlow without offload"
+ovs_conf_remove hw-offload
+ovs_conf_remove tc-policy
+config_ovs 2
+run 0.001
+verify_ovs
+
+title "Test OVS sFlow with tc-policy=skip_hw"
+ovs_conf_set hw-offload true
+ovs_conf_set tc-policy skip_hw
+config_ovs 2
+run 0.001
+verify_tc_policy_skip_hw
+
+title "Test OVS sFlow with tc-policy=none sampling-2"
+ovs_conf_set hw-offload true
+ovs_conf_set tc-policy none
+config_ovs 2
+run 0.001
+verify_tc_policy_none
+
+title "Test OVS sFlow with tc-policy=none sampling=1"
+config_ovs 1
+run 1
+verify_tc_policy_none
+
+ovs_conf_remove tc-policy
+start_clean_openvswitch
+test_done
