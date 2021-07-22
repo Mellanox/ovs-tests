bc3d028f9c38633361578785c8adf0f42d9ebc4f
Roi Dayan <roid@nvidia.com>
2021-07-22 10:56:44 +0300

    common.sh: Remove unused and redundant ovs_dump_tc_flows_only()
    Change-Id: Ifb89594b0fa04219bedee680ba19207f403443fe
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- common.sh
+++ common.sh
@@ -1294,11 +1294,6 @@ function ovs_dump_tc_flows() {
     [[ $? -ne 0 ]] && ovs-appctl dpctl/dump-flows $args type=offloaded
 }
 
-function ovs_dump_tc_flows_only() {
-    local args=$@
-    ovs-appctl dpctl/dump-flows $args type=tc 2>/dev/null
-}
-
 function ovs_dump_offloaded_flows() {
     local args=$@
     ovs-appctl dpctl/dump-flows $args type=offloaded 2>/dev/null
