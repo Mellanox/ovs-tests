60de97541327bacbf26ddfccda543df282bc5dce
Vlad Buslov <vladbu@nvidia.com>
2021-08-18 16:12:23 +0300

    test-bridge-lag-basic.sh: Increase ageing timeout
    Recent change set timeout to 2sec, which can be too aggressive when
    notification needs to be sent between eswitches, as is the case when LAG is
    active. Set it to 3 seconds for this test.
    
    Change-Id: I87824063f2bb390106d84f2991d09dff56a6acfc
    Signed-off-by: Vlad Buslov <vladbu@nvidia.com>

--- test-bridge-lag-basic.sh
+++ test-bridge-lag-basic.sh
@@ -84,6 +84,8 @@ on_remote ip link set bond0.2 up
 
 sleep 1
 
+# Default 2sec ageing timeout is too aggressive when notifying between esws
+ip link set name $br type bridge ageing_time 300
 ip link set tst1 type bridge vlan_filtering 1
 bridge vlan add dev $REP vid 2 pvid untagged
 bridge vlan add dev $REP2 vid 2
