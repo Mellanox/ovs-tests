4cce379673e2e7a9f583695a5b3fba044cf9e032
Mahmoud AbuZant <mahmoudabuz@nvidia.com>
2021-08-22 14:48:35 +0300

    test-vf-lag-xmit-hash-policy.sh: add new test
    Check bond with different xmit hash policy.
    
    Issue: 2780336
    Change-Id: Ia336be3c1e35a56e3965ca00616f4bb515bedb70
    Signed-off-by: Mahmoud AbuZant <mahmoudabuz@nvidia.com>

---
+++ test-vf-lag-xmit-hash-policy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Check configuring bond with different xmit hash policies.
+# Bond modes are balance-xor and 802.3ad.
+#
+# Bug SW #2780336: Kernel panic and call trace appears in mlx5_del_flow_rules when creating vf-lag with xmit hash policy layer2+3
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+require_module bonding
+
+function config() {
+    config_sriov 2
+    config_sriov 2 $NIC2
+    enable_switchdev
+    enable_switchdev $NIC2
+}
+
+function cleanup() {
+    clear_bonding
+    config_sriov 0 $NIC2
+}
+
+function check_bond_xmit_hash_policy() {
+    for mode in balance-xor 802.3ad; do
+        for policy in layer2 layer2+3 layer3+4 encap2+3 encap3+4; do
+            title "Checking bond mode $mode xmit hash policy $policy"
+            config_bonding $NIC $NIC2 $mode $policy
+            clear_bonding
+        done
+    done
+}
+
+trap cleanup EXIT
+cleanup
+config
+check_bond_xmit_hash_policy
+test_done
