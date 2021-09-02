26e4aa80f221d81145c67b7b154b0bf0c53836c0
Mamduh Alassi <mamduhala@nvidia.com>
2021-09-02 14:28:29 +0300

    ovn-tests: Add test 2 isolated switches on 2 nodes
    Change-Id: I7d1b6c756085233eb4d7e188c432faa9dcaa74a1
    Signed-off-by: Mamduh Alassi <mamduhala@nvidia.com>

---
+++ ovn-tests/test-ovn-geneve-2-switches-2-nodes-pf-tunnel.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+#
+# Verify no between VFs on different nodes configured with OVN 2 isolated switches
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/../common.sh
+. $my_dir/common-ovn.sh
+
+require_remote_server
+require_ovn
+
+# IPs and MACs
+IP1="7.7.7.1"
+IP2="7.7.7.2"
+
+MAC1="50:54:00:00:00:01"
+MAC2="50:54:00:00:00:02"
+
+# Ports
+PORT1="sw0-port1"
+PORT2="sw1-port1"
+
+OVN_REMOTE_SYSTEM_ID=$(get_remote_hostname)
+
+# stop OVN, clean namespaces, ovn network topology, and ovs br-int interfaces
+function cleanup() {
+    # Remove OVN topology
+    ovn_destroy_topology $TOPOLOGY_2_SWITCHES
+
+    # Stop ovn
+    ovn_remove_ovs_config
+    ovn_stop_ovn_controller
+    ovn_stop_northd_central
+
+    # Clean namespaces
+    ip netns del ns0 2>/dev/null
+
+    unbind_vfs
+    bind_vfs
+
+    ifconfig $NIC 0
+
+    ovs_clear_bridges
+
+    # Clean up on remote
+    on_remote_exec "
+    ovn_remove_ovs_config
+    ovn_stop_ovn_controller
+
+    ip netns del ns0 2>/dev/null
+
+    unbind_vfs
+    bind_vfs
+
+    ifconfig $NIC 0
+
+    ovs_clear_bridges
+    "
+}
+
+function config() {
+    # Verify NIC
+    require_interfaces NIC
+
+    # switchdev mode for NIC
+    enable_switchdev
+    bind_vfs
+
+    # Verify VFs and REPs
+    require_interfaces VF REP
+
+    # Start OVN
+    ifconfig $NIC $OVN_CENTRAL_IP
+    ovn_start_northd_central $OVN_CENTRAL_IP
+    ovn_start_ovn_controller
+    ovn_set_ovs_config $OVN_SYSTEM_ID $OVN_CENTRAL_IP $OVN_CENTRAL_IP $TUNNEL_GENEVE
+}
+
+function config_remote() {
+    on_remote_exec "
+    # Verify NIC
+    require_interfaces NIC
+
+    # switchdev mode for NIC
+    enable_switchdev
+    bind_vfs
+
+    # Verify VFs and REPs
+    require_interfaces VF REP
+
+    # Start OVN
+    ifconfig $NIC $OVN_REMOTE_CONTROLLER_IP
+    ovn_set_ovs_config $OVN_REMOTE_SYSTEM_ID $OVN_CENTRAL_IP $OVN_REMOTE_CONTROLLER_IP $TUNNEL_GENEVE
+    ovn_start_ovn_controller
+    "
+}
+
+function pre_test() {
+    config
+    config_remote
+}
+
+function run_test() {
+    # Add network topology to OVN
+    ovn_create_topology $TOPOLOGY_2_SWITCHES
+
+    # Add REP to OVS
+    ovs_add_port_to_switch $OVN_BRIDGE_INT $REP
+    on_remote_exec "ovs_add_port_to_switch $OVN_BRIDGE_INT $REP"
+
+    ovs-vsctl show
+
+    # Bind OVS ports to OVN
+    ovn_bind_ovs_port $REP $PORT1
+    on_remote_exec "ovn_bind_ovs_port $REP $PORT2"
+
+    ovn-sbctl show
+
+    # Move VFs to namespaces and set MACs and IPS
+    config_vf ns0 $VF $REP $IP1 $MAC1
+    on_remote_exec "
+    config_vf ns0 $VF $REP $IP2 $MAC2
+    "
+
+    title "Test no traffic between $VF($IP1) -> $VF2($IP2)"
+    ip netns exec ns0 ping -w 4 $IP2 && err || success "No Connection"
+}
+
+cleanup
+
+trap cleanup EXIT
+
+pre_test
+start_check_syndrome
+run_test
+
+check_syndrome
+
+cleanup
+trap - EXIT
+
+test_done
