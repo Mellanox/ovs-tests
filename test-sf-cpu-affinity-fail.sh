b310eeaf8ff3a03f684c7e570bad37d471aa0af0
Mahmoud AbuZant <mahmoudabuz@nvidia.com>
2021-09-05 16:05:37 +0300

    test-sf-cpu-affinity-fail.sh: add new test
    Test set cpu affinity for a sf over a disabled cpu should fail.
    
    Issue: 2793688
    Change-Id: I83ac9d2c3934100fe69ad744aa60d34d5018d356
    Signed-off-by: Mahmoud AbuZant <mahmoudabuz@nvidia.com>

---
+++ test-sf-cpu-affinity-fail.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#
+# Test set cpu affinity for a sf over a disabled cpu should fail.
+# [MLNX OFED] Bug SW #2793688: [ASAP, OFED 5.5, SFs] set cpu affinity for a sf over a disabled cpu should fail
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+. $my_dir/common-sf.sh
+
+function cleanup() {
+    disable_irq_reguest_debug
+    enable_cpu1
+    remove_sfs >/dev/null
+}
+
+trap cleanup EXIT
+
+function disable_cpu1() {
+    echo 0 > /sys/bus/cpu/devices/cpu1/online
+}
+
+function enable_cpu1() {
+    echo 1 > /sys/bus/cpu/devices/cpu1/online
+}
+
+function config() {
+    title "Config"
+    enbale_irq_reguest_debug
+    create_sfs 1
+
+    title "SFs Netdev Rep Info"
+    SF=`sf_get_netdev 1`
+    SF_DEV=`sf_get_dev 1`
+    echo "SF: $SF, DEV: $SF_DEV"
+
+    title "Disabling CPU #1"
+    disable_cpu1
+}
+
+function test_cpu_affinity_fail() {
+    title "Trying to set SF CPU affinity to CPU #1"
+    sf_set_cpu_affinity $SF_DEV 1
+
+    if [ "$?" != "0" ]; then
+        success
+    else
+        err "Expected to fail"
+    fi
+
+}
+
+enable_norep_switchdev $NIC
+config
+test_cpu_affinity_fail
+cleanup
+trap - EXIT
+test_done
