ebfc2bbe717da1bcc2913a6ea27ef42551968960
Mamduh Alassi <mamduhala@nvidia.com>
2021-09-07 16:01:09 +0300

    ovn-tests: Add TCP6 and UDP6 checks for OVN router and 2 switches on 2 nodes test
    Change-Id: Ide79f173b49b2fa92d34243a361f01898b0a4342
    Signed-off-by: Mamduh Alassi <mamduhala@nvidia.com>

--- ovn-tests/test-ovn-geneve-single-router-2-switches-2-nodes-pf-tunnel.sh
+++ ovn-tests/test-ovn-geneve-single-router-2-switches-2-nodes-pf-tunnel.sh
@@ -17,13 +17,18 @@ SWITCH2=$(ovn_get_switch_name_with_vif_port $TOPOLOGY 1)
 IP_GW1=$(ovn_get_switch_gateway_ip $TOPOLOGY $SWITCH1)
 IP_GW2=$(ovn_get_switch_gateway_ip $TOPOLOGY $SWITCH2)
 
+IP_V6_GW1=$(ovn_get_switch_gateway_ipv6 $TOPOLOGY $SWITCH1)
+IP_V6_GW2=$(ovn_get_switch_gateway_ipv6 $TOPOLOGY $SWITCH2)
+
 PORT1=$(ovn_get_switch_vif_port_name $TOPOLOGY $SWITCH1)
 MAC1=$(ovn_get_switch_port_mac $TOPOLOGY $SWITCH1 $PORT1)
 IP1=$(ovn_get_switch_port_ip $TOPOLOGY $SWITCH1 $PORT1)
+IP_V6_1=$(ovn_get_switch_port_ipv6 $TOPOLOGY $SWITCH1 $PORT1)
 
 PORT2=$(ovn_get_switch_vif_port_name $TOPOLOGY $SWITCH2)
 MAC2=$(ovn_get_switch_port_mac $TOPOLOGY $SWITCH2 $PORT2)
 IP2=$(ovn_get_switch_port_ip $TOPOLOGY $SWITCH2 $PORT2)
+IP_V6_2=$(ovn_get_switch_port_ipv6 $TOPOLOGY $SWITCH2 $PORT2)
 
 # stop OVN, clean namespaces, ovn network topology, and ovs br-int interfaces
 function cleanup() {
@@ -124,10 +129,14 @@ function run_test() {
     # Move VFs to namespaces and set MACs and IPS
     config_vf ns0 $VF $REP $IP1 $MAC1
     ip netns exec ns0 ip route add default via $IP_GW1 dev $VF
+    ip netns exec ns0 ip -6 addr add $IP_V6_1/124 dev $VF
+    ip netns exec ns0 ip -6 route add default via $IP_V6_GW1 dev $VF
 
     on_remote_exec "
     config_vf ns0 $VF $REP $IP2 $MAC2
     ip netns exec ns0 ip route add default via $IP_GW2 dev $VF
+    ip netns exec ns0 ip -6 addr add $IP_V6_2/124 dev $VF
+    ip netns exec ns0 ip -6 route add default via $IP_V6_GW2 dev $VF
     "
 
     title "Test ICMP traffic between $VF($IP1) -> $VF2($IP2) offloaded"
@@ -138,6 +147,12 @@ function run_test() {
 
     title "Test UDP traffic between $VF($IP1) -> $VF2($IP2) offloaded"
     check_remote_udp_traffic_offload $REP ns0 ns0 $IP2
+
+    title "Test TCP6 traffic between $VF($IP_V6_1) -> $VF2($IP_V6_2) offloaded"
+    check_remote_tcp6_traffic_offload $REP ns0 ns0 $IP_V6_2
+
+    title "Test UDP6 traffic between $VF($IP_V6_1) -> $VF2($IP_V6_2) offloaded"
+    check_remote_udp6_traffic_offload $REP ns0 ns0 $IP_V6_2
 }
 
 cleanup
