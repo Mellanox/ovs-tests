35fd8310b902bcb99f2adc5140648d69e86146fc
Emeel Hakim <ehakim@nvidia.com>
2021-10-05 14:10:13 +0300

    ipsec-tests: Added test-crypto-offload-vxlan.sh
    Added a dev-test which tests the traffic while both ipsec
    crypto offload and vxlan are configured.
    
    Test Matrix:
    -mtu 1500 and 9000
    
    Test pass criteria: have traffic
    
    Change-Id: Idce3caaa0271eb069172e4b6bc55f020557cfdd2
    Signed-off-by: Emeel Hakim <ehakim@nvidia.com>

---
+++ ipsec-tests/test-crypto-offload-vxlan.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# This test configures ipsec with vxlan
+# and verifies that it has traffic.
+
+my_dir="$(dirname "$0")"
+. $my_dir/common-ipsec.sh
+
+require_remote_server
+
+vxlan_lip="1.1.1.1"
+vxlan_rip="1.1.1.2"
+TCPDUMP_FILE="/tmp/temp2.txt"
+
+function config_vxlan_local() {
+    ip link add vx0 type vxlan id 100 local $LIP remote $RIP dev $NIC dstport 4789
+    ifconfig vx0 $vxlan_lip/24 up
+}
+
+function config_vxlan_remote() {
+    on_remote "ip link add vx0 type vxlan id 100 local 172.16.0.2 remote 172.16.0.1 dev $REMOTE_NIC dstport 4789
+               ifconfig vx0 $vxlan_rip/24 up"
+}
+
+function config() {
+    ipsec_config_on_both_sides transport 128 ipv4 offload
+    config_vxlan_local
+    config_vxlan_remote
+}
+
+function clean_up() {
+    kill_iperf
+    ipsec_clean_up_on_both_sides
+    ip link del dev vx0 2> /dev/null
+    on_remote "ip link del dev vx0 2> /dev/null"
+    change_mtu_on_both_sides 1500
+    rm -f $TCPDUMP_FILE
+}
+
+function run_test() {
+    title "Run traffic"
+    local t=5
+    start_iperf_server
+
+    timeout $((t+2)) tcpdump -qnnei $NIC -c 5 -w $TCPDUMP_FILE &
+    local pid=$!
+    (on_remote timeout $((t+2)) iperf3 -c $vxlan_lip -t $t -i 5) || err "iperf3 failed"
+
+    fail_if_err
+
+    sleep 3
+
+    title "Verify traffic on $NIC"
+    verify_have_traffic $pid
+    sleep 3
+}
+
+trap clean_up EXIT
+config
+run_test
+clean_up
+change_mtu_on_both_sides 9000
+config
+run_test
+trap - EXIT
+clean_up
+test_done
