25b83bed74cd189a64f4367b634b6b34bc97342a
Mahmoud AbuZant <mahmoudabuz@nvidia.com>
2021-10-18 15:31:59 +0300

    test-vf-lag-xmit-hash-policy.sh: enabling LAG_RESOURCE_ALLOCATION
    Changing the lag_port_select_mode requires to have
    LAG_RESOURCE_ALLOCATION config enabled to avoid seeing syndrome
    0xd3fcf1.
    
    Issue: 2786489
    Change-Id: I6fc6c4f0bc431ff0fef7eaa551f35bd2a212d6af
    Signed-off-by: Mahmoud AbuZant <mahmoudabuz@nvidia.com>

--- test-vf-lag-xmit-hash-policy.sh
+++ test-vf-lag-xmit-hash-policy.sh
@@ -4,12 +4,20 @@
 # Bond modes are balance-xor and 802.3ad.
 #
 # Bug SW #2780336: Kernel panic and call trace appears in mlx5_del_flow_rules when creating vf-lag with xmit hash policy layer2+3
+# Require LAG_RESOURCE_ALLOCATION to be enabled.
 
 my_dir="$(dirname "$0")"
 . $my_dir/common.sh
 
 require_module bonding
 not_relevant_for_nic cx4 cx4lx cx5 cx6 cx6lx
+
+function set_lag_resource_allocation() {
+    local mode=$1
+    title "lag_resource_allocation value to $mode"
+    fw_config LAG_RESOURCE_ALLOCATION=$mode || fail "Cannot set lag resource allocation to $mode"
+    fw_reset
+}
 
 function set_lag_port_select_mode() {
     if ! is_ofed ; then
@@ -33,8 +41,9 @@ function config() {
 function cleanup() {
     clear_bonding
     set_lag_port_select_mode "queue_affinity"
+    config_sriov 2 &>/dev/null
+    config_sriov 0 $NIC2 &>/dev/null
     enable_switchdev &>/dev/null
-    config_sriov 0 $NIC2
 }
 
 function check_bond_xmit_hash_policy() {
@@ -53,7 +62,11 @@ function check_bond_xmit_hash_policy() {
 }
 
 trap cleanup EXIT
+start_check_syndrome
 clear_bonding
+set_lag_resource_allocation 1
 config
 check_bond_xmit_hash_policy
+set_lag_resource_allocation 0
+check_syndrome
 test_done
