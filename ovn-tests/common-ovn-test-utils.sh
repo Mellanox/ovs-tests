5b4ae7d746598878e8863ce420fa74f199feb5cc
Mamduh Alassi <mamduhala@nvidia.com>
2021-11-02 09:59:42 +0200

    common-ovn-test-utils.sh: Add common function for OVN clean up
    Change-Id: I10a029f13aeb55f7f0393c752ec78cec26a437ed
    Signed-off-by: Mamduh Alassi <mamduhala@nvidia.com>

--- ovn-tests/common-ovn-test-utils.sh
+++ ovn-tests/common-ovn-test-utils.sh
@@ -9,4 +9,38 @@ OVN_LOCAL_CENTRAL_IP="127.0.0.1"
 OVN_CENTRAL_IP="192.168.100.100"
 OVN_REMOTE_CONTROLLER_IP="192.168.100.101"
 
+# Test Config
+TOPOLOGY=
+HAS_REMOTE=
+HAS_BOND=
+
+function __ovn_clean_up() {
+    ovn_stop_ovn_controller
+    ovn_destroy_topology
+    ovn_stop_northd_central
+    ovn_remove_ovs_config
+    ovs_clear_bridges
+
+    ip addr flush dev $NIC
+    ip link set $NIC mtu 1500
+    ip -all netns del
+    unbind_vfs
+
+    if [[ -n "$HAS_BOND" ]]; then
+        unbind_vfs $NIC2
+        clear_bonding
+        disable_sriov_port2
+    fi
+
+    bind_vfs
+}
+
+function ovn_clean_up() {
+    __ovn_clean_up
+
+    if [[ -n "$HAS_REMOTE" ]]; then
+        on_remote_exec "__ovn_clean_up"
+    fi
+}
+
 require_ovn
