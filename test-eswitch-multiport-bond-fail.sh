1595ab60faf3042fc281646d6f040f958e4cb40e
Mahmoud AbuZant <mahmoudabuz@nvidia.com>
2021-11-04 13:18:56 +0200

    test-eswitch-multiport-bond-fail.sh: Add new test
    Add new test that set lag_port_select_mode to multiport_esw
    and fails in attaching slaves to a bond after it.
    
    Issue: 2844777
    Change-Id: I286afc59b15be54e34cbb94da1215d252b6ee419
    Signed-off-by: Mahmoud AbuZant <mahmoudabuz@nvidia.com>

---
+++ test-eswitch-multiport-bond-fail.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# Set lag_port_select_mode to multiport_esw and check fail to attach slave to a bond.
+# Driver in multiport_esw mode doesn't support bonding.
+# Bug SW #2851706: [ASAP, OFED 5.5, multiport_esw] multiport_esw should block bond device enslaving
+# Require LAG_RESOURCE_ALLOCATION to be enabled.
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+require_module bonding
+not_relevant_for_nic cx4 cx4lx cx5 cx6 cx6lx
+
+function config() {
+    config_sriov 2
+    config_sriov 2 $NIC2
+    set_lag_port_select_mode "multiport_esw"
+    enable_switchdev
+    enable_switchdev $NIC2
+}
+
+function cleanup() {
+    clear_bonding
+    set_lag_port_select_mode "queue_affinity"
+    config_sriov 2
+    config_sriov 0 $NIC2
+    enable_switchdev
+}
+
+function check_bond_fail() {
+    title "Trying to attach slaves to a bond"
+    __ignore_errors=1
+    __config_bonding $NIC $NIC2
+    local rc=$?
+    __ignore_errors=0
+
+    if [ "$rc" != "0" ]; then
+        success
+    else
+        err "Expected to fail"
+    fi
+}
+
+trap cleanup EXIT
+
+start_check_syndrome
+clear_bonding
+set_lag_resource_allocation 1
+config
+check_bond_fail
+set_lag_resource_allocation 0
+check_syndrome
+trap - EXIT
+cleanup
+test_done
