9f6b291154148b11e6541fadd0626a95878a558e
Ziyad Atiyyeh <ziyadat@nvidia.com>
2021-11-17 10:05:31 +0200

    test-eswitch-toggle-mode-fail.sh: Update expected errors
    In smfs we get a different error msg than dmfs. ignore it.
    
    Issue: 2860310
    Change-Id: Ieb73dcd46d8e6092909d7c6cfdc2bd5d4a2729e0
    Signed-off-by: Ziyad Atiyyeh <ziyadat@nvidia.com>

--- test-eswitch-toggle-mode-fail.sh
+++ test-eswitch-toggle-mode-fail.sh
@@ -13,6 +13,7 @@ enable_legacy
 bind_vfs
 __ignore_errors=1
 add_expected_error_msg "Failed to create slow path FDB Table"
+add_expected_error_msg "Failed creating dr flow_table"
 add_expected_error_msg "0x98afbb"
 switch_mode_switchdev
 __ignore_errors=0
