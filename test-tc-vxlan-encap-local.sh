65790b745c86917d517be6c01167aa95ac6daf87
Chris Mi <cmi@nvidia.com>
2021-11-22 13:28:55 +0200

    test-tc-vxlan-encap-local.sh: Add check for decap rule
    This case only checks for encap rule. Add check for decap rule as well.
    
    Change-Id: I40542132b354950585ce0d224e07abd2b8c3f4d2
    Signed-off-by: Chris Mi <cmi@nvidia.com>

--- test-tc-vxlan-encap-local.sh
+++ test-tc-vxlan-encap-local.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #
-# Verify adding vxlan encap rule does not use local route which results in dst/src mac 0.
-# In newer kernel the driver has a fix to use vxlan device hint if available.
-# 79372f06556a net/mlx5e: Specify out ifindex when looking up encap route
+# Verify adding vxlan encap/decap rules does not use local route which results
+# in rule not in hw. In newer kernel the driver has fixes to use vxlan device
+# hint if available:
+#
+# 2f8ec867b6c3 net/mlx5e: Specify out ifindex when looking up encap route
+# 819c319c8c91 net/mlx5e: Specify out ifindex when looking up decap route
 #
 # Feature #2619265: [Alibaba-RoCE] local and remote VTEPs are in the same host for vxlan
 
@@ -37,12 +40,7 @@ function config_vxlan() {
     ifconfig $NIC $local_ip/24 up
 }
 
-function tc_filter_fail() {
-    eval tc -s filter $@ && err "Expected to fail adding rule" && return 1
-    return 0
-}
-
-function add_vxlan_rule() {
+function add_rules() {
     local local_ip="$1"
     local remote_ip="$2"
 
@@ -51,26 +49,38 @@ function add_vxlan_rule() {
     reset_tc $NIC $REP vxlan1
 
     # tunnel key set
-    local filter="add dev $REP protocol arp parent ffff: prio 1 \
-        flower dst_mac $dst_mac skip_sw \
+    tc_filter add dev $REP protocol arp parent ffff: prio 1 \
+        flower dst_mac $dst_mac \
         action tunnel_key set \
         id $id src_ip ${local_ip} dst_ip ${remote_ip} dst_port ${dst_port} \
-        action mirred egress redirect dev vxlan1"
-
+        action mirred egress redirect dev vxlan1
     if [ "$use_hint" == "true" ]; then
-        tc_filter $filter && success
+        verify_in_hw $REP 1
     else
-        tc_filter_fail $filter && success
+        verify_not_in_hw $REP 1
+    fi
+
+    tc_filter add dev vxlan1 protocol arp parent ffff: prio 2 flower \
+        enc_src_ip $remote_ip           \
+        enc_dst_ip $local_ip            \
+        enc_dst_port $dst_port          \
+        enc_key_id $id                  \
+        action tunnel_key unset pipe    \
+        action mirred egress redirect dev $REP
+    if [ "$use_hint" == "true" ]; then
+        verify_in_hw vxlan1 2
+    else
+        verify_not_in_hw vxlan1 2
     fi
 }
 
-function test_add_encap_rule_neigh_local() {
+function test_add_rule_neigh_local() {
     ifconfig $NIC up
     local mac2=`cat /sys/class/net/$NIC2/address`
     ifconfig $NIC2 $remote_ip/24 up
     ip r show dev $NIC
     ip n show $remote_ip
-    add_vxlan_rule $local_ip $remote_ip
+    add_rules $local_ip $remote_ip
     ifconfig $NIC2 0
     reset_tc $REP
 }
@@ -83,12 +93,12 @@ function do_test() {
 cleanup
 use_hint="false"
 config_vxlan
-do_test test_add_encap_rule_neigh_local
+do_test test_add_rule_neigh_local
 
 cleanup
 use_hint="true"
 config_vxlan "dev $NIC"
-do_test test_add_encap_rule_neigh_local
+do_test test_add_rule_neigh_local
 
 cleanup
 test_done
