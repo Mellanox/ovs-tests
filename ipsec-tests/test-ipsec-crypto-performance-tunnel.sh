e875c17e8b2cac792272d19ba12e78b11606c6c1
Emeel Hakim <ehakim@nvidia.com>
2021-12-06 15:50:39 +0200

    ipsec-tests: Added test-ipsec-crypto-performance-tunnel.sh
    Added a dev-test which compares the performance of IPsec in tunnel
    mode with crypto offload (in Mbits/sec) to the performance of
    IPsec in tunnel mode with no offload.
    
    Test Matrix:
    -mtu 1500,5250 and 9000
    -protocols: IPv4,IPv6
    
    Test pass criteria: offload throughput > no offload throughput
    
    Note: throughput is being measured using iperf3 with tcp,
    considering the "10.00-15.00" interval report.
    
    Change-Id: I6fe6b2283a5cee630c62e832ce1e6b7bc0addf47
    Signed-off-by: Emeel Hakim <ehakim@nvidia.com>

---
+++ ipsec-tests/test-ipsec-crypto-performance-tunnel.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#This test compares the performance of ipsec with offload
+#to the performance of ipsec without offload, it expects to
+#get better results with offload.
+
+my_dir="$(dirname "$0")"
+. $my_dir/common-ipsec.sh
+. $my_dir/common-ipsec-crypto.sh
+
+require_remote_server
+
+function clean_up() {
+    ipsec_clean_up_on_both_sides
+    kill_iperf
+    change_mtu_on_both_sides 1500
+    rm -f /tmp/offload_results.txt /tmp/results.txt
+}
+
+function run_test() {
+    title "Testing IPsec crypto offload performance"
+    #make sure iperf server is down
+    kill_iperf
+    local mtu
+    for mtu in 1500 5250 9000; do
+        title "Test with mtu = $mtu"
+        change_mtu_on_both_sides $mtu
+        run_performance_test tunnel ipv4
+        clean_up
+        change_mtu_on_both_sides $mtu
+        run_performance_test tunnel ipv6
+        clean_up
+    done
+}
+
+trap clean_up EXIT
+run_test
+trap - EXIT
+clean_up
+test_done
