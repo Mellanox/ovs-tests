b9676235f0b038f8bcb3727fa9209dea99a354f2
Mamduh Alassi <mamduhala@nvidia.com>
2022-01-20 14:58:41 +0200

    ovn-tests: Add OVN test test-ovn-geneve-single-router-2-switches-2-nodes-pf-tunnel-vlan
    Change-Id: Ia6a8d96898c9d5ca4341cda02ce034d0b5d0aa19
    Signed-off-by: Mamduh Alassi <mamduhala@nvidia.com>

---
+++ ovn-tests/test-ovn-geneve-single-router-2-switches-2-nodes-pf-tunnel-vlan.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#
+# Verify traffic between VFs on different nodes configured with OVN router and 2 switches and OVS VLAN is offloaded
+#
+
+CONFIG_REMOTE=1
+HAS_VLAN=1
+
+my_dir="$(dirname "$0")"
+. $my_dir/common-ovn-test-utils.sh
+
+not_relevant_for_nic cx4 cx4lx cx5 cx6 cx6lx
+
+TOPOLOGY=$TOPOLOGY_SINGLE_ROUTER_2_SWITCHES
+
+CLIENT_SWITCH=$SWITCH1
+CLIENT_PORT=$SWITCH1_PORT1
+CLIENT_MAC=$(ovn_get_switch_port_mac $TOPOLOGY $CLIENT_SWITCH $CLIENT_PORT)
+CLIENT_IPV4=$(ovn_get_switch_port_ip $TOPOLOGY $CLIENT_SWITCH $CLIENT_PORT)
+CLIENT_IPV6=$(ovn_get_switch_port_ipv6 $TOPOLOGY $CLIENT_SWITCH $CLIENT_PORT)
+CLIENT_GATEWAY_IPV4=$(ovn_get_switch_gateway_ip $TOPOLOGY $CLIENT_SWITCH)
+CLIENT_GATEWAY_IPV6=$(ovn_get_switch_gateway_ipv6 $TOPOLOGY $CLIENT_SWITCH)
+CLIENT_NS=ns0
+CLIENT_VF=$VF
+CLIENT_REP=$REP
+
+SERVER_SWITCH=$SWITCH2
+SERVER_PORT=$SWITCH2_PORT1
+SERVER_MAC=$(ovn_get_switch_port_mac $TOPOLOGY $SERVER_SWITCH $SERVER_PORT)
+SERVER_IPV4=$(ovn_get_switch_port_ip $TOPOLOGY $SERVER_SWITCH $SERVER_PORT)
+SERVER_IPV6=$(ovn_get_switch_port_ipv6 $TOPOLOGY $SERVER_SWITCH $SERVER_PORT)
+SERVER_GATEWAY_IPV4=$(ovn_get_switch_gateway_ip $TOPOLOGY $SERVER_SWITCH)
+SERVER_GATEWAY_IPV6=$(ovn_get_switch_gateway_ipv6 $TOPOLOGY $SERVER_SWITCH)
+SERVER_NS=ns0
+SERVER_VF=$VF
+SERVER_REP=$REP
+
+function run_test() {
+    ovn_config_interface_namespace $CLIENT_VF $CLIENT_REP $CLIENT_NS $CLIENT_PORT $CLIENT_MAC $CLIENT_IPV4 $CLIENT_IPV6 $CLIENT_GATEWAY_IPV4 $CLIENT_GATEWAY_IPV6
+    on_remote_exec "ovn_config_interface_namespace $SERVER_VF $SERVER_REP $SERVER_NS $SERVER_PORT $SERVER_MAC $SERVER_IPV4 $SERVER_IPV6 $SERVER_GATEWAY_IPV4 $SERVER_GATEWAY_IPV6"
+
+    ovs-vsctl show
+    ovn-sbctl show
+
+    title "Test ICMP traffic between $CLIENT_VF($CLIENT_IPV4) -> $SERVER_VF($SERVER_IPV4) offloaded"
+    check_icmp_traffic_offload $CLIENT_REP $CLIENT_NS $SERVER_IPV4
+
+    title "Test TCP traffic between $CLIENT_VF($CLIENT_IPV4) -> $SERVER_VF($SERVER_IPV4) offloaded"
+    check_remote_tcp_traffic_offload $CLIENT_REP $CLIENT_NS $SERVER_NS $SERVER_IPV4
+
+    title "Test UDP traffic between $CLIENT_VF($CLIENT_IPV4) -> $SERVER_VF($SERVER_IPV4) offloaded"
+    check_remote_udp_traffic_offload $CLIENT_REP $CLIENT_NS $SERVER_NS $SERVER_IPV4
+
+    # ICMP6 offloading is not supported because IPv6 packet header doesn't contain checksum header
+    # which cause offloading to fail
+    title "Test ICMP6 traffic between $CLIENT_VF($CLIENT_IPV6) -> $SERVER_VF($SERVER_IPV6)"
+    ip netns exec $CLIENT_NS ping -6 -w 4 $SERVER_IPV6 && success || err
+
+    title "Test TCP6 traffic between $CLIENT_VF($CLIENT_IPV6) -> $SERVER_VF($SERVER_IPV6) offloaded"
+    check_remote_tcp6_traffic_offload $CLIENT_REP $CLIENT_NS $SERVER_NS $SERVER_IPV6
+
+    title "Test UDP6 traffic between $CLIENT_VF($CLIENT_IPV6) -> $SERVER_VF($SERVER_IPV6) offloaded"
+    check_remote_udp6_traffic_offload $CLIENT_REP $CLIENT_NS $SERVER_NS $SERVER_IPV6
+}
+
+ovn_execute_test
