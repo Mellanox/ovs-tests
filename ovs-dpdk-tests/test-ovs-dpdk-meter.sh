14c24dd9c432ee4da4c7167cee07fc5ca821d826
Salem Sol <salems@nvidia.com>
2022-02-08 14:03:12 +0200

    ovs-dpdk-tests: Add new ovs-dpdk tests for openflow meters
    Issue: 2965239
    Change-Id: I433bb90c4865b326cfdf68cd1c6b83bdc90b9cc2
    Signed-off-by: Salem Sol <salems@nvidia.com>

---
+++ ovs-dpdk-tests/test-ovs-dpdk-meter.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#
+# Test OVS-DPDK openflow meters
+#
+
+my_dir="$(cd "$(dirname ${BASH_SOURCE[0]})" &>/dev/null && pwd)"
+. $my_dir/../common.sh
+. $my_dir/common-dpdk.sh
+
+pktgen=$my_dir/../scapy-traffic-tester.py
+
+trap cleanup_test EXIT
+
+NS0_IP=1.1.1.7
+NS1_IP=1.1.1.8
+
+#Test pre-req
+function test_pre_config() {
+    config_sriov 2
+    require_interfaces REP NIC
+    unbind_vfs
+    bind_vfs
+
+    cleanup_test
+    set_e2e_cache_enable false
+    debug "Restarting OVS"
+    start_clean_openvswitch
+}
+
+function config_topology() {
+    config_simple_bridge_with_rep 2
+
+    config_ns ns0 $VF1 $NS0_IP
+    config_ns ns1 $VF2 $NS1_IP
+}
+
+function run() {
+    config_topology
+
+    ovs_add_meter br-phy 1 pktps 1
+    ovs_add_simple_meter_rule
+    debug "testing meter on PF"
+    send_metered_ping
+    check_dpdk_offloads $NS0_IP
+    ovs_del_meter
+    ovs-appctl revalidator/purge
+
+    ovs_add_meter br-phy 1 pktps 50 1
+    ovs_add_meter br-phy 2 pktps 50 1
+    ovs_add_bidir_meter_rules
+    debug "testing meter on REPs"
+    ovs_send_scapy_packets $pktgen $VF1 $VF2 $NS0_IP $NS1_IP 1 150 ns0 ns1
+    check_dpdk_offloads $NS0_IP
+    ovs_check_tcpdump 70
+    ovs_del_meter br-phy 1
+    ovs_del_meter br-phy 2
+
+}
+
+test_pre_config
+run
+start_clean_openvswitch
+test_done
---
+++ ovs-dpdk-tests/test-ovs-dpdk-vxlan-meter.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# Test OVS-DPDK openflow meters with VxLAN traffic
+#
+# Require external server
+#
+
+my_dir="$(cd "$(dirname ${BASH_SOURCE[0]})" &>/dev/null && pwd)"
+. $my_dir/../common.sh
+. $my_dir/common-dpdk.sh
+
+trap cleanup_test EXIT
+
+#Test pre-req
+function test_pre_config() {
+    require_remote_server
+    config_sriov 2
+    require_interfaces REP NIC
+    unbind_vfs
+    bind_vfs
+    cleanup_test
+    set_e2e_cache_enable false
+    debug "Restarting OVS"
+    start_clean_openvswitch
+}
+
+function config_topology() {
+    config_tunnel vxlan 2
+    config_local_tunnel_ip $LOCAL_TUN_IP br-phy
+    config_remote_tunnel vxlan
+}
+
+function run() {
+    config_topology
+
+    ovs_add_meter br-phy 1 pktps 1
+    ovs_add_simple_meter_rule
+    debug "testing meter on PF"
+    send_metered_ping
+    check_dpdk_offloads $LOCAL_IP
+    ovs_del_meter
+    ovs-appctl revalidator/purge
+
+    ovs_add_meter br-int 1 pktps 50 1
+    ovs_add_simple_meter_rule br-int 1
+    debug "testing meter on REP"
+    send_metered_ping ns0 200 5 $REMOTE_IP 0.05 115
+    check_dpdk_offloads $LOCAL_IP
+    ovs_del_meter br-int 1
+}
+
+test_pre_config
+run
+start_clean_openvswitch
+test_done
