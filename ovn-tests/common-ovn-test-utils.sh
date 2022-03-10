021a425d1123b762d7fa805839e3658ac5c2436e
Mamduh Alassi <mamduhala@nvidia.com>
2022-03-10 14:59:42 +0200

    ovn-tests: Refactor two nodes VF LAG VLAN tunnel tests configuration
    Change-Id: Ie4e255c15a7f2baa7c1f79254fe7dc4550b1844c
    Signed-off-by: Mamduh Alassi <mamduhala@nvidia.com>

--- ovn-tests/common-ovn-test-utils.sh
+++ ovn-tests/common-ovn-test-utils.sh
@@ -207,6 +207,26 @@ function config_ovn_vf_lag() {
     ovn_start_ovn_controller
 }
 
+function config_ovn_vf_lag_vlan() {
+    local ovn_central_ip=$1
+    local ovn_controller_ip=$2
+    local vf_var=$3
+    local rep_var=$4
+    local mode=${5:-"802.3ad"}
+
+    config_vf_lag $mode
+    require_interfaces $vf_var $rep_var
+
+    start_clean_openvswitch
+    ovs_create_bridge_vlan_interface
+    ovs_add_port_to_switch $OVN_PF_BRIDGE $OVN_BOND
+    ovn_config_mtu $NIC $NIC2 $OVN_BOND $OVN_PF_BRIDGE $OVN_VLAN_INTERFACE
+    ip addr add $ovn_controller_ip/24 dev $OVN_VLAN_INTERFACE
+
+    ovn_set_ovs_config $ovn_central_ip $ovn_controller_ip
+    ovn_start_ovn_controller
+}
+
 function __ovn_config_mtu() {
     # Increase MTU NIC for non single node
     # Geneve packet contains additional data
