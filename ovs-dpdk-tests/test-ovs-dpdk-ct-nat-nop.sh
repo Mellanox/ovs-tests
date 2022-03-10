a1697d26b9ce16efc0e2f8f17ea3b7d98871d1ca
Noor Odah <nodah@nvidia.com>
2022-03-10 15:32:30 +0200

    ovs-dpdk-tests: Add new test test-ovs-dpdk-ct-nat-nop.sh
    Add new test 'test-ovs-dpdk-ct-nat-nop.sh' to test offload with an empty nat action
    
    Issue: 2976522
    Signed-off-by: Noor Odah <nodah@nvidia.com>
    Change-Id: I55a75e94b518eba82106c7714a0a3eff6683ffcf

---
+++ ovs-dpdk-tests/test-ovs-dpdk-ct-nat-nop.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Test OVS-DPDK CT with an empty nat action using TCP traffic
+#
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/../common.sh
+. $my_dir/common-dpdk.sh
+
+IP=4.4.4.10
+IP_2=4.4.4.11
+
+trap cleanup_test EXIT
+
+function config() {
+    enable_switchdev
+    bind_vfs
+    cleanup_test
+    set_e2e_cache_enable false
+    echo "Restarting OVS"
+    start_clean_openvswitch
+    config_simple_bridge_with_rep 2
+    config_ns ns0 $VF $IP
+    config_ns ns1 $VF2 $IP_2
+}
+
+function run() {
+    config
+    ovs_add_ct_nat_nop_rules br-phy
+    echo -e "\n Testing icmp traffic"
+    verify_ping $IP_2 ns0
+    echo -e "\n Testing TCP traffic"
+    # traffic
+    generate_traffic "local" $IP ns1
+    # check offloads
+    check_dpdk_offloads $IP
+    check_offloaded_connections 5
+}
+
+run
+start_clean_openvswitch
+test_done
