f7c37104a3179307d5aad683eb0a15383be4630c
Mamduh Alassi <mamduhala@nvidia.com>
2022-03-15 14:53:18 +0200

    ovn-tests: Add ovn router pf/vf lag vlan with ipv6 underlay tests
    Change-Id: I40b5fd33044f1229e36fe48e3acf2d2532794672
    Signed-off-by: Mamduh Alassi <mamduhala@nvidia.com>

---
+++ ovn-tests/test-ovn-geneve-single-router-2-switches-2-nodes-vf-lag-tunnel-vlan-ipv6.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Verify traffic between VFs on different nodes configured with OVN router and 2 switches is offloaded with ipv6 underlay
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common-ovn-test-utils.sh
+
+NO_TITLE=1
+
+ovn_set_ipv6_ips
+. $my_dir/test-ovn-geneve-single-router-2-switches-2-nodes-vf-lag-tunnel-vlan.sh
