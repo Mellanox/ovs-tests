da39baac5302e1eef64867951c5e06597d5826be
Mamduh Alassi <mamduhala@nvidia.com>
2022-03-15 08:30:53 +0200

    ovn-tests: Refactor ovn kubernetes single node test configuration
    Change-Id: I3f70e8c7e3aa1d5b574607b5189f340e49d95772
    Signed-off-by: Mamduh Alassi <mamduhala@nvidia.com>

--- ovn-tests/test-ovn-kubernetes-pod-pod-same-node.sh
+++ ovn-tests/test-ovn-kubernetes-pod-pod-same-node.sh
@@ -7,10 +7,9 @@ my_dir="$(dirname "$0")"
 . $my_dir/common-ovn-test-utils.sh
 . $my_dir/common-ovn-kubernetes.sh
 
-read_k8s_topology_pod_pod_same_node
+require_interfaces NIC
 
-nic=$NIC
-BRIDGE=$(nic_to_bridge $nic)
+read_k8s_topology_pod_pod_same_node
 
 function clean_up_test() {
     ovn_clean_up
@@ -18,22 +17,7 @@ function clean_up_test() {
 }
 
 function config_test() {
-    ovn_config_interfaces
-    start_clean_openvswitch
-
-    ip link set $nic up
-    ip link set $nic addr $CLIENT_NODE_MAC
-    ovn_add_network $BRIDGE $nic $OVN_KUBERNETES_NETWORK
-    ip addr add $CLIENT_NODE_IP/$CLIENT_NODE_IP_MASK dev $BRIDGE
-
-    ovn_set_ovs_config $CLIENT_NODE_IP $CLIENT_NODE_IP
-    ovn_start_ovn_controller
-    ovs_conf_set max-idle 20000
-
-    __ovn_config_mtu
-
-    ovn_start_northd_central $CLIENT_NODE_IP
-    ovn_create_topology
+    config_ovn_single_node
 
     ovn_config_interface_namespace $CLIENT_VF $CLIENT_REP $CLIENT_NS $CLIENT_PORT $CLIENT_MAC $CLIENT_IPV4 $CLIENT_IPV6 $CLIENT_GATEWAY_IPV4 $CLIENT_GATEWAY_IPV6
     ovn_config_interface_namespace $SERVER_VF $SERVER_REP $SERVER_NS $SERVER_PORT $SERVER_MAC $SERVER_IPV4 $SERVER_IPV6 $SERVER_GATEWAY_IPV4 $SERVER_GATEWAY_IPV6
