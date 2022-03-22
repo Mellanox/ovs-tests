a668f17fd0123fa6ef5d48ff1402dcdf70e3b21c
Chris Mi <cmi@nvidia.com>
2022-03-22 08:53:57 +0200

    common.sh: Disable sriov first
    Function disable_sriov sets eswitch mode to legacy first and then
    disable sriov. But if there are offloaded rules, we can't change
    eswitch mode from switchdev to legacy. So disable sriov first and
    then change mode to legacy.
    
    Issue: 2995817
    Change-Id: I580e32012690bae6c20c683e26aec5a8230f559b
    Signed-off-by: Chris Mi <cmi@nvidia.com>

--- common.sh
+++ common.sh
@@ -1071,10 +1071,10 @@ function config_sriov() {
 }
 
 function disable_sriov() {
-    enable_legacy $NIC
-    enable_legacy $NIC2
     config_sriov 0 $NIC
     config_sriov 0 $NIC2
+    enable_legacy $NIC
+    enable_legacy $NIC2
 }
 
 function enable_sriov() {
