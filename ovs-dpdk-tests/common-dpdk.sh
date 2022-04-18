013a9646edb354e238c0ce06cf71eb5ba1ac0116
Salem Sol <salems@nvidia.com>
2022-04-18 16:25:52 +0300

    common-dpdk: Add type=offloaded in check_offload_contains
    check_offload_contains should check offloaded datapath rules, due to noise or
    unsupported matches we might have datapath rules that we can't offload. In order
    to filter those out when checking for an offload add the flag type=offloaded
    when dumping the flows.
    
    Issue: 3044512
    Change-Id: Ie39233e6ca4459de29f208a426983d54e4c51f39
    Signed-off-by: Salem Sol <salems@nvidia.com>

--- ovs-dpdk-tests/common-dpdk.sh
+++ ovs-dpdk-tests/common-dpdk.sh
@@ -110,7 +110,7 @@ function check_offload_contains() {
     local text=$1
     local num_flows=$2
 
-    local flows=$(ovs-appctl dpctl/dump-flows -m | grep "$1" |wc -l)
+    local flows=$(ovs-appctl dpctl/dump-flows -m type=offloaded | grep "$1" |wc -l)
     if [ $flows -ne $num_flows ]; then
         err "expected $num_flows flows with $1 message but got $flows"
         echo "flows:"
