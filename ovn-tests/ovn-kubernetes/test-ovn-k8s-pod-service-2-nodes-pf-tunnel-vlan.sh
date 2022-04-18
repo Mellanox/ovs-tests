582e21e6232192c52ed1f7671030dc2bc7e0d91b
Mamduh Alassi <mamduhala@nvidia.com>
2022-04-18 09:54:33 +0300

    ovn-tests: ovn-k8s: Add pod to service pf/vf-lag with vlan tests
    Change-Id: Iafa5d4edf61ee7894de772ad37d3da3f61dbe28f
    Signed-off-by: Mamduh Alassi <mamduhala@nvidia.com>

---
+++ ovn-tests/ovn-kubernetes/test-ovn-k8s-pod-service-2-nodes-pf-tunnel-vlan.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#
+# Verify traffic for OVN-Kubernetes pod to service on different nodes with PF tunnel over vlan
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common-ovn-k8s-test.sh
+
+require_interfaces NIC
+require_remote_server
+
+read_k8s_topology_pod_service_different_nodes
+
+nic=$NIC
+BRIDGE=$(nic_to_bridge $nic)
+
+function clean_up_test() {
+    ovn_stop_ovn_controller
+    ovn_remove_ovs_config
+    __reset_nic
+    ovn_remove_network $BRIDGE $nic
+    start_clean_openvswitch
+    ip -all netns del
+    config_sriov 0
+
+    on_remote_exec "ovn_stop_ovn_controller
+                    ovn_remove_ovs_config
+                    __reset_nic
+                    ovn_remove_network $BRIDGE $nic
+                    start_clean_openvswitch
+                    ip -all netns del
+                    config_sriov 0"
+
+    ovn_start_clean
+    ovn_stop_northd_central
+}
+
+function config_test() {
+    ovn_start_northd_central $CLIENT_NODE_IP
+    ovn_create_topology
+
+    config_ovn_k8s_pf_vlan $CLIENT_NODE_IP $CLIENT_NODE_IP $CLIENT_NODE_IP_MASK $CLIENT_NODE_MAC $OVN_K8S_VLAN_NODE1_TUNNEL_IP CLIENT_VF CLIENT_REP
+    ovn_config_interface_namespace $CLIENT_VF $CLIENT_REP $CLIENT_NS $CLIENT_PORT $CLIENT_MAC $CLIENT_IPV4 $CLIENT_IPV6 $CLIENT_GATEWAY_IPV4 $CLIENT_GATEWAY_IPV6
+
+    on_remote_exec "config_ovn_k8s_pf_vlan $CLIENT_NODE_IP $SERVER_NODE_IP $SERVER_NODE_IP_MASK $SERVER_NODE_MAC $OVN_K8S_VLAN_NODE2_TUNNEL_IP SERVER_VF SERVER_REP
+                    ovn_config_interface_namespace $SERVER_VF $SERVER_REP $SERVER_NS $SERVER_PORT $SERVER_MAC $SERVER_IPV4 $SERVER_IPV6 $SERVER_GATEWAY_IPV4 $SERVER_GATEWAY_IPV6"
+}
+
+function run_test() {
+    ovs-vsctl show
+    ovn-sbctl show
+
+    # Offloading ICMP with connection tracking is not supported
+    title "Test ICMP traffic between $CLIENT_VF($CLIENT_IPV4) -> $SERVER_VF($LB_IPV4)"
+    ip netns exec $CLIENT_NS ping -w 4 $LB_IPV4 && success || err
+
+    title "Test TCP traffic between $CLIENT_VF($CLIENT_IPV4) -> $SERVER_VF($LB_IPV4) offloaded"
+    check_remote_tcp_traffic_offload $CLIENT_REP $CLIENT_NS $SERVER_NS $LB_IPV4
+
+    title "Test UDP traffic between $CLIENT_VF($CLIENT_IPV4) -> $SERVER_VF($LB_IPV4) offloaded"
+    check_remote_udp_traffic_offload $CLIENT_REP $CLIENT_NS $SERVER_NS $LB_IPV4
+
+    # ICMP6 offloading is not supported because IPv6 packet header doesn't contain checksum header
+    # which cause offloading to fail and offloading ICMP with connection tracking is not supported
+    title "Test ICMP6 traffic between $CLIENT_VF($CLIENT_IPV6) -> $SERVER_VF($LB_IPV6)"
+    ip netns exec $CLIENT_NS ping -6 -w 4 $LB_IPV6 && success || err
+
+    title "Test TCP6 traffic between $CLIENT_VF($CLIENT_IPV6) -> $SERVER_VF($LB_IPV6) offloaded"
+    check_remote_tcp6_traffic_offload $CLIENT_REP $CLIENT_NS $SERVER_NS $LB_IPV6
+
+    title "Test UDP6 traffic between $CLIENT_VF($CLIENT_IPV6) -> $SERVER_VF($LB_IPV6) offloaded"
+    check_remote_udp6_traffic_offload $CLIENT_REP $CLIENT_NS $SERVER_NS $LB_IPV6
+}
+
+clean_up_test
+
+trap clean_up_test EXIT
+
+config_test
+run_test
+
+trap - EXIT
+clean_up_test
+
+test_done
