abb6117845e556e6c6ff4ffc631f3fe0702ae9af
Emeel Hakim <ehakim@nvidia.com>
2022-04-19 16:05:38 +0300

    ipsec-tests: Added test-ipsec-crypto-ci.sh
    Added a short sanity IPsec test to add to the kernel ci.
    
    Test Matrix:
    -mtu 1500
    -protocols: IPv4
    -offload: both sides offloaded
    -encryption key length: 128
    
    Signed-off-by: Emeel Hakim <ehakim@nvidia.com>
    Change-Id: I777716130aca7125d0f0f67a91c2555e6dcddd68

---
+++ ipsec-tests/test-ipsec-crypto-basic.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+my_dir="$(dirname "$0")"
+. $my_dir/common-ipsec.sh
+. $my_dir/common-ipsec-crypto.sh
+
+require_remote_server
+
+function cleanup(){
+    cleanup_crypto
+}
+
+#This is a short ipsec test for sanity
+function run_test_ipsec_crypto_sanity() {
+    local mtu=$1
+    local ip_proto=$2
+    local ipsec_mode=${3:-"transport"}
+    local net_proto=${4:-"tcp"}
+    local trusted_vfs=${5:-"no_trusted_vfs"}
+
+    cleanup_crypto $mtu $trusted_vfs
+    test_tx_off_rx_off $ipsec_mode 128 $ip_proto $net_proto $trusted_vfs
+}
+
+function run_test() {
+    run_test_ipsec_crypto_sanity 1500 ipv4 transport tcp
+    run_test_ipsec_crypto_sanity 1500 ipv4 transport icmp
+
+}
+
+trap cleanup EXIT
+run_test
+trap - EXIT
+cleanup
+test_done
