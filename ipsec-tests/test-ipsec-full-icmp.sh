f92ed13ef6e8b9426530b95d657c084f3ef00680
Emeel Hakim <ehakim@nvidia.com>
2022-04-27 15:50:15 +0300

    ipsec-tests: Added test-ipsec-full-icmp.sh
    Added dev-test which tests ipsec configurations in transport mode
    over icmp traffic with full offload.
    
    Test Matrix:
    -mtu 1500,9000
    -protocols: IPv4
    -full offload: one of the sides offloaded , both sides offloaded
    -encryption key length: 128,256
    
    Test pass criteria: offlaod rules are being added && counters increased
    
    Change-Id: Ib8dcc1b627278149caa92effa10c8f2870e89f39
    Signed-off-by: Emeel Hakim <ehakim@nvidia.com>

---
+++ ipsec-tests/test-ipsec-full-icmp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This is a series of basic tests to check traffic with
+# different ipsec - configurations.In addition it tests if the rules
+# which are added are using offload when expected.
+# NOTE: in this test local machine is used as Rx.
+# NOTE: tunnel mode is not included in this tests yet.
+
+my_dir="$(dirname "$0")"
+. $my_dir/common-ipsec.sh
+. $my_dir/common-ipsec-offload.sh
+
+require_remote_server
+
+function config(){
+    ipsec_set_mode full
+    ipsec_set_mode_on_remote full
+}
+
+function cleanup() {
+    cleanup_full
+}
+
+function run_test() {
+    run_test_ipsec_offload 1500 ipv4 transport icmp no_trusted_vfs full_offload
+    run_test_ipsec_offload 9000 ipv4 transport icmp no_trusted_vfs full_offload
+}
+
+trap cleanup EXIT
+config
+run_test
+trap - EXIT
+cleanup
+test_done
