8c42ac6dd11d5812f019d4775648af70b8a01e47
Emeel Hakim <ehakim@nvidia.com>
2022-05-23 18:43:51 +0300

    macsec-tests: add test-macsec-icmp-ipv4-encrypt.sh
    Change-Id: Ib9102ace1c0c028137dfa18d391ba45f82c10cef
    Signed-off-by: Emeel Hakim <ehakim@nvidia.com>

---
+++ macsec-tests/test-macsec-icmp-ipv4-encrypt.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+my_dir="$(dirname "$0")"
+. $my_dir/macsec-common.sh
+
+require_remote_server
+
+function cleanup() {
+    macsec_cleanup
+}
+
+function run_test() {
+    run_test_macsec 1500 on ipv4 ipv4 icmp off
+    run_test_macsec 1500 on ipv4 ipv4 icmp mac
+    title "re-run the test with 9000 mtu\n"
+    run_test_macsec 9000 on ipv4 ipv4 icmp off
+    run_test_macsec 9000 on ipv4 ipv4 icmp mac
+}
+
+trap cleanup EXIT
+cleanup
+run_test
+trap - EXIT
+cleanup
+test_done
