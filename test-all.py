f75b74f3940e30605ad1ca63dcaa359766afa7f2
Roi Dayan <roid@nvidia.com>
2022-06-08 14:03:36 +0300

    test-all.py: Allow to compare min_fw against all device types
    Usage example: min_fw: xx.16.3030
    
    Change-Id: I9c844119d2b29a42dcce797dcb9e0e0b5baadb62
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- test-all.py
+++ test-all.py
@@ -742,7 +742,7 @@ def update_skip_according_to_db(rm, _tests, data):
             cx_ver = min_fw[min_fw.index('.')+1:]
             current_cx_type = current_fw_ver.split('.')[0]
             current_cx_ver = current_fw_ver[current_fw_ver.index('.')+1:]
-            if cx_type == current_cx_type and VersionInfo(current_cx_ver) < VersionInfo(cx_ver):
+            if cx_type in (current_cx_type, 'xx') and VersionInfo(current_cx_ver) < VersionInfo(cx_ver):
                 t.set_ignore("Unsupported fw version. Minimum %s" % min_fw)
 
         if t.ignore:
