3d03b620f7a2a414c24ab6bf7bbf03ad81fd5654
Roi Dayan <roid@nvidia.com>
2022-06-27 14:10:30 +0300

    test-eswitch-sf.sh: Add explicit cases
    Test explicitly with sriov enable and disabled.
    
    Change-Id: I064adb5c34c6e3515afec960023cd601f9d0b859
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- test-eswitch-sf.sh
+++ test-eswitch-sf.sh
@@ -13,17 +13,34 @@ function sf_port_add_del_test() {
     create_sf 0 88
     fail_if_err
     sleep 1
-    rep=`sf_get_rep 88`
+    local rep=`sf_get_rep 88`
     sf_show_port $rep
     delete_sf $rep
     fail_if_err
 }
 
-enable_switchdev $NIC
-title "Test sf port add delete commands"
-for iter in 1 2 ; do
-    title "iter $iter"
-    sf_port_add_del_test
-done
+function test_add_del_loop() {
+    for iter in 1 2 ; do
+        title "iter $iter"
+        sf_port_add_del_test
+    done
+}
+
+function sf_without_sriov() {
+    title "Test sf ports without sriov"
+    config_sriov 0 $NIC
+    enable_switchdev $NIC
+    test_add_del_loop
+}
+
+function sf_with_sriov() {
+    title "Test sf ports with sriov"
+    config_sriov 2 $NIC
+    enable_switchdev $NIC
+    test_add_del_loop
+}
+
+sf_without_sriov
+sf_with_sriov
 
 test_done
