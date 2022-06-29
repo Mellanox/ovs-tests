db93e8fe96d781b4bd4f354b8435de13a2e50526
Roi Dayan <roid@nvidia.com>
2022-06-29 11:26:01 +0300

    test-ovs-ct-tcp-2.sh: Use shared code
    Change-Id: I2303ed20d37708d8d95a9cc4189c1ff271e0e592
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- test-ovs-ct-tcp-2.sh
+++ test-ovs-ct-tcp-2.sh
@@ -7,6 +7,7 @@
 
 my_dir="$(dirname "$0")"
 . $my_dir/common.sh
+. $my_dir/common-ovs-ct.sh
 
 require_module act_ct
 require_remote_server
@@ -18,7 +19,6 @@ enable_switchdev
 require_interfaces REP NIC
 unbind_vfs
 bind_vfs
-
 
 function set_nf_liberal() {
     nf_liberal="/proc/sys/net/netfilter/nf_conntrack_tcp_be_liberal"
@@ -91,67 +91,15 @@ function run() {
     config_remote
     add_openflow_rules
 
-    # icmp
-    ip netns exec ns0 ping -q -c 1 -i 0.1 -w 2 $REMOTE
-    if [ $? -ne 0 ]; then
-        err "ping failed"
-        return
-    fi
+    ping_remote || return
 
-    t=15
+    initial_traffic
 
-    # initial traffic
-    # this part is important when using multi-table CT.
-    # the initial traffic will cause ovs to create initial tc rules
-    # and also tuple rules. but since ovs adds the rules somewhat late
-    # conntrack will already mark the conn est. and tuple rules will be in hw.
-    # so we start second traffic which will be faster added to hw before
-    # conntrack and this will check the miss rule in our driver is ok
-    # (i.e. restoring reg_0 correctly)
-    on_remote timeout 4 iperf -s -t 4 &
-    pid1=$!
-    sleep 2
-    ip netns exec ns0 timeout 3 iperf -c $REMOTE -t 2 &
-    pid2=$!
+    start_traffic || return
 
-    sleep 4
-    kill -9 $pid1 $pid2 &>/dev/null
-    wait $pid1 $pid2 &>/dev/null
+    verify_traffic "$VF" "$REP $NIC"
 
-    # traffic
-    on_remote timeout $((t+3)) iperf -s -t $t &
-    pid1=$!
-    sleep 2
-    ip netns exec ns0 timeout $((t+2)) iperf -c $REMOTE -t $t -P3 &
-    pid2=$!
-
-    # verify pid
-    sleep 4
-    kill -0 $pid2 &>/dev/null
-    if [ $? -ne 0 ]; then
-        err "iperf failed"
-        return
-    fi
-
-    # verify traffic
-    ip netns exec ns0 timeout $((t-4)) tcpdump -qnnei $VF -c 30 tcp &
-    tpid1=$!
-    timeout $((t-4)) tcpdump -qnnei $REP -c 10 tcp &
-    tpid2=$!
-    timeout $((t-4)) tcpdump -qnnei $NIC -c 10 tcp &
-    tpid3=$!
-
-    sleep $t
-    title "Verify traffic on $VF"
-    verify_have_traffic $tpid1
-    title "Verify offload on $REP"
-    verify_no_traffic $tpid2
-    title "Verify offload on $NIC"
-    verify_no_traffic $tpid3
-
-    kill -9 $pid1 $pid2 &>/dev/null
-    echo "wait for bgs"
-    wait &>/dev/null
+    kill_traffic
 }
 
 run
