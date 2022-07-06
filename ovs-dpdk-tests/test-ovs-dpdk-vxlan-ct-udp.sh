d94eec07f9681a04ff20dc7a8a25895bf1d2df33
Salem Sol <salems@nvidia.com>
2022-07-06 11:15:35 +0300

    test-ovs-dpdk-vxlan-ct-udp.sh: Refactor test
    Refactor test to use functions from common-testing and common-tunnel to configure
    the test.
    
    Issue: 3130356
    Change-Id: Ibb7267ef00c221fb26cc96c78f915ff04352eaa0
    Signed-off-by: Salem Sol <salems@nvidia.com>

--- ovs-dpdk-tests/test-ovs-dpdk-vxlan-ct-udp.sh
+++ ovs-dpdk-tests/test-ovs-dpdk-vxlan-ct-udp.sh
@@ -10,70 +10,31 @@ my_dir="$(dirname "$0")"
 
 require_remote_server
 
-IP=1.1.1.7
-REMOTE=1.1.1.8
-
-LOCAL_TUN=7.7.7.7
-REMOTE_IP=7.7.7.8
-VXLAN_ID=42
-
 config_sriov 2
 require_interfaces REP NIC
 unbind_vfs
 bind_vfs
 
-
-function cleanup_remote() {
-    on_remote ip a flush dev $REMOTE_NIC
-    on_remote ip l del dev vxlan1 &>/dev/null
-}
-
-function cleanup() {
-    ip a flush dev $NIC
-    ip netns del ns0 &>/dev/null
-    cleanup_e2e_cache
-    cleanup_remote
-    sleep 0.5
-}
-trap cleanup EXIT
+trap cleanup_test EXIT
 
 function config() {
-    cleanup
+    cleanup_test
     set_e2e_cache_enable false
     debug "Restarting OVS"
     start_clean_openvswitch
 
-    config_simple_bridge_with_rep 0
-    config_remote_bridge_tunnel $VXLAN_ID $REMOTE_IP
-    config_local_tunnel_ip $LOCAL_TUN br-phy
-    config_ns ns0 $VF $IP
+    config_tunnel "vxlan"
+    config_local_tunnel_ip $LOCAL_TUN_IP br-phy
 }
 
 function config_remote() {
-    on_remote ip link del vxlan1 &>/dev/null
-    on_remote ip link add vxlan1 type vxlan id $VXLAN_ID remote $LOCAL_TUN dstport 4789
-    on_remote ip a flush dev $REMOTE_NIC
-    on_remote ip a add $REMOTE_IP/24 dev $REMOTE_NIC
-    on_remote ip a add $REMOTE/24 dev vxlan1
-    on_remote ip l set dev vxlan1 up
-    on_remote ip l set dev $REMOTE_NIC up
-}
-
-function add_openflow_rules() {
-    ovs-ofctl del-flows br-int
-    ovs-ofctl add-flow br-int "arp,actions=NORMAL"
-    ovs-ofctl add-flow br-int "icmp,actions=NORMAL"
-    ovs-ofctl add-flow br-int "table=0,udp,ct_state=-trk,actions=ct(zone=5, table=1)"
-    ovs-ofctl add-flow br-int "table=1,udp,ct_state=+trk+new,actions=ct(zone=5, commit),NORMAL"
-    ovs-ofctl add-flow br-int "table=1,udp,ct_state=+trk+est,ct_zone=5,actions=normal"
-    debug "OVS flow rules:"
-    ovs-ofctl dump-flows br-int --color
+    config_remote_tunnel "vxlan"
 }
 
 function run() {
     config
     config_remote
-    add_openflow_rules
+    ovs_add_ct_rules "br-int" "udp"
 
     verify_ping
     debug "Testing UDP traffic"
@@ -82,7 +43,7 @@ function run() {
     ip netns exec ns0 timeout -k 1 $((t+2)) iperf -s &
     pid1=$!
     sleep 1
-    on_remote timeout -k 1 $((t+2)) iperf -c $IP -t $t -u -l 1000 &
+    on_remote timeout -k 1 $((t+2)) iperf -c $LOCAL_IP -t $t -u -l 1000 &
     pid2=$!
 
     # verify pid
@@ -93,9 +54,8 @@ function run() {
         return
     fi
 
-    sleep $t
     # check offloads
-    check_dpdk_offloads $IP
+    check_dpdk_offloads $LOCAL_IP
 
     killall -9 iperf &>/dev/null
     debug "wait for bgs"
