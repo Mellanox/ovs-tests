439fc1213247f468edb61846074ed6c61403f85b
Roi Dayan <roid@nvidia.com>
2022-07-12 14:42:49 +0300

    test-vf-vf-ping-legacy-vport-match.sh: Remove redundant verify_timedout
    Change-Id: I620b7081568049464366ab0820a09aa58f9631ba
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- test-vf-vf-ping-legacy-vport-match.sh
+++ test-vf-vf-ping-legacy-vport-match.sh
@@ -42,13 +42,6 @@ title "Test ping $VF($IP1) -> $VF2($IP2)"
 ip netns exec ns0 ping -q -c 10 -i 0.2 -w 4 $IP2 && success || err
 
 
-function verify_timedout() {
-    local pid=$1
-    wait $pid
-    local rc=$?
-    [ $rc == 124 ] && success || err "Didn't expect to see packets"
-}
-
 function start_sniff() {
     local dev=$1
     local filter=$2
@@ -64,7 +57,7 @@ echo "start sniff $REP"
 start_sniff $REP icmp
 ip netns exec ns0 ping -q -f -w 4 $IP2 && success || err
 echo "verify tcpdump"
-verify_timedout $tpid
+verify_no_traffic $tpid
 
 trap - EXIT
 cleanup
