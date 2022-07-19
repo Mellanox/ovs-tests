e930b19480352c6bc3a65f2714f3224d38bf33f6
Roi Dayan <roid@nvidia.com>
2022-07-19 11:41:39 +0300

    test-tc-merged-esw-vf-pf.sh: Redirect output to avoid error prints
    Change-Id: I8eac9a1a29409c319d07a5c5ac40a3a856aeda16
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- test-tc-merged-esw-vf-pf.sh
+++ test-tc-merged-esw-vf-pf.sh
@@ -15,13 +15,13 @@ config_sriov 2 $NIC2
 enable_switchdev $NIC2
 reset_tc $REP $NIC2
 
-title "- add redirect rule $REP -> $NIC2 - expect to fail as we don't support this"
+title "add redirect rule $REP -> $NIC2 - expect to fail as we don't support this"
 tc filter add dev $REP protocol ip ingress prio 1 flower skip_sw action \
-    mirred egress redirect dev $NIC2 && err "Expected to fail"
+    mirred egress redirect dev $NIC2 &>/dev/null && err "Expected to fail"
 
-title "- add redirect rule $NIC2 -> $REP - expect to fail as we don't support this"
+title "add redirect rule $NIC2 -> $REP - expect to fail as we don't support this"
 tc filter add dev $NIC2 protocol ip ingress prio 1 flower skip_sw action \
-    mirred egress redirect dev $REP && err "Expected to fail"
+    mirred egress redirect dev $REP &>/dev/null && err "Expected to fail"
 
 reset_tc $REP
 config_sriov 0 $NIC2
