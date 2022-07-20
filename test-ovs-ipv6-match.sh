27c1797c1df29202b3c0efc615e6314ca757bf09
Roi Dayan <roid@nvidia.com>
2022-07-20 13:45:35 +0300

    test-ovs-ipv6-match.sh: Refactor
    Change-Id: I1c6b3026c768cc75edda406321eaef69c2ffd394
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- test-ovs-ipv6-match.sh
+++ test-ovs-ipv6-match.sh
@@ -28,37 +28,35 @@ function cleanup() {
     done
 }
 
-start_clean_openvswitch
+function config() {
+    echo "setup veth and ns"
+    ip link add veth0 type veth peer name veth1
+    ip link add veth2 type veth peer name veth3
+    ip link add veth4 type veth peer name veth5
+    ip link add veth6 type veth peer name veth7
 
-echo "setup veth and ns"
-ip link add veth0 type veth peer name veth1
-ip link add veth2 type veth peer name veth3
-ip link add veth4 type veth peer name veth5
-ip link add veth6 type veth peer name veth7
+    ip netns add ns0
+    ip link set veth3 netns ns0
+    ip netns exec ns0 ifconfig veth3 inet6 add $VM2_IP/64 up
+    ip link set veth7 netns ns0
+    ip netns exec ns0 ifconfig veth7 inet6 add $VM4_IP/64 up
 
-ip netns add ns0
-ip link set veth3 netns ns0
-ip netns exec ns0 ifconfig veth3 inet6 add $VM2_IP/64 up
-ip link set veth7 netns ns0
-ip netns exec ns0 ifconfig veth7 inet6 add $VM4_IP/64 up
+    ifconfig veth0 inet6 add $VM1_IP/64 up
+    ifconfig veth1 up
+    ifconfig veth2 up
+    ifconfig veth4 inet6 add $VM3_IP/64 up
+    ifconfig veth5 up
+    ifconfig veth6 up
 
-ifconfig veth0 inet6 add $VM1_IP/64 up
-ifconfig veth1 up
-ifconfig veth2 up
-ifconfig veth4 inet6 add $VM3_IP/64 up
-ifconfig veth5 up
-ifconfig veth6 up
+    echo "setup ovs"
+    start_clean_openvswitch
 
-echo "setup ovs"
-systemctl restart openvswitch
-sleep 2
-ovs_clear_bridges
-
-ovs-vsctl add-br brv-1
-ovs-vsctl add-port brv-1 veth1
-ovs-vsctl add-port brv-1 veth2
-ovs-vsctl add-port brv-1 veth5
-ovs-vsctl add-port brv-1 veth6
+    ovs-vsctl add-br brv-1
+    ovs-vsctl add-port brv-1 veth1
+    ovs-vsctl add-port brv-1 veth2
+    ovs-vsctl add-port brv-1 veth5
+    ovs-vsctl add-port brv-1 veth6
+}
 
 function check_offloaded_rules() {
     local count=$1
@@ -70,21 +68,25 @@ function check_offloaded_rules() {
     if (( RES == $count )); then success; else err; fi
 }
 
-MATCH="ipv6,ipv6_src=2001::/16"
-echo "add drop on match $MATCH"
-ovs-ofctl add-flow brv-1 $MATCH,actions=drop
+function run() {
+    MATCH="ipv6,ipv6_src=2001::/16"
+    echo "add drop on match $MATCH"
+    ovs-ofctl add-flow brv-1 $MATCH,actions=drop
 
-title "Test ping $VM1_IP -> $VM2_IP - expect to fail"
-ping6 -q -c 2 -i 0.25 -w 2 $VM2_IP && err "ping expected to fail" || success
+    title "Test ping $VM1_IP -> $VM2_IP - expect to fail"
+    ping6 -q -c 2 -i 0.25 -w 2 $VM2_IP && err "ping expected to fail" || success
 
-title "Verify we have 1 drop rule"
-check_offloaded_rules 1
+    title "Verify we have 1 drop rule"
+    check_offloaded_rules 1
 
-title "Test ping $VM3_IP -> $VM4_IP - expect to pass"
-ping6 -q -c 10 -i 0.2 -w 4 $VM4_IP && success || err "ping failed"
+    title "Test ping $VM3_IP -> $VM4_IP - expect to pass"
+    ping6 -q -c 10 -i 0.2 -w 4 $VM4_IP && success || err "ping failed"
 
-title "Verify we have 3 rules, 1 drop rule, 2 redirect"
-check_offloaded_rules 3
+    title "Verify we have 3 rules, 1 drop rule, 2 redirect"
+    check_offloaded_rules 3
+}
 
+config
+run
 cleanup
 test_done
