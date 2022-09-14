06d03f01b702533cc5c4d0258a7a99a20e81a90c
Emeel Hakim <ehakim@nvidia.com>
2022-09-14 18:11:32 +0300

    macsec-config: Add option to configure extended packet number (EPN)
    Change-Id: I562d086bf0b43a61b63320832cce85ac22fb61a9
    Signed-off-by: Emeel Hakim <ehakim@nvidia.com>

--- macsec-tests/macsec-config.sh
+++ macsec-tests/macsec-config.sh
@@ -47,17 +47,30 @@ function configure_macsec_interface() {
 }
 
 function configure_macsec_secrets() {
-    ip macsec add $MACSEC_IF tx sa $TX_SA pn $PACKET_NUMBER $TX_SA_STATE key $TX_ID $TX_KEY
+    local pn_cmd="pn"
+    local packet_number="$PACKET_NUMBER"
+
+    if [[ "$XPN" == "on" ]];then
+        pn_cmd="xpn"
+    fi
+
+    ip macsec add $MACSEC_IF tx sa $TX_SA $pn_cmd $packet_number $TX_SA_STATE $SALT $SSCI key $TX_ID $TX_KEY
 
     ip macsec add $MACSEC_IF rx sci $RX_SCI on
 
-    ip macsec add $MACSEC_IF rx sci $RX_SCI sa $RX_SA pn $PACKET_NUMBER $RX_SA_STATE key $RX_ID $RX_KEY
+    ip macsec add $MACSEC_IF rx sci $RX_SCI sa $RX_SA $pn_cmd $packet_number $RX_SA_STATE $SALT $SSCI_RX key $RX_ID $RX_KEY
 }
 
 function configure_macsec_multi_secrets() {
     local effective_local_key="$MULTI_LOCAL_KEY"
     local effective_remote_key="$MULTI_REMOTE_KEY"
     local i
+    local pn_cmd="pn"
+    local packet_number="$PACKET_NUMBER"
+
+    if [[ "$XPN" == "on" ]];then
+        pn_cmd="xpn"
+    fi
 
     if [[ $CIPHER == "cipher gcm-aes-256"  || $CIPHER == "cipher gcm-aes-xpn-256" ]]; then
         effective_local_key="$MULTI_LOCAL_KEY_256"
@@ -67,11 +80,11 @@ function configure_macsec_multi_secrets() {
     for i in 2 3 4; do
         if [ "$SIDE" == "server" ]; then
             ip macsec add $MACSEC_IF tx sa $((i-1)) pn $PACKET_NUMBER off key "0$i" "$effective_remote_key$i"
-        
+
             ip macsec add $MACSEC_IF rx sci $RX_SCI sa $((i-1)) pn $PACKET_NUMBER on key "0$i" "$effective_local_key$i"
         else
             ip macsec add $MACSEC_IF tx sa $((i-1)) pn $PACKET_NUMBER off key "0$i" "$effective_local_key$i"
-        
+
             ip macsec add $MACSEC_IF rx sci $RX_SCI sa $((i-1)) pn $PACKET_NUMBER on key "0$i" "$effective_remote_key$i"
         fi
     done
@@ -129,6 +142,7 @@ function usage() {
         --scb                <on|off>
         --protect            <on|off>
         --replay             <on|off>
+        --xpn                <on|off>, use extended packet number, default is off
         --window             replay window size
         --validate           <strict|check|disabled>
         --encoding-sa        <0..3> , used for configuring macsec
@@ -236,6 +250,10 @@ function parse_args()
             ENCRYPT="encrypt $2"
             shift 2
             ;;
+            --xpn)
+            XPN="$2"
+            shift 2
+            ;;
             --send-sci)
             SEND_SCI="send_sci $2"
             shift 2
@@ -286,6 +304,14 @@ function parse_args()
             RX_SCI="$2"
             shift 2
             ;;
+            --ssci)
+            SSCI="ssci $2"
+            shift 2
+            ;;
+            --rx-ssci)
+            SSCI_RX="ssci $2"
+            shift 2
+            ;;
             -h | --help) # help option
             usage 0
             ;;
@@ -294,6 +320,30 @@ function parse_args()
             ;;
         esac
     done
+}
+
+function check_xpn()
+{
+    if [[ $CIPHER == "cipher gcm-aes-xpn-128"|| $CIPHER == "cipher gcm-aes-xpn-256" ]]; then
+        XPN="on"
+    fi
+
+    if [[ $XPN == "on" ]]; then
+        #in case cipher not provided use default
+        if [[ "$CIPHER" == "" ]]; then
+            CIPHER="cipher gcm-aes-xpn-128"
+        fi
+
+        #in case salt not provided use default
+        if [[ "$SALT" == "" ]]; then
+            SALT="salt fc8d7b9a43d5b9a3dfbbf6a3"
+        fi
+
+        #in case ssci not provided use default
+        if [[ "$SSCI" == "" ]]; then
+            SSCI="ssci 1"
+        fi
+    fi
 }
 
 function check_keys()
@@ -364,6 +414,18 @@ function check_sci()
             SCI="1"
         fi
     fi
+
+    #if only SSCI is passed then use same SSCI for SSCI_RX
+    if [[ "$SSCI" != "" && "$SSCI_RX" == "" ]]; then
+            SSCI_RX=$SSCI
+    fi
+
+    #if on server swap ssci's
+    if [[ "$SIDE" == "server" ]]; then
+            tmp=$SSCI
+            SSCI=$SSCI_RX
+            SSCI_RX=$tmp
+    fi
 }
 
 function main()
@@ -389,6 +451,8 @@ function main()
         exit 0
     fi
 
+    #Check for extended packet number
+    check_xpn
     #Check if to use default keys
     check_keys
     #Check if to use default IPs
