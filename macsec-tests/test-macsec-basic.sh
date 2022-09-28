647c2885a97d5f2d8989375c0a46be93b0a659b4
Emeel Hakim <ehakim@nvidia.com>
2022-09-28 16:28:15 +0300

    macsec-tests: Add test-macsec-basic.sh
    Add a basic test which covers the 9000 mtu case too.
    
    Change-Id: Ia247c77c219e4933e2e658cac7ba1176b59da6f4
    Signed-off-by: Emeel Hakim <ehakim@nvidia.com>

---
+++ macsec-tests/test-macsec-basic.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+my_dir="$(dirname "$0")"
+. $my_dir/macsec-common.sh
+
+require_remote_server
+
+function config() {
+    config_macsec_env
+}
+
+function cleanup() {
+    macsec_cleanup
+}
+
+function run_test() {
+    run_test_macsec 1500 on ipv4 ipv4 icmp mac
+    run_test_macsec 1500 on ipv4 ipv4 tcp mac
+    title "re-run the test with 9000 mtu\n"
+    run_test_macsec 9000 on ipv4 ipv4 icmp mac
+    run_test_macsec 9000 on ipv4 ipv4 tcp mac
+}
+
+trap cleanup EXIT
+cleanup
+config
+run_test
+trap - EXIT
+cleanup
+test_done
