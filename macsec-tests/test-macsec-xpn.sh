e5a2cd1feddde8f4fcc30d9b1ecf2eb4215af195
Emeel Hakim <ehakim@nvidia.com>
2022-09-29 13:53:03 +0300

    macsec-tests: Add test-macsec-xpn.sh
    Change-Id: I4c9124fb925a51941fdfa509fb1bb42a5d929666
    Signed-off-by: Emeel Hakim <ehakim@nvidia.com>

---
+++ macsec-tests/test-macsec-xpn.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+my_dir="$(dirname "$0")"
+. $my_dir/macsec-common.sh
+
+require_remote_server
+
+function config() {
+    config_macsec_env
+}
+
+function cleanup() {
+    macsec_cleanup
+}
+
+function run_test() {
+    run_test_macsec 1500 on ipv4 ipv4 icmp off off dont_use on
+    run_test_macsec 1500 on ipv4 ipv4 icmp mac off dont_use on
+}
+
+trap cleanup EXIT
+cleanup
+config
+run_test
+trap - EXIT
+cleanup
+test_done
