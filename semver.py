188b276d68f8c5761efe0a523287f9c4f6fd2dd1
Roi Dayan <roid@nvidia.com>
2022-10-19 09:43:42 +0300

    semver.py: Fix comparing rc in extra named kernels
    e.g. 6.1.0-rc1_for_upstream_min_debug_2022_10_18_15_06
    
    Change-Id: I867cb8973cdb890c24ddc30a4923791dc6585816
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- semver.py
+++ semver.py
@@ -30,14 +30,22 @@ class VersionInfo(object):
         self.plus = self.version[-1] == '+'
         stripped_version = self.version.strip('+')
         s = re.sub('[.-]', ' ', stripped_version).split()
-        # Quick WA to avoid split errors for short versions
+        # Quick WA to avoid split errors for short versions.
         s += [0, 0, 0, 0]
         self.major = tryint(s[0])
         self.minor = tryint(s[1])
         self.patch = tryint(s[2])
         self.build = tryint(s[3])
+
+        # rc tags appear in the build part.
         # e.g. 5.17.0-rc6
-        # rc tags appear in the build part
+        #      6.1.0-rc1_for_upstream_min_debug_2022_10_18_15_06
+        self.extra = ''
+        if '_' in str(self.build):
+            i = self.build.index('_')
+            self.extra = self.build[i+1:]
+            self.build = self.build[:i]
+
         self.rc = 0
         if 'rc' in str(self.build):
             self.rc = int(self.build.strip('rc'))
