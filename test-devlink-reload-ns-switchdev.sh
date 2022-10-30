10e962cf8613d11a6bba1bfcde47515951400282
Roi Dayan <roid@nvidia.com>
2022-10-30 14:05:04 +0200

    test-devlink-reload-ns-switchdev.sh: Add new test
    Change-Id: I0d1319cad84978934b6c72b6ff33e8b22ea3a9c7
    Signed-off-by: Roi Dayan <roid@nvidia.com>

---
+++ test-devlink-reload-ns-switchdev.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Test devlink reload in nic mode while adding/deleting net namespace
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+
+function toggle_ns() {
+    local ns
+    for ns in ns0 ns1 ns2; do
+        ip netns add $ns
+    done
+    sleep 1
+    for ns in ns0 ns1 ns2; do
+        ip netns del $ns
+    done
+}
+
+function start1() {
+    local i
+
+    title "devlink reload"
+    toggle_ns &
+    for i in 1 2; do
+        devlink dev reload pci/$PCI
+    done
+    echo wait
+    wait
+}
+
+config_sriov 0
+enable_switchdev
+start1
+config_sriov 2
+
+test_done
