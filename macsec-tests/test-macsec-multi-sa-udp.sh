b6a8c756d50c7d85f554f9b80c50ee9e8867f86e
Emeel Hakim <ehakim@nvidia.com>
2022-11-08 15:45:14 +0200

    Revert "macsec-tests: Remove multi-sa tests since its a none supported flow"
    After some fixes and macsec changes we managed to support the flow.
    This reverts commit 96c56b95899cfd02f31eff77da1e5a4b4a6476d9.
    
    Change-Id: I10d4f0ec7cb2f184a5276af349d216e291cea499
    Signed-off-by: Emeel Hakim <ehakim@nvidia.com>

---
+++ macsec-tests/test-macsec-multi-sa-udp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+my_dir="$(dirname "$0")"
+. $my_dir/macsec-common.sh
+
+require_remote_server
+
+function config() {
+    config_macsec_env
+}
+
+function cleanup() {
+    macsec_cleanup
+}
+
+function run_test() {
+    run_test_macsec 1500 ipv4 ipv4 udp both on
+}
+
+trap cleanup EXIT
+cleanup
+config
+run_test
+trap - EXIT
+cleanup
+test_done
