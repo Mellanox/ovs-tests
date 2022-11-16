6cd6f7fe2fc7b9f48e67d516cd66a05df553c849
Roi Dayan <roid@nvidia.com>
2022-11-16 11:57:28 +0200

    test-eswitch-pci-reset.sh: set rm info
    Change-Id: I69de7b13131388ae01a38e280940ba4887780706
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- test-eswitch-pci-reset.sh
+++ test-eswitch-pci-reset.sh
@@ -2,6 +2,7 @@
 #
 # Test PCI reset while in switchdev mode and reload modules
 #
+# Bug SW #2805586: [Upstream] null-ptr-deref in mlx5e_suspend+0x9c/0x130 while reseting PCI in switchdev mode
 
 my_dir="$(dirname "$0")"
 . $my_dir/common.sh
