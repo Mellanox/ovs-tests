5fbe36254e424813adc1b294f3361e0929e42432
Mahmoud AbuZant <mahmoudabuz@nvidia.com>
2022-12-07 12:05:41 +0200

    common-ovn.sh: Add WA over dpdk to openstack tests
    Issue: 3287703
    Change-Id: I55cefe39acfd9ce887d25c2d5523260a3dad552e
    Signed-off-by: Mahmoud AbuZant <mahmoudabuz@nvidia.com>

--- ovn-tests/basic/common-ovn-basic-test.sh
+++ ovn-tests/basic/common-ovn-basic-test.sh
@@ -1,13 +1,3 @@
 OVN_BASIC_DIR=$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)
 . $OVN_BASIC_DIR/../common-ovn-test.sh
 . $OVN_BASIC_DIR/common-ovn-basic.sh
-
-
-function WA_dpdk_initial_ping_and_flush() {
-    if [[ "$DPDK" == 1 ]]; then
-        # WA RM #3287703 require initial traffic + flush to start working.
-        echo "Init traffic"
-        ip netns exec $CLIENT_NS ping -w 1 $SERVER_IPV4 &> /dev/null
-        ovs_flush_rules
-    fi
-}
--- ovn-tests/common-ovn.sh
+++ ovn-tests/common-ovn.sh
@@ -708,3 +708,12 @@ function ovn_lsp_set_tag() {
 
     ovn-nbctl set LOGICAL_SWITCH_PORT $port tag=$tag
 }
+
+function WA_dpdk_initial_ping_and_flush() {
+    if [[ "$DPDK" == 1 ]]; then
+        # WA RM #3287703 require initial traffic + flush to start working.
+        echo "Init traffic"
+        ip netns exec $CLIENT_NS ping -w 1 $SERVER_IPV4 &> /dev/null
+        ovs_flush_rules
+    fi
+}
