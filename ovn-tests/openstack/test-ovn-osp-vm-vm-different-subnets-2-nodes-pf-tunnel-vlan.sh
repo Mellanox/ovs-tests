97c53fbe6306d200f9e8635b1c9a877d49b4934a
Mahmoud AbuZant <mahmoudabuz@nvidia.com>
2022-12-13 16:42:59 +0200

    ovn-tests: openstack: Add new vlan vst tests
    Issue: 3298255
    Change-Id: I5299f57e3053594bd4933812866d1f4339ba6fa0
    Signed-off-by: Mahmoud AbuZant <mahmoudabuz@nvidia.com>

---
+++ ovn-tests/openstack/test-ovn-osp-vm-vm-different-subnets-2-nodes-pf-tunnel-vlan.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#
+# Test traffic VM to VM on different subnet different nodes with tunnel over vlan configured with OSP and OVN then verify traffic is offloaded
+#
+
+CONFIG_REMOTE=1
+
+my_dir="$(dirname "$0")"
+. $my_dir/common-ovn-osp-test.sh
+
+require_interfaces NIC
+require_remote_server
+
+read_osp_topology_vm_vm_different_subnets
+ovn_set_ips
+
+function config_test() {
+    ovn_start_northd_central $ovn_central_ip
+    ovn_create_topology
+
+    config_ovn_pf_vlan $ovn_central_ip $ovn_controller_ip CLIENT_VF CLIENT_REP
+    ovn_config_interface_namespace $CLIENT_VF $CLIENT_REP $CLIENT_NS $CLIENT_PORT $CLIENT_MAC $CLIENT_IPV4 $CLIENT_IPV6 $CLIENT_GATEWAY_IPV4 $CLIENT_GATEWAY_IPV6
+
+    on_remote_exec "config_ovn_pf_vlan $ovn_central_ip $ovn_remote_controller_ip SERVER_VF SERVER_REP
+                    ovn_config_interface_namespace $SERVER_VF $SERVER_REP $SERVER_NS $SERVER_PORT $SERVER_MAC $SERVER_IPV4 $SERVER_IPV6 $SERVER_GATEWAY_IPV4 $SERVER_GATEWAY_IPV6"
+}
+
+function run_test() {
+    ovs-vsctl show
+    ovn-sbctl show
+
+    WA_dpdk_initial_ping_and_flush
+
+    title "Test ICMP traffic between $CLIENT_VF($CLIENT_IPV4) -> $SERVER_VF($SERVER_IPV4) offloaded"
+    check_icmp_traffic_offload $SERVER_IPV4
+
+    title "Test TCP traffic between $CLIENT_VF($CLIENT_IPV4) -> $SERVER_VF($SERVER_IPV4) offloaded"
+    check_remote_tcp_traffic_offload $SERVER_IPV4
+
+    title "Test UDP traffic between $CLIENT_VF($CLIENT_IPV4) -> $SERVER_VF($SERVER_IPV4) offloaded"
+    check_remote_udp_traffic_offload $SERVER_IPV4
+
+    # ICMP6 offloading is not supported because IPv6 packet header doesn't contain checksum header
+    # which cause offloading to fail
+    title "Test ICMP6 traffic between $CLIENT_VF($CLIENT_IPV6) -> $SERVER_VF($SERVER_IPV6)"
+    ip netns exec $CLIENT_NS ping -6 -w 4 $SERVER_IPV6 && success || err
+
+    title "Test TCP6 traffic between $CLIENT_VF($CLIENT_IPV6) -> $SERVER_VF($SERVER_IPV6) offloaded"
+    check_remote_tcp6_traffic_offload $SERVER_IPV6
+
+    title "Test UDP6 traffic between $CLIENT_VF($CLIENT_IPV6) -> $SERVER_VF($SERVER_IPV6) offloaded"
+    check_remote_udp6_traffic_offload $SERVER_IPV6
+}
+
+ovn_clean_up
+trap ovn_clean_up EXIT
+
+config_test
+run_test
+
+trap - EXIT
+ovn_clean_up
+
+test_done
