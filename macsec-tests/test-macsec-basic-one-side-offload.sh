3f4203b46d1490ec84432dd06bbfeb492d2bcc0a
Emeel Hakim <ehakim@nvidia.com>
2022-12-15 15:44:20 +0200

    macsec-tests: add test-macsec-basic-one-side-offload.sh
    Change-Id: I21436a9265103c4ff3f28f4c0456c17201432f8e
    Signed-off-by: Emeel Hakim <ehakim@nvidia.com>

---
+++ macsec-tests/test-macsec-basic-one-side-offload.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#Test macsec basic functionality while
+#only one side is offloaded to test if
+#the driver stays in sync with the kernel stack
+
+my_dir="$(dirname "$0")"
+. $my_dir/macsec-common.sh
+
+require_remote_server
+
+function config() {
+    config_macsec_env
+}
+
+function cleanup() {
+    macsec_cleanup
+}
+
+function run_test() {
+    run_test_macsec 1500 ipv4 ipv4 icmp local
+    run_test_macsec 1500 ipv4 ipv4 tcp local
+
+    echo
+    title "re-run the test with 9000 mtu"
+
+    run_test_macsec 9000 ipv4 ipv4 icmp local
+    run_test_macsec 9000 ipv4 ipv4 tcp local
+}
+
+trap cleanup EXIT
+cleanup
+config
+run_test
+trap - EXIT
+cleanup
+test_done
