8717a49b61a9f34db180ee645321a83438b9a861
Isra Mousa <isram@nvidia.com>
2023-01-22 15:37:32 +0200

    test-ethtool-nic-mode.sh: Fix test
    Enabling lro need rx_striding_rq to be enabled.
    so, Added step to enable rx_striding_rq in case it is not.
    
    Issue: 3337836
    Signed-off-by: Isra Mousa <isram@nvidia.com>
    Change-Id: If0542268eb174d8883aba26099246eca56cfe6ee

--- test-ethtool-nic-mode.sh
+++ test-ethtool-nic-mode.sh
@@ -15,9 +15,21 @@ function chk_lro() {
     success
 }
 
+function chk_rx_striding_rq() {
+    local failed=0
+    local rx_striding_rq=`ethtool --show-priv-flags ${NIC2} | grep rx_striding_rq | awk {'print $3'}`
+    if [ "$rx_striding_rq" == "off" ]; then
+        ethtool --set-priv-flags $NIC2 rx_striding_rq on || failed=1
+        [ $failed == 1 ] && err "Failed to enable rx_striding_rq" && return
+        success
+    fi
+
+}
+
 function do_test() {
     title "Verify nic ethtool features"
-
+    title "Test rx_striding_rq"
+    chk_rx_striding_rq
     title "Test LRO"
     chk_lro
 }
