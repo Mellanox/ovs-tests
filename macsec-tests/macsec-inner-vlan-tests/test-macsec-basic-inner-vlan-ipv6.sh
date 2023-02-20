b8676e80aaac9dce7b44848904b34ffbbf360835
Emeel Hakim <ehakim@nvidia.com>
2023-02-20 19:32:42 +0200

    macsec-tests: Add macsec inner vlan ipv6 tests
    Change-Id: I1d76fd1862c7258a65de5ca505517f16024c518a
    Signed-off-by: Emeel Hakim <ehakim@nvidia.com>

---
+++ macsec-tests/macsec-inner-vlan-tests/test-macsec-basic-inner-vlan-ipv6.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+my_dir="$(dirname "$0")"
+. $my_dir/../macsec-common.sh
+
+require_remote_server
+
+function config() {
+    config_macsec_env
+}
+
+function cleanup() {
+    macsec_cleanup
+}
+
+function run_test() {
+    run_test_macsec --mtu 1500 --ip-proto ipv4 --macsec-ip-proto ipv4 --net-proto icmp --offload-side both --inner-vlan --vlan-ip-proto ipv6
+    run_test_macsec --mtu 1500 --ip-proto ipv4 --macsec-ip-proto ipv4 --net-proto tcp --offload-side both --inner-vlan --vlan-ip-proto ipv6
+    title "re-run the test with 9000 mtu\n"
+    run_test_macsec --mtu 9000 --ip-proto ipv4 --macsec-ip-proto ipv4 --net-proto icmp --offload-side both --inner-vlan --vlan-ip-proto ipv6
+    run_test_macsec --mtu 9000 --ip-proto ipv4 --macsec-ip-proto ipv4 --net-proto tcp --offload-side both --inner-vlan --vlan-ip-proto ipv6
+}
+
+trap cleanup EXIT
+cleanup
+config
+run_test
+trap - EXIT
+cleanup
+test_done
