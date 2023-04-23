1c936efea2f6ea215569816248b2794afe399168
Roi Dayan <roid@nvidia.com>
2023-04-23 14:12:03 +0300

    test-sf-esw.sh: Add new test for SF ESW
    Change-Id: I8d74c000650d3f02ab097bb7ae3da1146c18846e
    Signed-off-by: Roi Dayan <roid@nvidia.com>

---
+++ test-sf-esw.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# Test traffic from SF to SF REP when SF in switchdev mode.
+
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+. $my_dir/common-sf.sh
+
+IP1="7.7.7.1"
+IP2="7.7.7.2"
+
+function cleanup() {
+    ip -all netns delete
+    remove_sfs
+}
+
+trap cleanup EXIT
+
+function config() {
+    title "Config"
+    create_sfs 1
+
+    unbind_sfs
+    title "Set SF switch id"
+    ~roid/SWS/gerrit2/iproute2/devlink/devlink port function set pci/0000:08:00.0/32768 sw_id aaa || fail "Failed to set sf switch id"
+    bind_sfs
+
+    title "Set SF switchdev"
+    devlink dev eswitch set auxiliary/mlx5_core.sf.2 mode switchdev || fail "Failed to config sf switchdev"
+
+    config_vf ns0 $SF1 $SF_REP1 $IP1
+    ifconfig $SF_REP1 $IP2 up
+}
+
+function run_traffic() {
+    t=5
+
+    title "Ping SF $SF1 -> SF_REP $SF_REP1, sniff $SF_REP1"
+    timeout $t tcpdump -qnnei $SF_REP1 -c 5 "icmp or arp" &
+    pid1=$!
+    sleep 0.1
+    ip netns exec ns0 timeout $((t+1)) ping -c 10 -i 0.1 $IP2
+    verify_have_traffic $pid1
+
+    title "Ping SF_REP $SF_REP1 -> SF $SF1, sniff $SF1"
+    ip netns exec ns0 timeout $t tcpdump -qnnei $SF1 -c 5 "icmp or arp" &
+    pid1=$!
+    sleep 0.1
+    timeout $((t+1)) ping -c 10 -i 0.1 $IP1
+    verify_have_traffic $pid1
+}
+
+enable_switchdev
+config
+run_traffic
+trap - EXIT
+cleanup
+test_done
