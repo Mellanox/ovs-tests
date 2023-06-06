caacb368162a538b220d0db183bc9af332761d5b
Emeel Hakim <ehakim@nvidia.com>
2023-06-06 18:04:11 +0300

    ovs-dpdk-tests: Add test-ovs-dpdk-esw-mgr-removal.sh
    Change-Id: I11d72b90a88ab34ca2c75da59e9ac30166b34f44
    Signed-off-by: Emeel Hakim <ehakim@nvidia.com>

---
+++ ovs-dpdk-tests/test-ovs-dpdk-esw-mgr-removal.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# This test will yield some OVS errors which are expected.
+# This will test removal of an esw manager port prior to its members.
+# We expect the members to get a "Resource temporarily unavailable" Error.
+
+my_dir="$(dirname "$0")"
+. $my_dir/common-dpdk.sh
+
+config_sriov 2
+enable_switchdev
+require_interfaces REP NIC
+unbind_vfs
+bind_vfs
+
+trap cleanup_test EXIT
+
+function config() {
+    cleanup_test
+    set_e2e_cache_enable false
+    debug "Restarting OVS"
+    start_clean_openvswitch
+
+    config_simple_bridge_with_rep 2
+    config_ns ns0 $VF $LOCAL_IP
+    config_ns ns1 $VF2 $REMOTE_IP
+}
+
+function del_and_add_pf() {
+    local pci=$(get_pf_pci)
+    local msg="Resource temporarily unavailable"
+
+    debug "Del port pf"
+    ovs-vsctl del-port pf
+
+    verify_ovs_expected_msg "$msg"
+    debug "Add port pf back"
+    ovs_add_pf
+}
+
+function run() {
+    config
+
+    del_and_add_pf
+
+    verify_ping
+
+    generate_traffic "local" $LOCAL_IP ns1
+}
+
+run
+trap - EXIT
+cleanup_test
+test_done
