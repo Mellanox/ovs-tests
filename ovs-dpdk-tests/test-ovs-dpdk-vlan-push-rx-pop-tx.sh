597e7380092fe7ed31a30c91836e04fc089cc96a
Salem Sol <salems@nvidia.com>
2023-06-07 15:57:52 +0300

    ovs-dpdk-tests: test-ovs-dpdk-vlan-push-rx-pop-tx: Add new test
    Add new test test-ovs-dpdk-vlan-push-rx-pop-tx.sh which test vlan_push on RX
    and vlan_pop on TX.
    
    Change-Id: Iaa004943589f5d196bdbe1c70fe5c94a6f8ae134
    Signed-off-by: Salem Sol <salems@nvidia.com>

---
+++ ovs-dpdk-tests/test-ovs-dpdk-vlan-push-rx-pop-tx.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Test OVS with vxlan traffic
+#
+# Require external server
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common-dpdk.sh
+
+require_remote_server
+
+config_sriov 2
+require_interfaces REP NIC
+unbind_vfs
+bind_vfs
+
+VLAN_ID=7
+VLAN_DEV=${VF}.${VLAN_ID}
+
+trap cleanup_test EXIT
+
+function config() {
+    cleanup_test
+    set_e2e_cache_enable false
+    debug "Restarting OVS"
+    start_clean_openvswitch
+
+    config_simple_bridge_with_rep 0
+    config_remote_bridge_tunnel $TUNNEL_ID $REMOTE_TUNNEL_IP
+    config_vlan_device_ns $VF $VLAN_DEV $VLAN_ID $LOCAL_IP $LOCAL_IP "ns0"
+    debug "Removing $VF ip address"
+    ip netns exec ns0 ifconfig $VF 0
+    config_local_tunnel_ip $LOCAL_TUN_IP br-phy
+}
+
+function add_openflow_rules() {
+    debug "Adding openflow rules"
+    ovs-ofctl del-flows br-int
+    ovs-ofctl add-flow br-int 'in_port=vxlan0,actions=push_vlan:0x8100,mod_vlan_vid:7, output:rep0' -O OpenFlow11
+    ovs-ofctl add-flow br-int 'in_port=rep0,dl_vlan=7,actions=pop_vlan, output:vxlan0' -O OpenFlow11
+    ovs-ofctl dump-flows br-int --color
+}
+
+function run() {
+    config
+    config_remote_tunnel "vxlan"
+    add_openflow_rules
+
+    # icmp
+    verify_ping $REMOTE_IP ns0
+
+    generate_traffic "remote" $LOCAL_IP
+}
+
+run
+start_clean_openvswitch
+trap - EXIT
+cleanup_test
+test_done
