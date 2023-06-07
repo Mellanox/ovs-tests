ea3fc7a756ad34fa9507ca9c2f56d4ee95eb4413
Emeel Hakim <ehakim@nvidia.com>
2023-06-07 16:28:13 +0300

    ovs-dpdk-tests: Add test-ovs-dpdk-esw-mgr-removal-with-offloads.sh
    Change-Id: I52613747287a3e834be4d69424933d16ada2cc26
    Signed-off-by: Emeel Hakim <ehakim@nvidia.com>

---
+++ ovs-dpdk-tests/test-ovs-dpdk-esw-mgr-removal-with-offloads.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# This test will yield some OVS errors which are expected.
+# This will test removal of an esw manager port while having offloads.
+# We expect the members to get a "Resource temporarily unavailable" error
+# and we expect to still be able to offload rules after re-attaching the PF.
+
+my_dir="$(dirname "$0")"
+. $my_dir/common-dpdk.sh
+
+config_sriov 2
+enable_switchdev
+require_interfaces REP NIC
+unbind_vfs
+bind_vfs
+
+trap cleanup EXIT
+
+function cleanup() {
+    cleanup_test
+    ovs_conf_remove max-idle
+}
+
+function config() {
+    cleanup_test
+    set_e2e_cache_enable false
+    debug "Restarting OVS"
+    start_clean_openvswitch
+
+    config_simple_bridge_with_rep 2
+    config_ns ns0 $VF $LOCAL_IP
+    config_ns ns1 $VF2 $REMOTE_IP
+    ovs_conf_set max-idle 300000
+}
+
+function del_and_add_pf() {
+    local pci=$(get_pf_pci)
+
+    debug "Del port pf"
+    ovs-vsctl del-port pf
+    debug "Add port pf"
+    ovs_add_pf
+}
+
+function run() {
+    config
+
+    verify_ping $REMOTE_IP ns0 56 50
+
+    validate_offload $LOCAL_IP
+
+    del_and_add_pf
+
+    sleep 3
+
+    verify_ping $REMOTE_IP ns0 56 50
+
+    validate_offload $LOCAL_IP
+}
+
+run
+trap - EXIT
+cleanup
+test_done
