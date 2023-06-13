0e14f046703099a7741b34927a6b0368884125d7
Roi Dayan <roid@nvidia.com>
2023-06-13 11:49:19 +0300

    test-sf-pair-esw.sh: refactor to be more generic
    Change-Id: I33d981a2149b1008ee8fba8b23bf2aed11a6b074
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- test-sf-pair-esw.sh
+++ test-sf-pair-esw.sh
@@ -12,8 +12,10 @@ IP2="7.7.7.2"
 
 function cleanup() {
     log "cleanup"
-    for i in 2 3 4; do
-        ip netns ls | grep -q ns0 && ip netns exec ns0 devlink dev reload auxiliary/mlx5_core.sf.$i netns 1
+    local i sfs
+    ip netns ls | grep -q ns0 && sfs=`ip netns exec ns0 devlink dev | grep -w sf`
+    for i in $sfs; do
+        ip netns exec ns0 devlink dev reload $i netns 1
     done
     ip -all netns delete
     remove_sfs
@@ -22,11 +24,14 @@ function cleanup() {
 trap cleanup EXIT
 
 function set_sf_esw() {
+    local count=$1
+    local i ids a sf
+
     title "Set SF eswitch"
 
     # Failing to change fw with sf inactive but works with unbind.
     unbind_sfs
-    for i in 68 69 70; do
+    for i in `seq 68 $((68-1+$count))`; do
         ~roid/SWS/gerrit2/iproute2/devlink/devlink port function set pci/0000:08:00.0/327$i eswitch enable || err "Failed to set SF eswitch"
         ~roid/SWS/gerrit2/iproute2/devlink/devlink port show pci/0000:08:00.0/327$i
     done
@@ -34,68 +39,91 @@ function set_sf_esw() {
 
     fail_if_err
 
-    for i in $SF1 $SF2 $SF3; do
-        echo "SF $i phys_switch_id `cat /sys/class/net/$i/phys_switch_id`" || fail "Failed to get SF switch id"
+    for i in `seq $count`; do
+        a="SF$i"
+        sf=${!a}
+        echo "SF $sf phys_switch_id `cat /sys/class/net/$sf/phys_switch_id`" || fail "Failed to get SF switch id"
     done
 }
 
 function set_sf_switchdev() {
+    local count=$1
+    local i
+
     title "Set SF switchdev"
-    for i in 2 3 4; do
+
+    for i in `seq 2 $((1+$count))`; do
         ip netns exec ns0 devlink dev eswitch set auxiliary/mlx5_core.sf.$i mode switchdev || fail "Failed to config SF switchdev"
     done
 }
 
+function reload_sfs_into_ns() {
+    local count=$1
+
+    title "Reload SF into ns0"
+
+    ip netns add ns0
+    for i in `seq 2 $((1+$count))`; do
+        devlink dev reload auxiliary/mlx5_core.sf.$i netns ns0 || fail "Failed to reload SF"
+    done
+}
+
 function verify_single_ib_device() {
+    local expected=$1
+
     title "Verify single IB device with multiple ports"
+
     rdma link show | grep -w mlx5_0
     local count=`rdma link show | grep -w mlx5_0 | wc -l`
-    if [ "$count" -ne 6 ]; then
-        err "Expected 6 ports"
+    if [ "$count" -ne $expected ]; then
+        err "Expected $expected ports"
     else
         success
     fi
 }
 
 function config() {
+    local count=$1
+
     title "Config"
-    create_sfs 3
-    ~roid/SWS/gerrit2/iproute2/devlink/devlink port show pci/0000:08:00.0/32768
-    ~roid/SWS/gerrit2/iproute2/devlink/devlink port show pci/0000:08:00.0/32769
-    ~roid/SWS/gerrit2/iproute2/devlink/devlink port show pci/0000:08:00.0/32770
+    create_sfs $count
+    set_sf_esw $count
+    reload_sfs_into_ns $count
+    set_sf_switchdev $count
+    verify_single_ib_device $((count*2))
+}
 
-    set_sf_esw
+function test_ping_single() {
+    local id=$1
+    local a sf sf_rep ip1 ip2
 
-    title "Reload SF into ns0"
-    ip netns add ns0
-    for i in 2 3 4; do
-        devlink dev reload auxiliary/mlx5_core.sf.$i netns ns0 || fail "Failed to reload SF"
-    done
+    sf="eth$((-1+$id))"
+    a="SF_REP$id"
+    sf_rep=${!a}
 
-    set_sf_switchdev
-    verify_single_ib_device
+    ip1="$id.$id.$id.1"
+    ip2="$id.$id.$id.2"
+
+    title "Verify ping SF$id $sf<->$sf_rep"
+
+    ip netns exec ns0 ifconfig $sf $ip1/24 up
+    ifconfig $sf_rep $ip2/24 up
+    ping -w 3 -c 2 $ip1 && success || err "Ping failed for SF$id $sf<->$sf_rep"
 }
 
 function test_ping() {
-    title "Verify ping SF1 eth0<->$SF_REP1"
-    ip netns exec ns0 ifconfig eth0 1.1.1.1/24 up
-    ifconfig $SF_REP1 1.1.1.2/24 up
-    ping -w 3 -c 2 1.1.1.1 && success || err "Ping failed for SF1 eth0<->$SF_REP1"
+    local count=$1
+    local i
 
-    title "Verify ping SF2 eth1<->$SF_REP2"
-    ip netns exec ns0 ifconfig eth1 2.2.2.1/24 up
-    ifconfig $SF_REP2 2.2.2.2/24 up
-    ping -w 3 -c 2 2.2.2.1 && success || err "Ping failed for SF2 eth1<->$SF_REP2"
-
-    title "Verify ping SF3 eth2<->$SF_REP3"
-    ip netns exec ns0 ifconfig eth2 3.3.3.1/24 up
-    ifconfig $SF_REP3 3.3.3.2/24 up
-    ping -w 3 -c 2 3.3.3.1 && success || err "Ping failed for SF3 eth2<->$SF_REP3"
+    for i in `seq $count`; do
+        test_ping_single $i
+    done
 }
 
 enable_switchdev
-config
-test_ping
+test_count=3
+config $test_count
+test_ping $test_count
 trap - EXIT
 cleanup
 test_done
