fa7c85d7065a80f2cbd443c8be4df13477fde2d4
Ziyad Atiyyeh <ziyadat@nvidia.com>
2023-06-20 14:19:22 +0300

    Skip expected dmesg error in eswitch-pci-reset tests
    Issue: 2805586
    Change-Id: I0c74fbe1ff3044b26eb7870b84996c5b82b29b53
    Signed-off-by: Ziyad Atiyyeh <ziyadat@nvidia.com>

--- test-eswitch-pci-reset.sh
+++ test-eswitch-pci-reset.sh
@@ -2,11 +2,15 @@
 #
 # Test PCI reset while in switchdev mode and reload modules
 #
-# Bug SW #2805586: [Upstream] null-ptr-deref in mlx5e_suspend+0x9c/0x130 while reseting PCI in switchdev mode
+# Bug SW #2805586: [Upstream] null-ptr-deref in mlx5e_suspend+0x9c/0x130 while resetting PCI in switchdev mode
 
 my_dir="$(dirname "$0")"
 . $my_dir/common.sh
 
+if [[ `uname -r` == *"upstream"* ]] || [[ `uname -r` == *"linust"* ]]; then
+    # Check comments in Bug SW #2805586: [Upstream] null-ptr-deref in mlx5e_suspend+0x9c/0x130 while resetting PCI in switchdev mode
+    add_expected_error_msg ".*E-Switch: Getting vhca_id for vport failed \(vport=.*,err=-67\)"
+fi
 
 config_sriov
 enable_switchdev
--- test-eswitch-pci-reset2.sh
+++ test-eswitch-pci-reset2.sh
@@ -7,6 +7,11 @@ my_dir="$(dirname "$0")"
 . $my_dir/common.sh
 
 
+if [[ `uname -r` == *"upstream"* ]] || [[ `uname -r` == *"linust"* ]]; then
+    # Check comments in Bug SW #2805586: [Upstream] null-ptr-deref in mlx5e_suspend+0x9c/0x130 while resetting PCI in switchdev mode
+    add_expected_error_msg ".*E-Switch: Getting vhca_id for vport failed \(vport=.*,err=-67\)"
+fi
+
 config_sriov
 enable_switchdev
 pci_reset
