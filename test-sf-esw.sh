ae5cd5b82a02a485660faeac3bf410c79157472f
Roi Dayan <roid@nvidia.com>
2023-06-27 12:05:21 +0300

    test-sf-esw.sh: generic cleanup
    Change-Id: Ib9250e30c14d11cb60f3db6d7c22e13c6ba7b516
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- test-sf-esw.sh
+++ test-sf-esw.sh
@@ -11,7 +11,12 @@ IP1="7.7.7.1"
 IP2="7.7.7.2"
 
 function cleanup() {
-    ip netns ls | grep -q ns0 && ip netns exec ns0 devlink dev reload auxiliary/mlx5_core.sf.2 netns 1
+    log "cleanup"
+    local i sfs
+    ip netns ls | grep -q ns0 && sfs=`ip netns exec ns0 devlink dev | grep -w sf`
+    for i in $sfs; do
+        ip netns exec ns0 devlink dev reload $i netns 1
+    done
     ip -all netns delete
     remove_sfs
 }
