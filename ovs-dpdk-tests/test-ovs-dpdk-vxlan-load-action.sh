ebaf110ca4fc9c1b5da28ced9669169e8d4f29b8
Mahmoud AbuZant <mahmoudabuz@nvidia.com>
2023-07-09 12:47:06 +0300

    test-ovs-dpdk-vxlan-load-action.sh: Adapt the test to work over bf
    Change-Id: I245da52dd8ff055b6b2dcdf8ec2a141e0ec8a8cd
    Signed-off-by: Mahmoud AbuZant <mahmoudabuz@nvidia.com>

--- ovs-dpdk-tests/test-ovs-dpdk-vxlan-load-action.sh
+++ ovs-dpdk-tests/test-ovs-dpdk-vxlan-load-action.sh
@@ -51,22 +51,22 @@ function add_openflow_rules() {
     VXLAN_MAC=$(on_remote cat /sys/class/net/$TUNNEL_DEV/address)
     ovs-ofctl del-flows br-int
     ovs-ofctl add-flow br-int "arp,actions=NORMAL"
-    ovs-ofctl add-flow br-int "table=0, idle_age=0, priority=200,tun_id=$TUNNNEL_ID,in_port=vxlan_br-int actions=load:0x1->OXM_OF_METADATA[],resubmit(,100)"
-    ovs-ofctl add-flow br-int "table=0, priority=100,idle_age=0,in_port=$IB_PF0_PORT0 actions=load:0x4f6->NXM_NX_REG6[],load:0x1->OXM_OF_METADATA[],load:0->OXM_OF_IN_PORT[],resubmit(,0)"
+    ovs-ofctl add-flow br-int "table=0, idle_age=0, priority=200,tun_id=$TUNNNEL_ID,in_port=vxlan_br-int, actions=load:0x1->OXM_OF_METADATA[],resubmit(,100)"
+    ovs-ofctl add-flow br-int "table=0, priority=100,idle_age=0,in_port=$IB_PF0_PORT0, actions=load:0x4f6->NXM_NX_REG6[],load:0x1->OXM_OF_METADATA[],load:0->OXM_OF_IN_PORT[],resubmit(,0)"
     ovs-ofctl add-flow br-int "table=0, priority=1,idle_age=0,actions=resubmit(,5)"
     ovs-ofctl add-flow br-int "table=5, priority=1,idle_age=0,actions=resubmit(,10)"
     ovs-ofctl add-flow br-int "table=10, priority=1,idle_age=0,actions=resubmit(,17)"
     ovs-ofctl add-flow br-int "table=17, priority=1,idle_age=0,actions=resubmit(,20)"
     ovs-ofctl add-flow br-int "table=20, priority=1,idle_age=0,actions=resubmit(,55)"
-    ovs-ofctl add-flow br-int "table=55, priority=200,idle_age=0,metadata=0x1,dl_dst=$VXLAN_MAC actions=resubmit(,60)"
-    ovs-ofctl add-flow br-int "table=60, priority=200,idle_age=0,ip,metadata=0x1,nw_dst=$REMOTE_IP actions=mod_dl_src:$DUMMY_MAC,resubmit(,65)"
-    ovs-ofctl add-flow br-int "table=65, priority=200,idle_age=0,ip,metadata=0x1,nw_dst=$REMOTE_IP actions=mod_dl_dst:$VXLAN_MAC,load:0x5b2->NXM_NX_REG7[],resubmit(,75)"
-    ovs-ofctl add-flow br-int "table=75, priority=100,idle_age=0,reg7=0x5b2 actions=load:0->OXM_OF_IN_PORT[],load:0xaca80102->NXM_NX_TUN_IPV4_DST[],load:0x64->NXM_NX_TUN_ID[],output:vxlan_br-int"
-    ovs-ofctl add-flow br-int "table=100, idle_age=0, priority=200,metadata=0x1,dl_dst=$VF_MAC actions=load:0x4f6->NXM_NX_REG7[],resubmit(,105)"
-    ovs-ofctl add-flow br-int "table=105, idle_age=0, priority=1 actions=resubmit(,112)"
-    ovs-ofctl add-flow br-int "table=112, idle_age=0, priority=1 actions=resubmit(,114)"
-    ovs-ofctl add-flow br-int "table=114, idle_age=0, priority=1 actions=resubmit(,115)"
-    ovs-ofctl add-flow br-int "table=115, idle_age=0, priority=100,reg7=0x4f6 actions=output:$IB_PF0_PORT0"
+    ovs-ofctl add-flow br-int "table=55, priority=200,idle_age=0,metadata=0x1,dl_dst=$VXLAN_MAC, actions=resubmit(,60)"
+    ovs-ofctl add-flow br-int "table=60, priority=200,idle_age=0,ip,metadata=0x1,nw_dst=$REMOTE_IP, actions=mod_dl_src:$DUMMY_MAC,resubmit(,65)"
+    ovs-ofctl add-flow br-int "table=65, priority=200,idle_age=0,ip,metadata=0x1,nw_dst=$REMOTE_IP, actions=mod_dl_dst:$VXLAN_MAC,load:0x5b2->NXM_NX_REG7[],resubmit(,75)"
+    ovs-ofctl add-flow br-int "table=75, priority=100,idle_age=0,reg7=0x5b2, actions=load:0->OXM_OF_IN_PORT[],load:0xaca80102->NXM_NX_TUN_IPV4_DST[],load:0x64->NXM_NX_TUN_ID[],output:vxlan_br-int"
+    ovs-ofctl add-flow br-int "table=100, idle_age=0, priority=200,metadata=0x1,dl_dst=$VF_MAC, actions=load:0x4f6->NXM_NX_REG7[],resubmit(,105)"
+    ovs-ofctl add-flow br-int "table=105, idle_age=0, priority=1, actions=resubmit(,112)"
+    ovs-ofctl add-flow br-int "table=112, idle_age=0, priority=1, actions=resubmit(,114)"
+    ovs-ofctl add-flow br-int "table=114, idle_age=0, priority=1, actions=resubmit(,115)"
+    ovs-ofctl add-flow br-int "table=115, idle_age=0, priority=100,reg7=0x4f6, actions=output:$IB_PF0_PORT0"
 
     ovs-ofctl dump-flows br-int --color
 }
