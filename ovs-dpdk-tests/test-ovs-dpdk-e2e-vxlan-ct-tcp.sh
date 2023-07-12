734d7ee06743bc20414e4ac178d5f053d889d2c0
Mahmoud AbuZant <mahmoudabuz@nvidia.com>
2023-07-12 14:56:51 +0300

    test-ovs-dpdk-e2e-vxlan-ct-tcp.sh: Adapt test to work over bf
    Change-Id: I83c91f5a92a0c88151a4cfdafc1bab29c4f11550
    Signed-off-by: Mahmoud AbuZant <mahmoudabuz@nvidia.com>

--- ovs-dpdk-tests/test-ovs-dpdk-e2e-vxlan-ct-tcp.sh
+++ ovs-dpdk-tests/test-ovs-dpdk-e2e-vxlan-ct-tcp.sh
@@ -30,16 +30,6 @@ function config() {
     config_local_tunnel_ip $LOCAL_TUN_IP br-phy
 }
 
-function config_remote() {
-    on_remote ip link del $TUNNEL_DEV &>/dev/null
-    on_remote ip link add $TUNNEL_DEV type vxlan id $TUNNEL_ID remote $LOCAL_TUN_IP dstport 4789
-    on_remote ip a flush dev $REMOTE_NIC
-    on_remote ip a add $REMOTE_TUNNEL_IP/24 dev $REMOTE_NIC
-    on_remote ip a add $REMOTE_IP/24 dev $TUNNEL_DEV
-    on_remote ip l set dev $TUNNEL_DEV up
-    on_remote ip l set dev $REMOTE_NIC up
-}
-
 function add_openflow_rules() {
     ovs-ofctl del-flows br-int
     ovs-ofctl add-flow br-int "arp,actions=NORMAL"
@@ -53,7 +43,7 @@ function add_openflow_rules() {
 
 function run() {
     config
-    config_remote
+    config_remote_tunnel vxlan
     add_openflow_rules
 
     verify_ping
