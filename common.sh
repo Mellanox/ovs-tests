ff8cd58c7a1f3dbfd00582d1f8151e4bfe475309
Roi Dayan <roid@nvidia.com>
2023-07-31 11:07:44 +0300

    common.sh: skip nics not exposing phys switch id when detecting config
    Change-Id: I2646c56ad73885c85070a90c6bf86345562a4f7a
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- common.sh
+++ common.sh
@@ -2370,6 +2370,7 @@ function fw_ver_lt() {
 function __detect_config() {
     local i
     for i in `ls -1d /sys/class/net/*`; do
+        cat $i/phys_switch_id &>/dev/null || continue
         local port_name=`cat $i/phys_port_name 2>/dev/null`
         if [ "$port_name" == "p0" ]; then
             NIC=`basename $i`
