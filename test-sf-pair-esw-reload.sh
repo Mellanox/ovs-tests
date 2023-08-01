ff157019ea4f93a2b206f5133e39397af23f9668
Roi Dayan <roid@nvidia.com>
2023-08-01 16:19:51 +0300

    Add new test test-sf-pair-esw-reload.sh
    Change-Id: I7f2a68148ea282234a9e0a44cb0483dc9b47b21a
    Signed-off-by: Roi Dayan <roid@nvidia.com>

---
+++ test-sf-pair-esw-reload.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Test reload drivers while SFs in switchdev and shared fdb.
+
+my_dir="$(dirname "$0")"
+. $my_dir/common.sh
+. $my_dir/common-sf.sh
+
+IP1="7.7.7.1"
+IP2="7.7.7.2"
+
+function cleanup() {
+    log "cleanup"
+    reset_sfs_ns
+    remove_sfs
+}
+
+trap cleanup EXIT
+
+function config() {
+    local count=$1
+    local direction=${2:-""}
+
+    title "Config"
+
+    if [ -z $direction ]; then
+        create_sfs $count
+    elif [ "$direction" == "network" ]; then
+        create_network_direction_sfs $count
+    elif [ "$direction" == "host" ]; then
+        create_host_direction_sfs $count
+    elif [ "$direction" == "both" ]; then
+        create_network_direction_sfs $count
+        create_host_direction_sfs $count
+        ((count+=count))
+    fi
+
+    set_sf_eswitch
+    #reload_sfs_into_ns
+    set_sf_switchdev
+    verify_single_ib_device $((count*2))
+}
+
+enable_switchdev
+
+config 2
+reload_modules
+
+trap - EXIT
+cleanup
+test_done
