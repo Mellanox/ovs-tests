11896e19cf9e2deb0a99dfdbac8a022f304d8219
Eli Britstein <elibr@nvidia.com>
2023-08-13 20:52:09 +0300

    test-ovs-dpdk-modify-tos: New test
    Issue: 3530749
    Change-Id: I8516e0cdd237b94d6146b8f492f73e2c5780a2a2
    Signed-off-by: Eli Britstein <elibr@nvidia.com>

---
+++ ovs-dpdk-tests/test-ovs-dpdk-modify-tos.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Test OVS-DPDK with TOS rewrite
+#
+# Require external server
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common-dpdk.sh
+
+trap cleanup_test EXIT
+
+require_remote_server
+
+config_sriov 2
+require_interfaces REP NIC
+enable_switchdev
+bind_vfs
+
+cleanup_test
+config_remote_nic
+config_simple_bridge_with_rep 1
+start_vdpa_vm
+config_ns ns0 $VF $LOCAL_IP
+
+ovs-ofctl add-flow br-phy ip,actions=mod_nw_tos=8,normal
+
+verify_ping
+generate_traffic "remote" $LOCAL_IP
+
+check_offload_contains "set.*ipv4.*tos=0x8/0xfc" 2
+
+trap - EXIT
+cleanup_test
+test_done
