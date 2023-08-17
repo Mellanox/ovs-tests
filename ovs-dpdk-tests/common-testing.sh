1b87ec44ea45fb3142a8825aeb7534569f96bee1
Salem Sol <salems@nvidia.com>
2023-08-17 15:07:30 +0300

    ovs-dpdk-tests: common-testing: Fix function config_remote_vlan()
    This function assumed that REMOTE_NIC is already up before being called, this
    assumption is not necessarily true. Fix it.
    
    Change-Id: Ib1b60db159838a85f98b54e4db070001efdef4d6
    Signed-off-by: Salem Sol <salems@nvidia.com>

--- ovs-dpdk-tests/common-testing.sh
+++ ovs-dpdk-tests/common-testing.sh
@@ -477,6 +477,7 @@ function config_remote_vlan() {
     local vlan_dev=$2
     local ip=${3:-$REMOTE_IP}
     on_remote "ip a flush dev $REMOTE_NIC
+               ip l set dev $REMOTE_NIC up
                ip link add link $REMOTE_NIC name $vlan_dev type vlan id $vlan
                ip a add $ip/24 dev $vlan_dev
                ip l set dev $vlan_dev up"
