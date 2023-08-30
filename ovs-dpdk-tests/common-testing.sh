eed025c91e2bd9568667b09d9fff00630023a8b8
Roi Dayan <roid@nvidia.com>
2023-08-30 10:49:12 +0300

    common-testing.sh: Skip tcpdump translating protocol names
    Easier to read with the port numbers.
    
    Change-Id: I7029fe716ae26a6bee6e4ee4083ad30a778537d8
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- ovs-dpdk-tests/common-testing.sh
+++ ovs-dpdk-tests/common-testing.sh
@@ -226,7 +226,7 @@ function ovs_send_scapy_packets() {
     local pktgen="$DPDK_DIR/../scapy-traffic-tester.py"
 
     rm -rf $p_scapy
-    local tcpdump_cmd="tcpdump -nei $dev2 -Q in &> $p_scapy &"
+    local tcpdump_cmd="tcpdump -qnnei $dev2 -Q in &> $p_scapy &"
     local scapy_dst_cmd="timeout $((t+5)) $pktgen -l -i $dev2 --src-ip $src_ip --time $(($t+2)) &"
     local scapy_src_cmd="timeout $((t+5)) $pktgen -i $dev1 --src-ip $src_ip --dst-ip $dst_ip --time $t --pkt-count $pkt_count --inter 0.01 &"
 
