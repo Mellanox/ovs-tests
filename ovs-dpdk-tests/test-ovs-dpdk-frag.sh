8e5587e706050a8f31cc52bd6f742d37ae89b4a1
Eli Britstein <elibr@nvidia.com>
2023-09-03 12:54:14 +0300

    test-ovs-dpdk-frag: New test
    Change-Id: I9e25d81066a6f0cb0ac3300b82f88113fcf1fe3e
    Signed-off-by: Eli Britstein <elibr@nvidia.com>

---
+++ ovs-dpdk-tests/test-ovs-dpdk-frag.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# Test OVS with fragmented traffic
+#
+# Require external server
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common-dpdk.sh
+
+require_remote_server
+
+config_sriov 2
+require_interfaces REP NIC
+enable_switchdev
+bind_vfs
+
+trap cleanup_test EXIT
+
+function config() {
+    cleanup_test
+
+    config_tunnel "vxlan"
+    config_remote_tunnel "vxlan"
+    config_local_tunnel_ip $LOCAL_TUN_IP br-phy
+}
+
+function validate_rules() {
+    local x=$(ovs-appctl dpctl/dump-flows type=non-offloaded | grep 'eth_type(0x0800)' | wc -l)
+    if [ "$x" != "4" ]; then
+        ovs-appctl dpctl/dump-flows type=non-offloaded | grep 'eth_type(0x0800)'
+        err "Expected to have 4 flows (first/later, 2 dirs), have $x"
+    fi
+}
+
+function run() {
+    config
+
+    # icmp
+    verify_ping $REMOTE_IP ns0 2000
+
+    validate_rules
+}
+
+run
+trap - EXIT
+cleanup_test
+test_done
