f35ea475bd09bbeefb127a915a75b9181b3ab8cc
Dima Chumak <dchumak@nvidia.com>
2023-09-19 14:56:38 +0000

    ovs-dpdk-tests: Add test-ovs-dpdk-meter-modify.sh
    Test OVS-DOCA `ovs-ofctl mod-meter` support.
    
    Change-Id: I1dcee53708b288cd8f054a6165ba64df549cedac
    Signed-off-by: Dima Chumak <dchumak@nvidia.com>

---
+++ ovs-dpdk-tests/test-ovs-dpdk-meter-modify.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+# Test OVS-DOCA openflow mod-meter
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common-dpdk.sh
+
+trap cleanup_test EXIT
+
+function test_pre_config() {
+    config_sriov 2
+    require_interfaces REP NIC
+    enable_switchdev
+    bind_vfs
+
+    cleanup_test
+    set_e2e_cache_enable false
+    debug "Restarting OVS"
+    start_clean_openvswitch
+}
+
+function config_topology() {
+    config_simple_bridge_with_rep 2
+
+    config_ns ns0 $VF1 $REMOTE_IP
+    config_ns ns1 $VF2 $LOCAL_IP
+}
+
+function run() {
+    local max_rate_mbits=400
+    local sec=21
+
+    config_topology
+
+    title "Testing add meter on REPs"
+    ovs_add_meter br-phy 1 kbps 100000 10000
+    ovs_add_meter br-phy 2 kbps ${max_rate_mbits}000 ${max_rate_mbits}00
+    ovs_add_bidir_meter_rules
+
+    title "Generating traffic"
+    generate_traffic_verify_bw $sec $((max_rate_mbits * 3 / 2)) \
+        "ovs_mod_meter br-phy 2 kbps $((max_rate_mbits * 1000 * 2))"
+
+    ovs_del_meter br-phy 1
+    ovs_del_meter br-phy 2
+}
+
+test_pre_config
+run
+start_clean_openvswitch
+trap - EXIT
+cleanup_test
+test_done
