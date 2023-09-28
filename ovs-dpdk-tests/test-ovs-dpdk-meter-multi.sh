b2c80fc56ef294c2971877e604a829ade5516f6d
Dima Chumak <dchumak@nvidia.com>
2023-09-28 13:42:56 +0200

    ovs-dpdk-tests: Add test-ovs-dpdk-meter-multi.sh
    Doca meter test with multiple actions:meter in data path.
    
    Change-Id: I18a0210268b08d1d89b342ba7f49a66f99630b76
    Signed-off-by: Dima Chumak <dchumak@nvidia.com>

---
+++ ovs-dpdk-tests/test-ovs-dpdk-meter-multi.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+# Test OVS-DPDK openflow meters
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common-dpdk.sh
+
+trap cleanup_test EXIT
+
+function test_pre_config() {
+    config_sriov 2
+    require_interfaces REP NIC
+    enable_switchdev
+    bind_vfs
+
+    cleanup_test
+    set_e2e_cache_enable false
+    debug "Restarting OVS"
+    start_clean_openvswitch
+}
+
+function config_topology() {
+    config_simple_bridge_with_rep 2
+
+    config_ns ns0 $VF1 $REMOTE_IP
+    config_ns ns1 $VF2 $LOCAL_IP
+}
+
+function run() {
+    local max_rate_mbits=100
+    local sec=11
+
+    config_topology
+
+    title "Testing add meter on REPs"
+    ovs_add_meter br-phy 1 kbps 400000 40000
+    ovs_add_meter br-phy 2 kbps 200000 20000
+    ovs_add_meter br-phy 3 kbps ${max_rate_mbits}000 ${max_rate_mbits}00
+
+    ovs_add_multi_meter_rules
+
+    title "Generating traffic"
+    generate_traffic_verify_bw $sec $max_rate_mbits
+
+    ovs_del_meter br-phy 1
+    ovs_del_meter br-phy 2
+    ovs_del_meter br-phy 3
+}
+
+test_pre_config
+run
+start_clean_openvswitch
+trap - EXIT
+cleanup_test
+test_done
