8aa55d50dc9a29be44da14c27eae55c1eab43170
Roi Dayan <roid@nvidia.com>
2023-10-01 12:20:13 +0300

    New test test-ovs-add-del-br.sh
    Change-Id: I83b4d8e84cf272c74faefb3a62a7a6624e663adc
    Signed-off-by: Roi Dayan <roid@nvidia.com>

---
+++ ovs-dpdk-tests/test-ovs-add-del-br.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Test OVS-DPDK add/del bridge multiple times.
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common-dpdk.sh
+
+trap cleanup_test EXIT
+
+function run() {
+    local i
+
+    for i in `seq 4`; do
+        ovs-vsctl add-br ov1 || err "Failed to add br"
+        ovs-vsctl del-br ov1 || err "Failed to del br"
+    done
+    journalctl_for_test | grep -i "vswitchd.*killed.*SEGV"
+    if [ $? -eq 0 ]; then
+        err "openvswitch crashed"
+    fi
+}
+
+run
+trap - EXIT
+cleanup_test
+test_done
