83aa60820f7ffd2ca159d98cd30ce32e38a7f122
Mahmoud AbuZant <mahmoudabuz@nvidia.com>
2023-10-01 12:01:29 +0300

    test-ovs-dpdk-frag.sh: Fix filtering rules
    Change-Id: Ia883281a44299304b0e86820a7fbf2f55f479c09
    Signed-off-by: Mahmoud AbuZant <mahmoudabuz@nvidia.com>

--- ovs-dpdk-tests/test-ovs-dpdk-frag.sh
+++ ovs-dpdk-tests/test-ovs-dpdk-frag.sh
@@ -25,9 +25,9 @@ function config() {
 }
 
 function validate_rules() {
-    local x=$(ovs-appctl dpctl/dump-flows type=non-offloaded | grep 'eth_type(0x0800)' | wc -l)
+    local x=$(ovs-appctl dpctl/dump-flows type=non-offloaded | grep 'eth_type(0x0800)' | grep -E '(frag=first|frag=later)' | wc -l)
     if [ "$x" != "4" ]; then
-        ovs-appctl dpctl/dump-flows type=non-offloaded | grep 'eth_type(0x0800)'
+        ovs-appctl dpctl/dump-flows type=non-offloaded | grep 'eth_type(0x0800)' | grep -E '(frag=first|frag=later)'
         err "Expected to have 4 flows (first/later, 2 dirs), have $x"
     fi
 }
