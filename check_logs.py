0d1559ed1d8e9504dded8469d3c41250440a60d5
Roi Dayan <roid@nvidia.com>
2023-10-01 15:24:43 +0300

    check_logs.py: General improvements
    Add expected applied for all tests.
    Allow to pass url with artifact subfolder.
    Add tags to search also in fail tests.
    
    Change-Id: I608ff7ae7595fe2cec97cfda447691817fc39245
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- check_logs.py
+++ check_logs.py
@@ -3,6 +3,7 @@
 import argparse
 import requests
 from glob import glob
+import os
 
 
 def parse_args():
@@ -12,61 +13,75 @@ def parse_args():
     return parser.parse_args()
 
 
-TAGS = (
-    "error",
-    "failed",
-    "mlx5_cmd_out_err",
-    "unreferenced object",
-    "backtrace",
-    "command not found",
-    "invalid handle",
-    "No such file or directory",
-    "WARNING",
-    "XXX",
-    "TODO",
-    "Killed",
-    "File exists",
-    "No such file or directory",
-    "SSH connection",
-    "Cannot find device",
-)
+TAGS_ON_SUCCESS = [
+    'error',
+    'failed',
+    'mlx5_cmd_out_err',
+    'unreferenced object',
+    'backtrace',
+    'invalid handle',
+    'WARNING',
+    'Killed',
+    'File exists',
+    'SSH connection',
+    'Cannot find device',
+]
+
+TAGS_ALWAYS = [
+    'XXX',
+    'TODO',
+    'command not found',
+    'No such file or directory',
+    'too many arguments',
+]
 
 expected = {
-    "test-eswitch-devlink-reload.sh": ["Warning: mlx5_core: reload while VFs are present is unfavorable."]
+    'all': [
+        'failed to flow_del'
+    ],
+    'test-eswitch-devlink-reload.sh': ['Warning: mlx5_core: reload while VFs are present is unfavorable.']
 }
 
 
 def expected_line(test, line):
-    for e in expected.get(test, []):
+    _exp = expected['all'][:]
+    _exp.extend(expected.get(test, []))
+    for e in _exp:
         if e in line:
             return True
     return False
 
 
 def start():
-    tests = glob("test-*.sh")
+    tests = glob(os.path.join('*', "test-*.sh"))
     for test in tests:
-        url = args.url+"/artifact/test_logs/"+test+".html"
+        test = os.path.basename(test)
+        if 'artifact/test_logs' in args.url:
+            url = args.url + '/' + test + '.html'
+        else:
+            url = args.url + '/artifact/test_logs/' + test + '.html'
         r = requests.get(url)
         if not r.ok:
-            #print(url)
-            #print("ERROR: can't get url: %s" % r.status_code)
-            continue
-        if r.content.find('TEST PASSED') < 0:
+            # Didn't get the test names from summary so don't show an error.
+            #print("SKIP: Failed to fetch url: %s" % r.status_code)
             continue
 
-        for i in TAGS:
+        _tags = TAGS_ALWAYS[:]
+        if r.content.find('TEST PASSED') >= 0:
+            _tags.extend(TAGS_ON_SUCCESS)
+
+        for i in _tags:
             for line in r.content.splitlines():
                 if (i.lower() in line.lower()) and not expected_line(test, line):
                     break
             if i.lower() not in line.lower():
                 continue
-            print("%s    - %s" % (test, i))
-            print("  %s" % url)
+            print('%s - %s' % (test, i))
+            print('    %s' % url)
             break
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     args = parse_args()
     try:
         start()
