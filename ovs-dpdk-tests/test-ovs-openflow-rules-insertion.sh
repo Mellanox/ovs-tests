78d81fae9e639aeb7e9a0a3011b85a900f9236ff
Mahmoud AbuZant <mahmoudabuz@nvidia.com>
2023-11-05 14:36:38 +0200

    test-ovs-openflow-rules-insertion.sh: Fix the test
    The reproduction of slow insert was affected if bridge had dpdk ports.
    The more the ports the slower.
    
    Fix the test by adding dpdk ports to ovs bridge.
    Add in_port match to make sure we have ports or fail adding the rule.
    Also refactor some prints for readability.
    
    Issue: 3658059
    Signed-off-by: Mahmoud AbuZant <mahmoudabuz@nvidia.com>
    Change-Id: I5eedf353c2ffa17993db7545e0ececbd7f199e35

--- ovs-dpdk-tests/test-ovs-openflow-rules-insertion.sh
+++ ovs-dpdk-tests/test-ovs-openflow-rules-insertion.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #
-# Test OVS is capable to add 4k rules per second.
-# Feature Request #3494640: [HBN] [OVS-DOCA] 4K Openflow per sec on Bluefield-3.
+# Test OVS adding 4k openflow rules per second on a bridge with configured dpdk ports.
 #
+# Feature Request #3494640: [HBN] [OVS-DOCA] 4K Openflow per sec on Bluefield-3.
+# Task #3659033: low openflow insertion
 
 my_dir="$(dirname "$0")"
 . $my_dir/common-dpdk.sh
 
-config_sriov 2
+config_sriov 16
 enable_switchdev
 bind_vfs
 
@@ -15,18 +16,20 @@ trap cleanup_test EXIT
 
 number_of_rules=${1:-4000}
 output_file="/tmp/openflow_batch_$$"
-br=br-ovs
+br=br-phy
+ofctl_rule_prefix="in_port=2,ip,tcp"
+
 
 function config() {
     echo > $output_file
     start_clean_openvswitch
-    ovs-vsctl add-br $br
+    config_simple_bridge_with_rep 16
 }
 
 function create_openflow_rules_port_change_batch() {
     title "Creating openflow rules batch with port change"
     for i in `seq 1 $number_of_rules`; do
-        echo "ip,tcp,tcp_src=$i,action=drop" >> $output_file
+        echo "$ofctl_rule_prefix,tcp_src=$i,action=drop" >> $output_file
     done
 }
 
@@ -40,7 +43,7 @@ function create_openflow_rules_mac_change_batch() {
             for ((k = 0; k < 99; k++)); do
                 for ((l = 0; l < 99; l++)); do
                     mac="e4:11:$i:$j:$k:$l"
-                    echo "ip,tcp,dl_src=$mac,dl_dst=$mac, action=drop" >> $output_file
+                    echo "$ofctl_rule_prefix,dl_src=$mac,dl_dst=$mac,action=drop" >> $output_file
                     let count=count+1
                     [ $count -eq $number_of_rules ] && return
                 done
@@ -53,11 +56,11 @@ function test_time_cmd() {
     local x=$1
     local cmd=$2
     local t1=`get_ms_time`
-    time $cmd
+    time $cmd || err "Command failed: $cmd"
     local t2=`get_ms_time`
     let t=t2-t1
     if [ $t -gt $x ]; then
-        err "Took $t but expected less than $x ms"
+        err "Took $t ms but expected less than $x ms"
     else
         success "took $t ms (max $x)"
     fi
@@ -68,21 +71,26 @@ function apply_batch() {
     test_time_cmd 1000 "ovs-ofctl add-flows $br $output_file"
     local rules=`ovs-appctl bridge/dump-flows $br | wc -l`
 
-    if [ $rules -lt $number_of_rules ]; then
-        err "Expected at least $number_of_rules and found only $rules"
+    if [ $rules -ge $number_of_rules ]; then
+        success "Found $rules rules which is more than $number_of_rules rules"
     else
-        success "Found $rules which is more than $number_of_rules"
+        err "Expected at least $number_of_rules rules and found only $rules rules"
     fi
 }
 
 function run() {
     config
+
     create_openflow_rules_port_change_batch
     apply_batch
+
     ovs-ofctl del-flows $br
+
     echo > $output_file
     create_openflow_rules_mac_change_batch
     apply_batch
+
+    config_sriov 2
 }
 
 run
