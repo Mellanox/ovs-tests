11d39c349f6380d4482c994cc69c274d1dfebb0b
Roi Dayan <roid@nvidia.com>
2023-11-30 12:44:26 +0200

    add test test-ovs-dpdk-dp-hash-vxlan-busy-port.sh
    Change-Id: I6fee0538ab46fb3906c432840f29cabc45568adc
    Signed-off-by: Roi Dayan <roid@nvidia.com>

---
+++ ovs-dpdk-tests/test-ovs-dpdk-dp-hash-vxlan-busy-port.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+#
+# Test dp-hash after vxlan encap, busy port issue
+#
+# [DAL2OVS] Bug SW #3676502: After sr-iov deletion/recreation, Ping started failing from x86 host to connected svi(configured on vf interface).
+#
+# Require external server
+#
+
+my_dir="$(dirname "$0")"
+. $my_dir/common-dpdk.sh
+
+require_remote_server
+
+config_sriov 2
+enable_switchdev
+bind_vfs
+
+IB_PORT=`get_port_from_pci`
+
+function cleanup() {
+    ovs_conf_remove hw-offload-ct-size
+    ovs_conf_remove max-idle
+    cleanup_test
+}
+trap cleanup EXIT
+
+function config() {
+    ovs_conf_set hw-offload-ct-size 0
+    cleanup_test
+    config_tunnel "vxlan" 1 br-phy br-phy
+    bf_wrap "ip link add dev dummy type veth peer name rep-dummy"
+    config_local_tunnel_ip $LOCAL_TUN_IP br-phy
+    ovs-vsctl add-port br-phy rep-dummy
+    ovs-vsctl show
+}
+
+function add_openflow_rules() {
+    local bridge="br-phy"
+    ovs-ofctl del-flows $bridge
+    ovs-ofctl add-group $bridge group_id=1,type=select,bucket=watch_port=$IB_PORT,output:$IB_PORT,bucket=watch_port=rep-dummy,output:rep-dummy
+
+    ovs-ofctl add-flow $bridge in_port=$IB_PF0_PORT0,actions=vxlan_$bridge
+    ovs-ofctl add-flow $bridge in_port=vxlan_$bridge,actions=$IB_PF0_PORT0
+
+    ovs-ofctl add-flow $bridge in_port=LOCAL,actions=group:1
+    ovs-ofctl add-flow $bridge in_port=$IB_PORT,actions=LOCAL
+
+    debug "OVS groups:"
+    ovs-ofctl dump-groups $bridge --color
+
+    ovs_ofctl_dump_flows
+}
+
+function run() {
+    config
+    config_remote_tunnel vxlan
+    add_openflow_rules
+
+    ovs_conf_set max-idle 300000
+    verify_ping $REMOTE_IP ns0
+    ovs-appctl dpctl/dump-flows -m
+
+    config_sriov 0
+    local i
+    for i in `seq 8`; do
+        ovs-vsctl show | grep -q error && break
+        sleep 1
+    done
+    if [ $i -gt 7 ]; then
+        err "Expected a port error"
+    fi
+    ovs-vsctl show
+    ovs-vsctl show | grep -q "already in use" && err "Failed to clean some ports."
+    # e.g.: error: "'pf0vf0' is trying to use device '0000:08:00.0,representor=[0],dv_xmeta_en=4,dv_flow_en=2' which is already in use by 'pf0vf0'"
+
+    # issue could be on reconfigure.
+    config_sriov 2
+    sleep $i
+    ovs-vsctl show
+    ovs-vsctl show | grep -q "already in use" && err "Failed to clean some ports."
+}
+
+run
+bf_wrap "ip link del dummy"
+trap - EXIT
+cleanup
+test_done
