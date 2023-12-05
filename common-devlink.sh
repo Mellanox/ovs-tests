cd4a8b4f17f3f180862971b281156d90ee7df4c8
Roi Dayan <roid@nvidia.com>
2023-12-05 14:09:26 +0200

    common-devlink.sh: Fix to newer SF interface
    The common-devlink.sh file is old and expect an old
    interface when SFs were first introduced.
    
    The two tests using this commit were disabled for
    a year and changes were not detected.
    
    Instead of full modification of the tests to use common-sf.sh
    update what is needed and cleanup later.
    
    Change-Id: I04403e78e8407b94b785488ec9fbaacc745a7135
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- common-devlink.sh
+++ common-devlink.sh
@@ -1,12 +1,6 @@
-function devlink_get_pci_dev() {
-    local pci_dev=`devlink_get_port_dev $1`
-    pci_dev=${pci_dev%/*}
-    echo $pci_dev
-}
-
 function devlink_get_port_dev() {
     local dev=$1
-    local pci_dev=`devlink port show | grep "netdev $dev" | cut -d" " -f1`
+    local pci_dev=`devlink port show | grep -w "netdev $dev" | cut -d":" -f1`
     echo $pci_dev
 }
 
@@ -15,23 +9,36 @@ function devlink_dev_set_param() {
     local param_name=$2
     local value=$3
     local cmode=${4:-driverinit}
-    local pci_dev=`devlink_get_pci_dev $dev`
 
-    devlink dev param set $pci_dev name $param_name value $value cmode $cmode || err "Failed to set $dev param $param_name=$value"
+    log "devlink set param dev $dev name $param_name value $value"
+    devlink dev param set $dev name $param_name value $value cmode $cmode || err "Failed to set $dev param $param_name=$value"
 }
 
 function devlink_dev_get_param() {
     local dev=$1
     local param_name=$2
-    local pci_dev=`devlink_get_pci_dev $dev`
 
-    devlink dev param show $pci_dev name $param_name | grep "cmode .* value " | awk '{print $4}' || err "Failed to get $dev param $param_name value"
+    devlink dev param show $dev name $param_name &>/dev/null || err_stderr "Failed to get $dev param $param_name"
+    devlink dev param show $dev name $param_name | grep "cmode .* value " | awk '{print $4}'
 }
 
 function devlink_dev_reload() {
-    local dev=$1
-    local pci_dev=`devlink_get_pci_dev $dev`
-    devlink dev reload $pci_dev
+    local dev=${1:-$NIC}
+    local pci_dev
+
+    if [ "$dev" == "$NIC" ]; then
+        pci_dev="pci/$PCI"
+    elif [ "$dev" == "$NIC2" ]; then
+        pci_dev="pci/$PCI2"
+    else
+	pci_dev=`devlink_get_port_dev $dev`
+	if [ -z "$pci_dev" ]; then
+	    pci_dev=$dev
+	fi
+    fi
+
+    log "devlink reload dev $dev pci $pci_dev"
+    devlink dev reload $pci_dev || err "Failed devlink reload"
 }
 
 function devlink_dev_set_eq() {
@@ -39,6 +46,8 @@ function devlink_dev_set_eq() {
     local event_eq_size=$2
     local devs=${@:3}
     local dev
+
+    [ -z "$devs" ] && err "devlink_dev_set_eq: empty dev list" && return
 
     for dev in $devs; do
         devlink_dev_set_param $dev io_eq_size $io_eq_size
@@ -48,5 +57,5 @@ function devlink_dev_set_eq() {
 }
 
 function devlink_get_sfs() {
-    devlink port show | grep mlx5_core.sf. | grep -E -o "netdev [a-z0-9]+" | awk {'print $2'}
+    devlink dev show | grep -w sf
 }
