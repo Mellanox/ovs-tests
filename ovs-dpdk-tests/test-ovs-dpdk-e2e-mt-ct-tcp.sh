cd5e154e9ecf2adc8b28f9e2354e71f91d782e34
Salem Sol <salems@nvidia.com>
2023-12-06 13:43:02 +0200

    ovs-dpdk-tests: test-ovs-dpdk-e2e-mt-ct-tcp: Adapt test to support new case
    This test was originally added to check issue #3541222, however it lacked proper
    checking for the connections after the aging time and missed a bug described in #3696162.
    Add support for it.
    
    Issue: 3541222
    Issue: 3696162
    Change-Id: Ib76f4e813dd74c580213c874948d9281e616eb31
    Signed-off-by: Salem Sol <salems@nvidia.com>

--- ovs-dpdk-tests/test-ovs-dpdk-e2e-mt-ct-tcp.sh
+++ ovs-dpdk-tests/test-ovs-dpdk-e2e-mt-ct-tcp.sh
@@ -6,6 +6,7 @@
 # E2E-CACHE
 #
 # Bug SW #3541222: [BF2,OVS-DPDK,Ubuntu20.04] - OVS got error core dumped (Segmentation fault) after DPIX (connection tracking insertion) testing
+# Bug SW #3696162: [ovs-dpdk] e2e-cache flows are wrongly aged | Assignee: Salem Sol | Status: Assigned
 
 my_dir="$(dirname "$0")"
 . $my_dir/common-dpdk.sh
@@ -14,12 +15,17 @@ config_sriov 2
 enable_switchdev
 bind_vfs
 
-trap cleanup_test EXIT
+function cleanup() {
+    ovs_clear_datapaths
+    cleanup_test
+}
+
+trap cleanup EXIT
 
 function config() {
     cleanup_test
     set_e2e_cache_enable
-    ovs_conf_set e2e-size 2
+    ovs_conf_set e2e-size 3
     debug "Restarting OVS"
     restart_openvswitch
 
@@ -28,6 +34,10 @@ function config() {
     start_vdpa_vm $NESTED_VM_NAME2 $NESTED_VM_IP2
     config_ns ns0 $VF $LOCAL_IP
     config_ns ns1 $VF2 $REMOTE_IP
+
+    # ct zone used in ovs_add_ct_rules
+    local ct_zone=5
+    ovs_set_ct_zone_timeout $ct_zone "tcp" 30
 }
 
 function add_openflow_rules() {
@@ -39,12 +49,11 @@ function run() {
     add_openflow_rules
 
     verify_ping
-    generate_traffic "local" $LOCAL_IP ns1
-    check_e2e_stats
+    generate_traffic "local" $LOCAL_IP ns1 true "ns0" "local" 60
     ovs_conf_remove e2e-size
 }
 
 run
 trap - EXIT
-cleanup_test
+cleanup
 test_done
