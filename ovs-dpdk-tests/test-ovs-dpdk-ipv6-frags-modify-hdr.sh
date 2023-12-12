0b96e7648cb6983ed5ed8be1865e9873461fd9fe
Roi Dayan <roid@nvidia.com>
2023-12-12 16:57:15 +0200

    test-ovs-dpdk-ipv6-frags-modify-hdr.sh: Avoid redundant ovs restarts
    Change hw-offload before call to cleanup() which already restarts ovs.
    
    Change-Id: I75246017c402b5f54f2a6355b2b879a8f9e9ca1e
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- ovs-dpdk-tests/test-ovs-dpdk-ipv6-frags-modify-hdr.sh
+++ ovs-dpdk-tests/test-ovs-dpdk-ipv6-frags-modify-hdr.sh
@@ -17,15 +17,13 @@ IP6_ADDR2="10:0:1::2"
 DUMMY_IP6_ADDR="10:0:1::3"
 
 function cleanup() {
-    cleanup_test
     ovs_conf_set hw-offload true
+    cleanup_test
 }
 
 function config() {
-    cleanup_test
-    debug "Restarting OVS"
     ovs_conf_set hw-offload false
-    start_clean_openvswitch
+    cleanup_test
 
     config_simple_bridge_with_rep 2
     config_ns ns0 $VF $LOCAL_IP $IP6_ADDR1
