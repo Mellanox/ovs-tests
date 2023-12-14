d619c4eb97bfc2fc42bc99fcd7968d5b565571d7
Roi Dayan <roid@nvidia.com>
2023-12-14 19:14:23 +0200

    common.sh: Set core dump limit to unlimited
    Make sure core dump size limit is to unlimited,
    in case a process is crashing to get a possible code dump.
    
    Change-Id: If83a0ce70ad959041b82e826c5965601ac097353
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- common.sh
+++ common.sh
@@ -247,6 +247,7 @@ function __setup_common() {
     require_interfaces NIC NIC2
     require_cmd lspci ethtool tc bc jq
     fail_if_err
+    bf_wrap ulimit -c unlimited
 
     sysfs_pci_device=`readlink -f /sys/class/net/$NIC/../../`
     SRIOV_NUMVFS_NIC=$sysfs_pci_device/sriov_numvfs
