c158a84bf9c2353c3e93565fe1d486a3999167ea
Roi Dayan <roid@nvidia.com>
2023-12-19 17:02:14 +0200

    test-ovs-dpdk-vfs-add-del-offload-false.sh: Remove redundant ovs restarts
    Add some space lines for readability.
    
    Change-Id: I8c9ed865cf49796efd23eb9c8aefd9cfd1985340
    Signed-off-by: Roi Dayan <roid@nvidia.com>

--- ovs-dpdk-tests/test-ovs-dpdk-vfs-add-del-offload-false.sh
+++ ovs-dpdk-tests/test-ovs-dpdk-vfs-add-del-offload-false.sh
@@ -13,23 +13,24 @@ enable_switchdev
 bind_vfs
 
 function cleanup() {
-    cleanup_test
     ovs_conf_set hw-offload true
-    restart_openvswitch
+    cleanup_test
 }
 
 trap cleanup EXIT
 
 function config() {
-    cleanup_test
     ovs_conf_set hw-offload false
-    restart_openvswitch
+    cleanup_test
     ovs_add_bridge
     ovs_add_port "PF"
+
     ovs_add_port "VF" 0
     ovs_del_port "VF" 0
+
     ovs_add_port "VF" 0
     ovs_add_port "VF" 1
+
     config_ns ns0 $VF $LOCAL_IP
     config_ns ns1 $VF2 $REMOTE_IP
     ovs-vsctl show
