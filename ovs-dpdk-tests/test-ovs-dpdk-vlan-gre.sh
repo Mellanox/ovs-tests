046fef2182c1c0b62408f53069b8fefbe41ab53b
Emeel Hakim <ehakim@nvidia.com>
2024-01-15 16:15:35 +0200

    test-ovs-dpdk-vlan-gre.sh: Refactor
    The main motivation behind this refactoring is to address
    statistical failures related to Unexpected rules:
    
    for example:
    
    ufid:acbae6b8-5c83-48cf-8eb6-b24372faea21, skb_priority(0/0),skb_mark(0/0),
    ct_state(0/0),ct_zone(0/0),ct_mark(0/0),ct_label(0/0),recirc_id(0),dp_hash(0/0),
    in_port(pf.5),packet_type(ns=0,id=0),eth(src=c2:a8:22:ff:0f:11,dst=10:70:fd:d9:0f:a8),
    eth_type(0x0800),ipv4(src=7.7.7.7/0.0.0.0,dst=7.7.7.8/0.0.0.0,proto=1/0,tos=0xc0/0,
    ttl=64/0,frag=no),icmp(type=3/0,code=3/0), packets:0, bytes:0, used:never, dp:ovs,
    actions:push_vlan(vid=5,pcp=0),pf0, dp-extra-info:miniflow_bits(5,1)
    
    This rule serving as noise for forwarding traffic from 'br-int' to the internal VLAN
    'pf.5' on 'br-phy', the issue is similar to in_port:<bridge> or action:<bridge>.
    
    The root cause of receiving such a rule is applying some ip to br-phy then
    upon configuring tunnel removing this ip from the br-phy.
    
    To address this, don't provide an ip for the br-phy.
    
    While at it modularize the test by dividing it into functions.
    
    Issue: 3716271
    Change-Id: If5968d0d8aa55d679bc467ff67cc7061a413c11f
    Signed-off-by: Emeel Hakim <ehakim@nvidia.com>

--- ovs-dpdk-tests/test-ovs-dpdk-vlan-gre.sh
+++ ovs-dpdk-tests/test-ovs-dpdk-vlan-gre.sh
@@ -15,27 +15,31 @@ vlan_dev=${REMOTE_NIC}.$vlan
 
 trap 'cleanup_test $vlan_dev' EXIT
 
-config_sriov 2
-enable_switchdev
-bind_vfs
+function config() {
+    config_sriov 2
+    enable_switchdev
+    bind_vfs
 
-cleanup_test $vlan_dev
+    cleanup_test $vlan_dev
 
-gre_set_entropy
+    gre_set_entropy
 
-debug "Restarting OVS"
-start_clean_openvswitch
+    debug "Restarting OVS"
+    start_clean_openvswitch
 
-config_tunnel gre
-config_local_tunnel_ip $LOCAL_TUN_IP br-phy
-config_remote_tunnel gre
-config_remote_vlan $vlan $vlan_dev $REMOTE_TUNNEL_IP
-config_local_vlan $vlan $LOCAL_TUN_IP
+    config_tunnel gre
+    config_remote_tunnel gre
+    config_remote_vlan $vlan $vlan_dev $REMOTE_TUNNEL_IP
+    config_local_vlan $vlan $LOCAL_TUN_IP
+}
 
-verify_ping $REMOTE_IP
+function run_test() {
+    config
+    verify_ping $REMOTE_IP
+    generate_traffic "remote" $LOCAL_IP
+}
 
-generate_traffic "remote" $LOCAL_IP
-
+run_test
 trap - EXIT
 cleanup_test $vlan_dev
 test_done
