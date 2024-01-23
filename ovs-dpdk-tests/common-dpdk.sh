55060d798ba495d139396dc10a428f38e9a862f7
Emeel Hakim <ehakim@nvidia.com>
2024-01-23 13:03:59 +0200

    common-dpdk.sh: Filter out rules of type destination unreachable
    Due to noise OVS receives a packet of icmp(type=3/0,code=3/0)
    which is destination unreachable. Filter out those to avoid
    statistical failures.
    
    Change-Id: I37248b612da534ce93adadb4c64314d27c5ee14f
    Signed-off-by: Emeel Hakim <ehakim@nvidia.com>

--- ovs-dpdk-tests/common-dpdk.sh
+++ ovs-dpdk-tests/common-dpdk.sh
@@ -592,7 +592,7 @@ function check_offload_contains() {
 function check_dpdk_offloads() {
     local IP=$1
     local expected_sw_packets=${2:-100000}
-    local filter='arp\|drop\|ct_state(0x21/0x21)\|flow-dump\|nd(\|33:33:00:00'
+    local filter='arp\|drop\|ct_state(0x21/0x21)\|flow-dump\|nd(\|33:33:00:00\|icmp(type=3/0,code=3/0)'
     local regex_filter='icmp.*ct\(.*'
     local ovs_type="DPDK"
     local bridge
