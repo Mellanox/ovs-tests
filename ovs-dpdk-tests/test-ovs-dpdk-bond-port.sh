cc6e5781bd230fbea5a282d001a722e920101c00
Roi Dayan <roid@nvidia.com>
2024-02-07 12:07:40 +0200

    test-ovs-dpdk-bond-port.sh: Add test for bond port
    Change-Id: I32dc462808fb558788a8681f30942f6394170b45
    Signed-off-by: Roi Dayan <roid@nvidia.com>

---
+++ ovs-dpdk-tests/test-ovs-dpdk-bond-port.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# Test adding bond0 port
+#
+# [OVS] Bug SW #3770681: [OVS] Can't to connect bond to OVS bridge
+
+my_dir="$(dirname "$0")"
+. $my_dir/common-dpdk.sh
+. $my_dir/../common-sf.sh
+
+enable_switchdev
+
+function cleanup() {
+    clean_vf_lag
+    cleanup_test
+}
+
+trap cleanup EXIT
+
+function clean_vf_lag() {
+    # must unbind vfs to create/destroy lag
+    unbind_vfs $NIC
+    unbind_vfs $NIC2
+    clear_bonding
+}
+
+function config_vf_lag() {
+    local mode=${1:-"802.3ad"}
+
+    config_sriov 2 $NIC
+    config_sriov 2 $NIC2
+    enable_switchdev $NIC
+    enable_switchdev $NIC2
+    config_bonding $NIC $NIC2 $mode || fail
+    bind_vfs $NIC
+    bind_vfs $NIC2
+}
+
+function config() {
+    config_vf_lag
+}
+
+function run() {
+    cleanup
+    config
+    start_clean_openvswitch
+    ovs_add_bridge br-phy
+    __ovs_add_port br-phy bond0 dpdk
+    ovs-vsctl show
+    # ovs could return success on adding the bond port but it still could be in error state.
+    ovs-vsctl show | grep -q "error" && err "Some ports in error state."
+    ovs_clear_bridges
+}
+
+run
+trap - EXIT
+cleanup
+test_done
