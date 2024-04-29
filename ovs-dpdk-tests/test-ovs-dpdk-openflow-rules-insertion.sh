631f483c7ee4883921516c4638450e0184be221e
Mahmoud AbuZant <mahmoudabuz@nvidia.com>
2024-04-29 09:53:34 +0300

    test-ovs-dpdk-openflow-rules-insertion.sh: Expect more time in ovs asan
    We noticed longer but acceptable time for ovs asan.
    Around 1700 ms vs 1200 ms.
    
    Signed-off-by: Mahmoud AbuZant <mahmoudabuz@nvidia.com>
    Change-Id: I06df44a3f30b93ac7f077dfd138e6b2f07a99c01

--- ovs-dpdk-tests/test-ovs-dpdk-openflow-rules-insertion.sh
+++ ovs-dpdk-tests/test-ovs-dpdk-openflow-rules-insertion.sh
@@ -26,7 +26,7 @@ output_file="/tmp/openflow_batch_$$"
 br=br-phy
 ofctl_rule_prefix="in_port=2,ip,tcp"
 
-if [ "$short_device_name" == "bf2" ]; then
+if [ "$short_device_name" == "bf2" ] || is_asan; then
     expected_time=$((number_of_rules/1000*500))
 fi
 
