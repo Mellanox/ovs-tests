7962a70f3d3b43cd160b67aae0ef4f477dffc11d
Ziyad Atiyyeh <ziyadat@nvidia.com>
2024-05-05 16:57:51 +0300

    Skip expected dmesg error in eswitch-pci-reset tests
    Issue: 3888796
    Issue: 3888602
    Issue: 2805586
    Signed-off-by: Ziyad Atiyyeh <ziyadat@nvidia.com>
    Change-Id: I67a00a0bd4e5d3e230b881b7ffd087e968937f5d

--- test-eswitch-pci-reset-twice.sh
+++ test-eswitch-pci-reset-twice.sh
@@ -10,6 +10,8 @@ my_dir="$(dirname "$0")"
 if is_upstream; then
     # Check comments in Bug SW #2805586: [Upstream] null-ptr-deref in mlx5e_suspend+0x9c/0x130 while resetting PCI in switchdev mode
     add_expected_error_msg ".*E-Switch: Getting vhca_id for vport failed \(vport=.*,err=-67\)"
+    # This is after Bug SW #3888602: [Upstream] vport X error -67 reading stats
+    add_expected_error_msg ".*failed reading stats on vport .*, error -67"
 fi
 
 config_sriov
