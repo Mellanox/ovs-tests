17f6a8d498449b3c21dcde9ecf0cd7402c6c7e96
Paul Blakey <paulb@nvidia.com>
2024-09-18 15:08:37 +0300

    ovs-dpdk-tests/test-ovs-dpdk-ct-multipmd.sh: New test
    New test that uses OVS configured with multipmd and default doca-ct.
    
    Issue: 4069266
    Change-Id: I74858dcb1ab10396e1863fdf77a51196a39756fa
    Signed-off-by: Paul Blakey <paulb@nvidia.com>

--- ovs-dpdk-tests/test-ovs-dpdk-ct-multipmd.sh
+++ ovs-dpdk-tests/test-ovs-dpdk-ct-multipmd.sh
@@ -4,7 +4,7 @@
 #
 # Require external server
 #
-# Bug SW #4069266: [OVS-DOCA, Performance] Failed to insert DOCA-CT entry: -24 when sending 20 mpps rate with packet size 114 and ct connections | Assignee: Paul Blakey | Status: In Progress
+# Bug SW #4069266: [OVS-DOCA, Performance] Failed to insert DOCA-CT entry: -24 when sending 20 mpps rate with packet size 114 and ct connections
 #
 
 my_dir="$(dirname "$0")"
@@ -21,12 +21,16 @@ trap cleanup EXIT
 function cleanup() {
     ovs-vsctl --no-wait remove o . other_config pmd-cpu-mask
     ovs-vsctl --no-wait remove o . other_config hw-offload-ct-size
+    ovs-vsctl --no-wait remove o . other_config doca-ct
+    ovs-vsctl --no-wait remove o . other_config doca-ct-ipv6
     cleanup_test
 }
 
 function config() {
     config_simple_bridge_with_rep 1
     config_ns ns0 $VF $LOCAL_IP
+    ovs-vsctl --timeout=$OVS_VSCTL_TIMEOUT set o . other_config:doca-ct=true
+    ovs-vsctl --timeout=$OVS_VSCTL_TIMEOUT set o . other_config:doca-ct-ipv6=false
     ovs-vsctl --timeout=$OVS_VSCTL_TIMEOUT set o . other_config:pmd-cpu-mask=0x6
     ovs-vsctl --timeout=$OVS_VSCTL_TIMEOUT set o . other_config:hw-offload-ct-size=4096
     restart_openvswitch_nocheck
@@ -43,6 +47,7 @@ function run() {
 
     verify_ping
 
+    title "Run traffic and try to fill ct table entirely"
     set_iperf2
     generate_traffic "remote" $LOCAL_IP none true ns0 local 30 4096
 
